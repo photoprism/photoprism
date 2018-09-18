a204b21cda9c489f16566eb148a4f1b97dde4562
Michael Mayer <michael@liquidbytes.net>
2018-09-18 10:12:25 +0200

    Added support for setting config values via environment variables

--- cmd/photoprism/photoprism.go
+++ cmd/photoprism/photoprism.go
@@ -48,16 +48,19 @@ func main() {
 					Name:  "server-port, p",
 					Usage: "HTTP server port",
 					Value: 80,
+					EnvVar: "PHOTOPRISM_SERVER_PORT",
 				},
 				cli.StringFlag{
 					Name:  "server-ip, i",
 					Usage: "HTTP server IP address (optional)",
 					Value: "",
+					EnvVar: "PHOTOPRISM_SERVER_IP",
 				},
 				cli.StringFlag{
 					Name:  "server-mode, m",
 					Usage: "debug, release or test",
 					Value: "",
+					EnvVar: "PHOTOPRISM_SERVER_MODE",
 				},
 			},
 			Action: func(context *cli.Context) error {
@@ -287,50 +290,60 @@ var globalCliFlags = []cli.Flag{
 	cli.BoolFlag{
 		Name:  "debug",
 		Usage: "run in debug mode",
+		EnvVar: "PHOTOPRISM_DEBUG",
 	},
 	cli.StringFlag{
 		Name:  "config-file, c",
 		Usage: "load configuration from `FILENAME`",
 		Value: "/etc/photoprism/photoprism.yml",
+		EnvVar: "PHOTOPRISM_CONFIG_FILE",
 	},
 	cli.StringFlag{
 		Name:  "darktable-cli",
 		Usage: "darktable command-line executable `FILENAME`",
 		Value: "/usr/bin/darktable-cli",
+		EnvVar: "PHOTOPRISM_DARKTABLE_CLI",
 	},
 	cli.StringFlag{
 		Name:  "originals-path",
 		Usage: "originals `PATH`",
 		Value: "/var/photoprism/photos/originals",
+		EnvVar: "PHOTOPRISM_ORIGINALS_PATH",
 	},
 	cli.StringFlag{
 		Name:  "thumbnails-path",
 		Usage: "thumbnails `PATH`",
 		Value: "/var/photoprism/photos/thumbnails",
+		EnvVar: "PHOTOPRISM_THUMBNAILS_PATH",
 	},
 	cli.StringFlag{
 		Name:  "import-path",
 		Usage: "import `PATH`",
 		Value: "/var/photoprism/photos/import",
+		EnvVar: "PHOTOPRISM_IMPORT_PATH",
 	},
 	cli.StringFlag{
 		Name:  "export-path",
 		Usage: "export `PATH`",
 		Value: "/var/photoprism/photos/export",
+		EnvVar: "PHOTOPRISM_EXPORT_PATH",
 	},
 	cli.StringFlag{
 		Name:  "assets-path",
 		Usage: "assets `PATH`",
 		Value: "/var/photoprism",
+		EnvVar: "PHOTOPRISM_ASSETS_PATH",
 	},
 	cli.StringFlag{
 		Name:  "database-driver",
 		Usage: "database `DRIVER` (mysql, mssql, postgres or sqlite)",
 		Value: "mysql",
+		EnvVar: "PHOTOPRISM_DATABASE_DRIVER",
 	},
 	cli.StringFlag{
 		Name:  "database-dsn",
 		Usage: "database data source name (`DSN`)",
 		Value: "photoprism:photoprism@tcp(localhost:3306)/photoprism",
+		EnvVar: "PHOTOPRISM_DATABASE_DSN",
 	},
 }
