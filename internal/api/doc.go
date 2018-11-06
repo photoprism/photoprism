3e3327ade356b8d68f582bb57575819568fc2fff
Michael Mayer <michael@liquidbytes.net>
2018-11-06 11:10:57 +0100

    Added link to wiki / developer guide, see #12

--- internal/api/doc.go
+++ internal/api/doc.go
@@ -2,5 +2,9 @@
 Package api contains REST request handlers used by the server package.
 
 See https://photoprism.org/ for more information about PhotoPrism.
+
+Additional information concerning the API can be found in our Developer Guide:
+
+https://github.com/photoprism/photoprism/wiki/REST-API
 */
 package api
