48a9f78cb39cb494192a63fc4fd7e0bc0efe8542
Sebastian Kunert <skunert49@gmail.com>
2019-04-18 17:27:08 +0200

    Improve color indexing by using k-means clustering (#103)
    * First commit for color-improvements
    
    * Use k-means for prominent color detection
    
    * Remove debugging code
    
    * Add color checks to tests
    
    * Add additional test for color names

--- internal/photoprism/colors.go
+++ internal/photoprism/colors.go
@@ -1,24 +1,59 @@
 package photoprism
 
 import (
-	"image"
-	"os"
-	"sort"
-
+	"fmt"
+	"github.com/EdlinOrg/prominentcolor"
 	"github.com/RobCherry/vibrant"
 	"github.com/lucasb-eyer/go-colorful"
-	"golang.org/x/image/colornames"
+	"image"
+	"image/color"
+	"log"
+	"os"
+	"sort"
 )
 
-func getColorNames(actualColor colorful.Color) (result []string) {
-	var maxDistance = 0.22
+var colorMap = map[string]color.RGBA{
+	"red":    {0xf4, 0x43, 0x36, 0xff},
+	"pink":   {0xe9, 0x1e, 0x63, 0xff},
+	"purple": {0x9c, 0x27, 0xb0, 0xff},
+	"indigo": {0x3F, 0x51, 0xB5, 0xff},
+	"blue":   {0x21, 0x96, 0xF3, 0xff},
+	"cyan":   {0x00, 0xBC, 0xD4, 0xff},
+	"teal":   {0x00, 0x96, 0x88, 0xff},
+	"green":  {0x4C, 0xAF, 0x50, 0xff},
+	"lime":   {0xCD, 0xDC, 0x39, 0xff},
+	"yellow": {0xFF, 0xEB, 0x3B, 0xff},
+	"amber":  {0xFF, 0xC1, 0x07, 0xff},
+	"orange": {0xFF, 0x98, 0x00, 0xff},
+	"brown":  {0x79, 0x55, 0x48, 0xff},
+	"grey":   {0x9E, 0x9E, 0x9E, 0xff},
+	"white":  {0x00, 0x00, 0x00, 0xff},
+	"black":  {0xFF, 0xFF, 0xFF, 0xff},
+}
 
-	for colorName, colorRGBA := range colornames.Map {
+func deduplicate(s []string) []string {
+	seen := make(map[string]struct{}, len(s))
+	j := 0
+	for _, v := range s {
+		if _, ok := seen[v]; ok {
+			continue
+		}
+		seen[v] = struct{}{}
+		s[j] = v
+		j++
+	}
+	return s[:j]
+}
+
+func getColorNames(actualColor colorful.Color) (result []string) {
+	var maxDistance = 0.27
+
+	for colorName, colorRGBA := range colorMap {
 		colorColorful, _ := colorful.MakeColor(colorRGBA)
-		currentDistance := colorColorful.DistanceRgb(actualColor)
+		currentDistance := colorColorful.DistanceLab(actualColor)
 
 		if maxDistance >= currentDistance {
-			result = append(result, colorName)
+			result = append(result, fmt.Sprintf("%s", colorName))
 		}
 	}
 
@@ -32,21 +67,30 @@ func (m *MediaFile) GetColors() (colors []string, vibrantHex string, mutedHex st
 	defer file.Close()
 
 	decodedImage, _, _ := image.Decode(file)
+
 	palette := vibrant.NewPaletteBuilder(decodedImage).Generate()
 
 	if vibrantSwatch := palette.VibrantSwatch(); vibrantSwatch != nil {
 		color, _ := colorful.MakeColor(vibrantSwatch.Color())
-		colors = append(colors, getColorNames(color)...)
 		vibrantHex = color.Hex()
 	}
 
 	if mutedSwatch := palette.MutedSwatch(); mutedSwatch != nil {
 		color, _ := colorful.MakeColor(mutedSwatch.Color())
-		colors = append(colors, getColorNames(color)...)
 		mutedHex = color.Hex()
 	}
 
-	sort.Strings(colors)
+	centroids, err := prominentcolor.KmeansWithAll(5, decodedImage, prominentcolor.ArgumentDefault|prominentcolor.ArgumentNoCropping, prominentcolor.DefaultSize, prominentcolor.GetDefaultMasks())
+	if err == nil {
+		for _, centroid := range centroids {
+			colorfulColor, _ := colorful.MakeColor(color.RGBA{R: uint8(centroid.Color.R), G: uint8(centroid.Color.G), B: uint8(centroid.Color.B), A: 0xff})
+			colors = append(colors, getColorNames(colorfulColor)...)
+		}
+		colors = deduplicate(colors)
+		sort.Strings(colors)
+	} else {
+		log.Printf("Unable to detect most dominent color in image: %s", err)
+	}
 
 	return colors, vibrantHex, mutedHex
 }
--- internal/photoprism/colors_test.go
+++ internal/photoprism/colors_test.go
@@ -20,6 +20,20 @@ func TestMediaFile_GetColors(t *testing.T) {
 		assert.IsType(t, []string{}, names)
 		assert.Equal(t, "#e0ed21", vibrantHex)
 		assert.Equal(t, "#977d67", mutedHex)
+		assert.Equal(t, []string([]string{"black", "brown", "grey", "white"}), names);
+	} else {
+		t.Error(err)
+	}
+
+	if mediaFile2, err := NewMediaFile(conf.ImportPath() + "/ape.jpeg"); err == nil {
+		names, vibrantHex, mutedHex := mediaFile2.GetColors()
+
+		t.Log(names, vibrantHex, mutedHex)
+
+		assert.IsType(t, []string{}, names)
+		assert.Equal(t, "#97c84a", vibrantHex)
+		assert.Equal(t, "#6c9a68", mutedHex)
+		assert.Equal(t, []string([]string{"grey", "teal", "white"}), names);
 	} else {
 		t.Error(err)
 	}
