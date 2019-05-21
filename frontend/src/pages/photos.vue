493df991333112074470d54d4fe14caaf0dc17e5
Michael Mayer <michael@liquidbytes.net>
2019-05-21 02:47:48 +0200

    Frontend: Code clean-up on photos page #15

--- frontend/src/pages/photos.vue
+++ frontend/src/pages/photos.vue
@@ -17,15 +17,15 @@
 
                 <v-spacer></v-spacer>
 
-                <v-btn icon @click="advandedSearch = !advandedSearch" id="advancedMenu">
-                    <v-icon>{{ advandedSearch ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}</v-icon>
+                <v-btn icon @click="searchExpanded = !searchExpanded" id="advancedMenu">
+                    <v-icon>{{ searchExpanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down' }}</v-icon>
                 </v-btn>
             </v-toolbar>
 
             <v-card class="pt-1"
                     flat
                     color="blue-grey lighten-5"
-                    v-show="advandedSearch">
+                    v-show="searchExpanded">
                 <v-card-text>
                     <v-layout row wrap>
                         <v-flex xs12 sm6 md3 pa-2 id="countriesFlex">
@@ -108,6 +108,7 @@
                         small
                         color="deep-purple lighten-2"
                         @click.stop="batchLike()"
+                        v-if="selected.length > 0"
                 >
                     <v-icon>favorite</v-icon>
                 </v-btn>
@@ -115,8 +116,8 @@
                         fab
                         dark
                         small
-                        @click.stop="dialog = true"
                         color="cyan accent-4"
+                        @click.stop="batchTag()"
                 >
                     <v-icon>label</v-icon>
                 </v-btn>
@@ -125,6 +126,7 @@
                         dark
                         small
                         color="teal accent-4"
+                        @click.stop="batchDownload()"
                 >
                     <v-icon>save</v-icon>
                 </v-btn>
@@ -132,8 +134,8 @@
                         fab
                         dark
                         small
-                        @click.stop="dialog2 = true"
                         color="yellow accent-4"
+                        @click.stop="batchAlbum()"
                 >
                     <v-icon>create_new_folder</v-icon>
                 </v-btn>
@@ -144,6 +146,7 @@
                         small
                         color="delete"
                         @click.stop="batchDelete()"
+                        v-if="selected.length > 0"
                 >
                     <v-icon>delete</v-icon>
                 </v-btn>
@@ -158,179 +161,6 @@
             <p-photo-list v-if="view === 'list'" :photos="results" :selection="selected" :select="selectPhoto"
                           :open="openPhoto" :like="likePhoto"></p-photo-list>
         </v-container>
-
-        <v-dialog v-model="dialog" dark persistent max-width="600px">
-            <v-card dark>
-                <v-card-title>
-                    <span class="headline">Edit tags</span>
-                </v-card-title>
-                <v-card-text>
-                    <p>13 photos selected</p>
-                    <h4>Add tags</h4>
-                    <v-spacer></v-spacer>
-                    <v-combobox
-                            v-model="model"
-                            :hide-no-data="!search"
-                            :items="items"
-                            :search-input.sync="search"
-                            hide-selected
-                            label="Add tags"
-                            multiple
-                            small-chips
-                            solo
-                    >
-                        <template v-slot:no-data>
-                            <v-list-tile>
-                                <span class="subheading">Create</span>
-                                <v-chip
-                                        color="blue"
-                                        label
-                                        small
-                                >
-                                    search
-                                </v-chip>
-                            </v-list-tile>
-                        </template>
-                        <template v-slot:selection="{ item, parent, selected }">
-                            <v-chip
-                                    v-if="item === Object(item)"
-                                    color="green"
-                                    :selected="selected"
-                                    label
-                                    small
-                            >
-        <span class="pr-2">
-          item text
-        </span>
-                                <v-icon
-                                        small
-                                        @click="parent.selectItem(item)"
-                                >close
-                                </v-icon>
-                            </v-chip>
-                        </template>
-                        <template v-slot:item="{ index, item }">
-                            <v-list-tile-content>
-                                <v-text-field
-                                        v-if="editing === item"
-                                        v-model="editing.text"
-                                        autofocus
-                                        flat
-                                        background-color="transparent"
-                                        hide-details
-                                        solo
-                                        @keyup.enter="edit(index, item)"
-                                ></v-text-field>
-                                <v-chip
-                                        v-else
-                                        color="red"
-                                        dark
-                                        label
-                                        small
-                                >
-                                    item text
-                                </v-chip>
-                            </v-list-tile-content>
-                        </template>
-                    </v-combobox>
-                    <h4>Remove tags</h4>
-                    <v-combobox
-                            v-model="model"
-                            :hide-no-data="!search"
-                            :items="items"
-                            :search-input.sync="search"
-                            hide-selected
-                            label="Remove tags"
-                            multiple
-                            small-chips
-                            solo
-                    >
-                        <template v-slot:no-data>
-                            <v-list-tile>
-                                <span class="subheading">Create</span>
-                                <v-chip
-                                        color="blue"
-                                        label
-                                        small
-                                >
-                                    search
-                                </v-chip>
-                            </v-list-tile>
-                        </template>
-                        <template v-slot:selection="{ item, parent, selected }">
-                            <v-chip
-                                    v-if="item === Object(item)"
-                                    color="green"
-                                    :selected="selected"
-                                    label
-                                    small
-                            >
-        <span class="pr-2">
-          item text
-        </span>
-                                <v-icon
-                                        small
-                                        @click="parent.selectItem(item)"
-                                >close
-                                </v-icon>
-                            </v-chip>
-                        </template>
-                        <template v-slot:item="{ index, item }">
-                            <v-list-tile-content>
-                                <v-text-field
-                                        v-if="editing === item"
-                                        v-model="editing.text"
-                                        autofocus
-                                        flat
-                                        background-color="transparent"
-                                        hide-details
-                                        solo
-                                        @keyup.enter="edit(index, item)"
-                                ></v-text-field>
-                                <v-chip
-                                        v-else
-                                        color="red"
-                                        dark
-                                        label
-                                        small
-                                >
-                                    item text
-                                </v-chip>
-                            </v-list-tile-content>
-                        </template>
-                    </v-combobox>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="success" flat @click="dialog = false">Cancel</v-btn>
-                    <v-btn color="success" flat @click="dialog = false">Apply</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialog2" dark persistent max-width="600px">
-            <v-card dark>
-                <v-card-title>
-                    <span class="headline">Add to album</span>
-                </v-card-title>
-                <v-card-text>
-                    <template>
-                        <form>
-                            13 photos selected
-                            <v-select
-                                    v-model="select"
-                                    :items="items2"
-                                    label="Album"
-                            ></v-select>
-                        </form>
-                    </template>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="success" flat @click="dialog2 = false">Cancel</v-btn>
-                    <v-btn color="success" flat @click="dialog2 = false">apply</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
     </div>
 </template>
 
@@ -354,8 +184,14 @@
             }].concat(this.$config.getValue('countries'));
 
             return {
-                'advandedSearch': false,
+                'searchExpanded': false,
+                'loadMoreDisabled': true,
+                'menuVisible': false,
                 'results': [],
+                'selected': [],
+                'view': view,
+                'pageSize': 60,
+                'offset': 0,
                 'filter': {
                     country: country,
                     camera: camera,
@@ -387,50 +223,27 @@
                         {value: 'imported', text: 'Recently imported'},
                     ],
                 },
-                'view': view,
-                'pageSize': 60,
-                'offset': 0,
-                'loadMoreDisabled': true,
-                'menuVisible': false,
-                'submitTimeout': false,
-                'selected': [],
-                'dialog': false,
-                'search': null,
-                'activator': null,
-                'attach': null,
-                'colors': ['green', 'purple', 'indigo', 'primary', 'success', 'orange'],
-                'editing': null,
-                'index': -1,
-                'items': [
-                    {header: 'Select a tag or create one'},
-                    {text: 'Cat', color: 'primary'},
-                    {text: 'Sun', color: 'red'},
-                    {text: 'Dog', color: 'primary'},
-                    {text: 'Holiday', color: 'primary'},
-                    {text: 'Tiger', color: 'primary'},
-                    {text: 'Soup', color: 'primary'},
-                    {text: 'Night', color: 'primary'},
-                    {text: 'Table', color: 'primary'},
-                    {text: 'Apple', color: 'primary'},
-                    {text: 'Frog', color: 'primary'},
-                ],
-                'nonce': 1,
-                'menu': false,
-                'model': [],
-                'dialog2': false,
-                'select': null,
-                'items2': [
-                    'South Africa',
-                    'Cats & Dogs',
-                    'Christmas 2018',
-                ],
             };
         },
         methods: {
             batchLike() {
+                this.$alert.warning("Not implemented yet");
                 this.menuVisible = false;
             },
             batchDelete() {
+                this.$alert.warning("Not implemented yet");
+                this.menuVisible = false;
+            },
+            batchTag() {
+                this.$alert.warning("Not implemented yet");
+                this.menuVisible = false;
+            },
+            batchAlbum() {
+                this.$alert.warning("Not implemented yet");
+                this.menuVisible = false;
+            },
+            batchDownload() {
+                this.$alert.warning("Not implemented yet");
                 this.menuVisible = false;
             },
             clearSelection() {
