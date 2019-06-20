3d929902153656ed1479d57771b67c801c8ae323
Theresa Gresch <theresagresch@gmail.com>
2019-06-20 14:38:22 +0200

    Improve test for liking and disliking photos

--- frontend/tests/acceptance/favorites.js
+++ frontend/tests/acceptance/favorites.js
@@ -3,14 +3,34 @@ import testcafeconfig from './testcafeconfig';
 import Page from "./page-model";
 
 fixture`Test favorites page`
-    .page `localhost:2342/photos`;
+    .page `localhost:2342/favorites`;
 
 const page = new Page();
 
-test('See favorites', async t => {
+test('Like photo', async t => {
+
+    const FavoritesCount = await Selector('button.p-photo-like').count;
     await t
-        .hover(Selector('div[class="v-image__image v-image__image--cover"]').nth(0))
+        .navigateTo("../photos")
+        .hover(Selector('div[class="v-image__image v-image__image--cover"]').nth(5))
         .click(Selector('button.p-photo-like'))
-        .navigateTo("../favorites")
+        .navigateTo("../favorites");
+
+    const FavoritesCountAfterLike = await Selector('button.p-photo-like').count;
+    await t
+        .expect(FavoritesCountAfterLike).eql(FavoritesCount + 1)
         .expect(Selector('div.v-image__image').visible).ok();
-});
+}),
+
+    test('Dislike photo', async t => {
+
+        const FavoritesCount = await Selector('button.p-photo-like').count;
+        await t
+            .hover(Selector('div[class="v-image__image v-image__image--cover"]').nth(0))
+            .click(Selector('button.p-photo-like'))
+            .navigateTo("../favorites");
+
+        const FavoritesCountAfterDislike = await Selector('button.p-photo-like').count;
+        await t
+            .expect(FavoritesCountAfterDislike).eql(FavoritesCount - 1);
+    });
