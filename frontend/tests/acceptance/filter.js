f1bfbf224e8d027f581e238c79eb26dd3810af15
Theresa Gresch <theresagresch@gmail.com>
2019-06-20 14:08:42 +0200

    Improve filter test by adding check for expected response status code

--- frontend/tests/acceptance/filter.js
+++ frontend/tests/acceptance/filter.js
@@ -1,9 +1,16 @@
 import { Selector } from 'testcafe';
 import testcafeconfig from './testcafeconfig';
 import Page from './page-model';
+import { RequestLogger } from 'testcafe';
+
+const logger = RequestLogger( /http:\/\/localhost:2342\/api\/v1\/photos*/ , {
+    logResponseHeaders: true,
+    logResponseBody:    true
+});
 
 fixture`Use filters`
-    .page`${testcafeconfig.url}`;
+    .page`${testcafeconfig.url}`
+    .requestHooks(logger);
 
 const page = new Page();
 
@@ -11,22 +18,25 @@ test('Test camera filter', async t => {
     await t
         .click('#advancedMenu');
         await page.setFilter('camera', 'iPhone 6');
-        await page.setFilter('view', 'Details');
     await t
-        .expect(Selector('div.v-image__image').visible).ok()
-        .expect(Selector('div.caption').visible).ok()
+        .expect(logger.requests[1].response.statusCode).eql(200)
+        .expect(Selector('div.v-image__image').visible).ok();
 }),
 test('Test time filter', async t => {
     await t
         .click('#advancedMenu');
     await page.setFilter('time', 'Oldest');
     await t
-        .expect(Selector('div.v-image__image').visible).ok()
+        .expect(logger.requests[1].response.statusCode).eql(200)
+        .expect(logger.requests[1].request.url).contains('order=oldest')
+        .expect(Selector('div.v-image__image').visible).ok();
 }),
     test('Test countries filter', async t => {
         await t
             .click('#advancedMenu');
         await page.setFilter('countries', 'Cuba');
         await t
-            .expect(Selector('div.v-image__image').visible).ok()
+            .expect(logger.requests[1].response.statusCode).eql(200)
+            .expect(logger.requests[1].request.url).contains('country=cu')
+            .expect(Selector('div.v-image__image').visible).ok();
     },);
--- frontend/tests/acceptance/search.js
+++ frontend/tests/acceptance/search.js
@@ -14,7 +14,6 @@ fixture`Search photos`
 
 const page = new Page();
 
-
 test('Test search object', async t => {
     await page.search('label:cat');
     await t
