56ed571b42d5818d16849138e6fbcca9a4bdadb5
Theresa Gresch <theresagresch@gmail.com>
2019-06-20 13:46:58 +0200

    Improve search test by adding check for expected response status code

--- frontend/tests/acceptance/search.js
+++ frontend/tests/acceptance/search.js
@@ -1,19 +1,29 @@
 import { Selector } from 'testcafe';
 import testcafeconfig from './testcafeconfig';
 import Page from "./page-model";
+import { RequestLogger } from 'testcafe';
+
+const logger = RequestLogger( /http:\/\/localhost:2342\/api\/v1\/photos*/ , {
+    logResponseHeaders: true,
+    logResponseBody:    true
+});
 
 fixture`Search photos`
-    .page`${testcafeconfig.url}`;
+    .page`${testcafeconfig.url}`
+    .requestHooks(logger);
 
 const page = new Page();
 
+
 test('Test search object', async t => {
-    await page.search('cat');
+    await page.search('label:cat');
     await t
+        .expect(logger.requests[0].response.statusCode).eql(200)
         .expect(Selector('div.v-image__image').visible).ok();
 }),
 test('Test search color', async t => {
     await page.search('color:pink');
     await t
+        .expect(logger.requests[1].response.statusCode).eql(200)
         .expect(Selector('div.v-image__image').visible).ok();
 });
