11621cd4dda9f86d8d3757e4bb47fdafd8f97b4b
FBY <fdyvoire@gmail.com>
2019-06-21 15:47:45 +0100

    test firstorcreate

--- internal/api/photos_test.go
+++ internal/api/photos_test.go
@@ -41,13 +41,10 @@ func TestLikePhoto(t *testing.T) {
 	
 	t.Run("Like Existing record", func(t *testing.T) {
 		// Ensure that at least one record exist in the test database
-		photo1 := models.Photo{
-			Model: models.Model{ID: 1},
-			TakenAt: time.Date(2019, time.June, 6, 21, 0, 0, 0, time.UTC), // required as default value 0000-00-00 not accepted by the SQL database
-		}
-		if ctx.Db().NewRecord(photo1){
-			ctx.Db().Create(&photo1)
-		}
+		
+		photoFirst := ctx.Db().FirstOrCreate(models.Photo)
+		idFirst := photoFirst.ID
+		t.Print(idfirst)
 		result := PerformRequest(app, "POST", "/api/v1/photos/1/like")
 		assert.Equal(t, http.StatusOK, result.Code)
 	})
