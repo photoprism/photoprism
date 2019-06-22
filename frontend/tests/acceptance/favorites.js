3797830165366498c983c2d6d7247dc10e6b6cb3
Theresa Gresch <theresagresch@gmail.com>
2019-06-22 15:11:45 +0200

    Improve test for favorites

--- frontend/tests/acceptance/favorites.js
+++ frontend/tests/acceptance/favorites.js
@@ -1,20 +1,34 @@
 import { Selector } from 'testcafe';
 import testcafeconfig from './testcafeconfig';
 import Page from "./page-model";
+import { RequestLogger } from 'testcafe';
+
+const logger = RequestLogger( /http:\/\/localhost:2342\/api\/v1\/photos*/ , {
+    logResponseHeaders: true,
+    logResponseBody:    true
+});
 
 fixture`Test favorites page`
-    .page `localhost:2342/favorites`;
+    .page `localhost:2342/favorites`
+    .requestHooks(logger);
 
 const page = new Page();
 
 test('Like photo', async t => {
 
     const FavoritesCount = await Selector('button.p-photo-like').count;
+    logger.clear();
     await t
         .navigateTo("../photos")
+    const request = await logger.requests[0];
+    await t
         .hover(Selector('div[class="v-image__image v-image__image--cover"]').nth(5))
         .click(Selector('button.p-photo-like'))
+    logger.clear();
+    await t
         .navigateTo("../favorites");
+    const request2 = await logger.requests[1];
+    logger.clear();
 
     const FavoritesCountAfterLike = await Selector('button.p-photo-like').count;
     await t
@@ -22,15 +36,18 @@ test('Like photo', async t => {
         .expect(Selector('div.v-image__image').visible).ok();
 }),
 
-    test('Dislike photo', async t => {
+test('Dislike photo', async t => {
 
-        const FavoritesCount = await Selector('button.p-photo-like').count;
-        await t
-            .hover(Selector('div[class="v-image__image v-image__image--cover"]').nth(0))
-            .click(Selector('button.p-photo-like'))
-            .navigateTo("../favorites");
+    const FavoritesCount = await Selector('button.p-photo-like').count;
+    await t
+        .hover(Selector('div[class="v-image__image v-image__image--cover"]').nth(0))
+        .click(Selector('button.p-photo-like'))
+    logger.clear();
+    await t
+        .navigateTo("../favorites");
+    const request3 = await logger.requests[0];
 
-        const FavoritesCountAfterDislike = await Selector('button.p-photo-like').count;
-        await t
-            .expect(FavoritesCountAfterDislike).eql(FavoritesCount - 1);
-    });
+    const FavoritesCountAfterDislike = await Selector('button.p-photo-like').count;
+    await t
+        .expect(FavoritesCountAfterDislike).eql(FavoritesCount - 1);
+});
