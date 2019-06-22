dec718aa821bc0dbe56b03dc202776cc93efcc1f
Theresa Gresch <theresagresch@gmail.com>
2019-06-22 15:14:47 +0200

    Improve test for search

--- frontend/tests/acceptance/search.js
+++ frontend/tests/acceptance/search.js
@@ -17,12 +17,14 @@ const page = new Page();
 
 test('Test search object', async t => {
     await page.search('label:cat');
+    const request3 = await logger.requests[1];
     await t
         .expect(logger.requests[0].response.statusCode).eql(200)
         .expect(Selector('div.v-image__image').visible).ok();
 }),
 test('Test search color', async t => {
     await page.search('color:pink');
+    const request2 = await logger.requests[8];
     await t
         .expect(logger.requests[1].response.statusCode).eql(200)
         .expect(Selector('div.v-image__image').visible).ok();
