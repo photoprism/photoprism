562341e19728abed5a6e07baee34e001f66e5d5a
Theresa Gresch <theresagresch@gmail.com>
2019-07-05 12:32:06 +0200

    Add logger to util.go

--- internal/util/util.go
+++ internal/util/util.go
@@ -6,3 +6,11 @@ Additional information can be found in our Developer Guide:
 https://github.com/photoprism/photoprism/wiki
 */
 package util
+
+import "github.com/sirupsen/logrus"
+
+var log *logrus.Logger
+
+func init() {
+	log = logrus.StandardLogger()
+}
