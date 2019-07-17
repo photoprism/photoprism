429f226c5c2f026f6e0100223486a63303e28b08
Theresa Gresch <theresagresch@gmail.com>
2019-07-17 13:22:55 +0200

    Add test for hash function

--- internal/util/hash_test.go
+++ internal/util/hash_test.go
@@ -7,6 +7,12 @@ import (
 )
 
 func TestHash(t *testing.T) {
-	hash := Hash("testdata/test.jpg")
-	assert.Equal(t, "516cb1fefbfd9fa66f1db50b94503a480cee30db", hash)
+	t.Run("existing image", func(t *testing.T) {
+		hash := Hash("testdata/test.jpg")
+		assert.Equal(t, "516cb1fefbfd9fa66f1db50b94503a480cee30db", hash)
+	})
+	t.Run("not existing image", func(t *testing.T) {
+		hash := Hash("testdata/xxx.jpg")
+		assert.Equal(t, "", hash)
+	})
 }
