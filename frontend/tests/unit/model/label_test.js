4a3f08450a6ea96aff40dd64868c552d82ac127d
Theresa Gresch <theresagresch@gmail.com>
2019-07-28 18:40:33 +0200

    Add tests for labels.js

--- frontend/tests/unit/model/label_test.js
+++ frontend/tests/unit/model/label_test.js
@@ -1,7 +1,16 @@
 import assert from "assert";
 import Label from "model/label";
+import Api from "common/api";
+import MockAdapter from "axios-mock-adapter";
 
-describe.only("model/label", () => {
+const mock = new MockAdapter(Api);
+
+const postLikeEntity = [
+    {id: 5, LabelFavorite: true},
+];
+mock.onPost("foo").reply(201, postLikeEntity);
+
+describe("model/label", () => {
     it("should get label entity name",  () => {
         const values = {id: 5, LabelName: "Black Cat", LabelSlug: "black-cat"};
         const label = new Label(values);
@@ -34,8 +43,40 @@ describe.only("model/label", () => {
         const values = {id: 5, LabelName: "Black Cat", LabelSlug: "black-cat"};
         const label = new Label(values);
         const result = label.getThumbnailSrcset("");
-        console.log(result);
         assert.equal(result, "/api/v1/labels/black-cat/thumbnail/fit_720 720w, /api/v1/labels/black-cat/thumbnail/fit_1280 1280w, /api/v1/labels/black-cat/thumbnail/fit_1920 1920w, /api/v1/labels/black-cat/thumbnail/fit_2560 2560w, /api/v1/labels/black-cat/thumbnail/fit_3840 3840w");
     });
+
+    it("should get thumbnail sizes",  () => {
+        const values = {id: 5, LabelName: "Black Cat", LabelSlug: "black-cat"};
+        const label = new Label(values);
+        const result = label.getThumbnailSizes();
+        assert.equal(result, "(min-width: 2560px) 3840px, (min-width: 1920px) 2560px, (min-width: 1280px) 1920px, (min-width: 720px) 1280px, 720px");
+    });
+
+    it("should get model name",  () => {
+        const result = Label.getModelName();
+        assert.equal(result, "Label");
+    });
+
+    it("should get collection resource",  () => {
+        const result = Label.getCollectionResource();
+        assert.equal(result, "labels");
+    });
+
+    it("should toggle like",  () => {
+        Api.post('foo', postLikeEntity).then(
+            (response) => {
+                console.log(response);
+                assert.equal(201, response.status);
+                assert.deepEqual(postLikeEntity, response.data);
+                done();
+            }
+        ).catch(
+            (error) => {
+                done(error);
+            }
+        );
+    });
+
 
 });
