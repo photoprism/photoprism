c4006d891078395169b5482b14de4d9d2d912335
Theresa Gresch <theresagresch@gmail.com>
2019-08-09 10:24:20 +0200

    Add tests for user model

--- frontend/tests/unit/model/user_test.js
+++ frontend/tests/unit/model/user_test.js
@@ -1,7 +1,7 @@
 import assert from "assert";
 import User from "model/user";
 
-describe.only("model/user", () => {
+describe("model/user", () => {
     it("should get entity name",  () => {
         const values = {ID: 5, userFirstName: "Max", userLastName: "Last", userEmail: "test@test.com", userRole: "admin"};
         const user = new User(values);
@@ -16,4 +16,13 @@ describe.only("model/user", () => {
         assert.equal(result, 5);
     });
 
+    it("should get model name",  () => {
+        const result = User.getModelName();
+        assert.equal(result, "User");
+    });
+
+    it("should get collection resource",  () => {
+        const result = User.getCollectionResource();
+        assert.equal(result, "users");
+    });
 });
