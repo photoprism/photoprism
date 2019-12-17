b5336eebb0f7c9818d714695e16968083325c1a6
Theresa Gresch <theresagresch@gmail.com>
2019-12-17 18:27:48 +0100

    Add test for entity/photo_keyword.go

---
+++ internal/entity/photo_keyword_test.go
@@ -0,0 +1,22 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewPhotoKeyword(t *testing.T) {
+	t.Run("new keyword", func(t *testing.T) {
+		m := NewPhotoKeyword(uint(3), uint(8))
+		assert.Equal(t, uint(3), m.PhotoID)
+		assert.Equal(t, uint(8), m.KeywordID)
+	})
+}
+
+func TestPhotoKeyword_TableName(t *testing.T) {
+	photoKeyword := &PhotoKeyword{}
+	tableName := photoKeyword.TableName()
+
+	assert.Equal(t, "photos_keywords", tableName)
+}
