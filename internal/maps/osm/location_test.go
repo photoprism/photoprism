c7bb86200835e90a844b14f6b56e4ef0724cf2b6
Theresa Gresch <theresagresch@gmail.com>
2019-12-22 21:38:55 +0100

    Add test for osm/location

--- internal/maps/osm/location_test.go
+++ internal/maps/osm/location_test.go
@@ -77,3 +77,97 @@ func TestFindLocation(t *testing.T) {
 		assert.False(t, l.Cached)
 	})
 }
+
+func TestOSM_State(t *testing.T) {
+	t.Run("Berlin", func(t *testing.T) {
+
+		a := Address{CountryCode: "de", City: "Berlin", State: "Berlin", HouseNumber: "63", Suburb: "Neukölln"}
+		l := &Location{LocCategory: "natural", LocLat: "52.5208", LocLng: "13.40953", LocTitle: "Nice title", LocType: "hill", LocDisplayName: "dipslay name", Address: a}
+		assert.Equal(t, "Berlin", l.State())
+	})
+}
+
+func TestOSM_City(t *testing.T) {
+	t.Run("Berlin", func(t *testing.T) {
+
+		a := Address{CountryCode: "de", City: "Berlin", State: "Berlin", HouseNumber: "63", Suburb: "Neukölln", Town: "Hamburg", Village: "Köln", County: "Wiesbaden"}
+		l := &Location{LocCategory: "natural", LocLat: "52.5208", LocLng: "13.40953", LocTitle: "Nice title", LocType: "hill", LocDisplayName: "dipslay name", Address: a}
+		assert.Equal(t, "Berlin", l.City())
+	})
+	t.Run("Hamburg", func(t *testing.T) {
+
+		a := Address{CountryCode: "de", City: "", State: "Berlin", HouseNumber: "63", Suburb: "Neukölln", Town: "Hamburg", Village: "Köln", County: "Wiesbaden"}
+		l := &Location{LocCategory: "natural", LocLat: "52.5208", LocLng: "13.40953", LocTitle: "Nice title", LocType: "hill", LocDisplayName: "dipslay name", Address: a}
+		assert.Equal(t, "Hamburg", l.City())
+	})
+	t.Run("Köln", func(t *testing.T) {
+
+		a := Address{CountryCode: "de", City: "", State: "Berlin", HouseNumber: "63", Suburb: "Neukölln", Town: "", Village: "Köln", County: "Wiesbaden"}
+		l := &Location{LocCategory: "natural", LocLat: "52.5208", LocLng: "13.40953", LocTitle: "Nice title", LocType: "hill", LocDisplayName: "dipslay name", Address: a}
+		assert.Equal(t, "Köln", l.City())
+	})
+	t.Run("Wiesbaden", func(t *testing.T) {
+
+		a := Address{CountryCode: "de", City: "", State: "Berlin", HouseNumber: "63", Suburb: "Neukölln", Town: "", Village: "", County: "Wiesbaden"}
+		l := &Location{LocCategory: "natural", LocLat: "52.5208", LocLng: "13.40953", LocTitle: "Nice title", LocType: "hill", LocDisplayName: "dipslay name", Address: a}
+		assert.Equal(t, "Wiesbaden", l.City())
+	})
+	t.Run("Frankfurt", func(t *testing.T) {
+
+		a := Address{CountryCode: "de", City: "", State: "Frankfurt", HouseNumber: "63", Suburb: "Neukölln", Town: "", Village: "", County: ""}
+		l := &Location{LocCategory: "natural", LocLat: "52.5208", LocLng: "13.40953", LocTitle: "Nice title", LocType: "hill", LocDisplayName: "dipslay name", Address: a}
+		assert.Equal(t, "Frankfurt", l.City())
+	})
+}
+
+func TestOSM_Suburb(t *testing.T) {
+	t.Run("Neukölln", func(t *testing.T) {
+
+		a := Address{CountryCode: "de", City: "Berlin", State: "Berlin", HouseNumber: "63", Suburb: "Neukölln"}
+		l := &Location{LocCategory: "natural", LocLat: "52.5208", LocLng: "13.40953", LocTitle: "Nice title", LocType: "hill", LocDisplayName: "dipslay name", Address: a}
+		assert.Equal(t, "Neukölln", l.Suburb())
+	})
+}
+
+func TestOSM_CountryCode(t *testing.T) {
+	t.Run("de", func(t *testing.T) {
+
+		a := Address{CountryCode: "de", City: "Berlin", State: "Berlin", HouseNumber: "63", Suburb: "Neukölln"}
+		l := &Location{LocCategory: "natural", LocLat: "52.5208", LocLng: "13.40953", LocTitle: "Nice title", LocType: "hill", LocDisplayName: "dipslay name", Address: a}
+		assert.Equal(t, "de", l.CountryCode())
+	})
+}
+
+func TestOSM_Latitude(t *testing.T) {
+	t.Run("52.5208", func(t *testing.T) {
+
+		a := Address{CountryCode: "de", City: "Berlin", State: "Berlin", HouseNumber: "63", Suburb: "Neukölln"}
+		l := &Location{LocCategory: "natural", LocLat: "52.5208", LocLng: "13.40953", LocTitle: "Nice title", LocType: "hill", LocDisplayName: "dipslay name", Address: a}
+		assert.Equal(t, 52.5208, l.Latitude())
+	})
+}
+
+func TestOSM_Longitude(t *testing.T) {
+	t.Run("13.40953", func(t *testing.T) {
+
+		a := Address{CountryCode: "de", City: "Berlin", State: "Berlin", HouseNumber: "63", Suburb: "Neukölln"}
+		l := &Location{LocCategory: "natural", LocLat: "52.5208", LocLng: "13.40953", LocTitle: "Nice title", LocType: "hill", LocDisplayName: "dipslay name", Address: a}
+		assert.Equal(t, 13.40953, l.Longitude())
+	})
+}
+
+func TestOSM_Keywords(t *testing.T) {
+	t.Run("cat", func(t *testing.T) {
+
+		a := Address{CountryCode: "de", City: "Berlin", State: "Berlin", HouseNumber: "63", Suburb: "Neukölln"}
+		l := &Location{LocCategory: "natural", LocLat: "52.5208", LocLng: "13.40953", LocTitle: "Nice title", LocType: "hill", LocDisplayName: "cat", Address: a}
+		assert.Equal(t, []string{"cat"}, l.Keywords())
+	})
+}
+
+func TestOSM_Source(t *testing.T) {
+
+	a := Address{CountryCode: "de", City: "Berlin", State: "Berlin", HouseNumber: "63", Suburb: "Neukölln"}
+	l := &Location{LocCategory: "natural", LocLat: "52.5208", LocLng: "13.40953", LocTitle: "Nice title", LocType: "hill", LocDisplayName: "cat", Address: a}
+	assert.Equal(t, "osm", l.Source())
+}
