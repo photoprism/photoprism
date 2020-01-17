673ba884bd56ce6a3745b063f8dd0ab1a088bff6
Theresa Gresch <theresagresch@gmail.com>
2020-01-17 11:47:07 +0100

    feature/link-statically-against-TF #83 Add NewPlace() function

--- internal/maps/places/place.go
+++ internal/maps/places/place.go
@@ -8,3 +8,15 @@ type Place struct {
 	LocState   string `json:"state"`
 	LocCountry string `json:"country"`
 }
+
+func NewPlace(id string, label string, city string, state string, country string) Place {
+	result := Place{
+		PlaceID:    id,
+		LocLabel:   label,
+		LocCity:    city,
+		LocState:   state,
+		LocCountry: country,
+	}
+
+	return result
+}
