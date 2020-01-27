98c63755b25e6ab371caea099278756cc50ec8d3
Theresa Gresch <theresagresch@gmail.com>
2020-01-27 16:52:47 +0100

    Add test fixtures

--- assets/resources/examples/fixtures.sql
+++ assets/resources/examples/fixtures.sql
@@ -6,3 +6,10 @@ INSERT INTO cameras (id, camera_slug, camera_model, camera_make, camera_type, ca
 INSERT INTO cameras (id, camera_slug, camera_model, camera_make, camera_type, camera_owner, camera_description, camera_notes, created_at, updated_at, deleted_at) VALUES (6, 'apple-iphone-6', 'iPhone 6', 'Apple', '', '', '', '', '2020-01-06 02:06:42', '2020-01-06 02:06:42', null);
 INSERT INTO cameras (id, camera_slug, camera_model, camera_make, camera_type, camera_owner, camera_description, camera_notes, created_at, updated_at, deleted_at) VALUES (7, 'apple-iphone-7', 'iPhone 7', 'Apple', '', '', '', '', '2020-01-06 02:06:51', '2020-01-06 02:06:51', null);
 INSERT INTO countries (id, country_slug, country_name, country_description, country_notes, country_photo_id) VALUES ('de', 'germany', 'Germany', 'Country Description', 'Country Notes', 0);
+INSERT INTO albums (id, album_uuid, album_name, album_slug, album_favorite) VALUES ('2', '3', 'Christmas2030', 'christmas2030', 0);
+INSERT INTO albums (id, album_uuid, cover_uuid, album_name, album_slug, album_favorite) VALUES ('1', '4', '654', 'Holiday2030', 'holiday-2030', 1);
+INSERT INTO photos_albums (album_uuid, photo_uuid) VALUES ('4', '654');
+INSERT INTO files (id, photo_id, photo_uuid, file_name, file_primary) VALUES ('1', '1', '654', 'exampleFileName.jpg', 1);
+INSERT INTO photos (id, photo_uuid) VALUES ('1', '654');
+
+
