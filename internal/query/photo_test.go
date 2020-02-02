7cbdd31793e34cddb2c20a04d20d8ae5d25d7729
Theresa Gresch <theresagresch@gmail.com>
2020-02-02 13:03:08 +0100

    Test: Add tests to query/photo.go

--- internal/query/photo_test.go
+++ internal/query/photo_test.go
@@ -9,6 +9,78 @@ import (
 	"github.com/photoprism/photoprism/internal/form"
 )
 
+/*func TestRepo_Photos(t *testing.T) {
+	conf := config.TestConfig()
+
+	search := New(conf.OriginalsPath(), conf.Db())
+
+	t.Run("search with query", func(t *testing.T) {
+		query := form.NewPhotoSearch("Title:Reunion")
+		result, err := search.Photos(query)
+
+		t.Log(result)
+		t.Log(err)
+
+		assert.Nil(t, err)
+		assert.Equal(t, 2, len(result))
+		assert.Equal(t, "Cake", result[1].PhotoName)
+		assert.Equal(t, "COW", result[0].PhotoName)
+	})
+}*/
+
+func TestRepo_FindPhotoByID(t *testing.T) {
+	conf := config.TestConfig()
+
+	search := New(conf.OriginalsPath(), conf.Db())
+
+	t.Run("photo found", func(t *testing.T) {
+		result, err := search.FindPhotoByID(1)
+		assert.Nil(t, err)
+		assert.Equal(t, 2790, result.PhotoYear)
+	})
+
+	t.Run("no photo found", func(t *testing.T) {
+		result, err := search.FindPhotoByID(99999)
+		assert.Error(t, err, "record not found")
+		t.Log(result)
+	})
+}
+
+func TestRepo_FindPhotoByUUID(t *testing.T) {
+	conf := config.TestConfig()
+
+	search := New(conf.OriginalsPath(), conf.Db())
+
+	t.Run("photo found", func(t *testing.T) {
+		result, err := search.FindPhotoByUUID("659")
+		assert.Nil(t, err)
+		assert.Equal(t, "Reunion", result.PhotoTitle)
+	})
+
+	t.Run("no photo found", func(t *testing.T) {
+		result, err := search.FindPhotoByUUID("99999")
+		assert.Error(t, err, "record not found")
+		t.Log(result)
+	})
+}
+
+func TestRepo_PreloadPhotoByUUID(t *testing.T) {
+	conf := config.TestConfig()
+
+	search := New(conf.OriginalsPath(), conf.Db())
+
+	t.Run("photo found", func(t *testing.T) {
+		result, err := search.PreloadPhotoByUUID("659")
+		assert.Nil(t, err)
+		assert.Equal(t, "Reunion", result.PhotoTitle)
+	})
+
+	t.Run("no photo found", func(t *testing.T) {
+		result, err := search.PreloadPhotoByUUID("99999")
+		assert.Error(t, err, "record not found")
+		t.Log(result)
+	})
+}
 func TestSearch_Photos_Query(t *testing.T) {
 	conf := config.TestConfig()
 
