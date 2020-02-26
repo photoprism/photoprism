2a5d97f7013f98ada3090110e3a9241002d0370b
Paul Thiele <thielepaul@gmail.com>
2020-02-26 11:16:36 +0100

    Backend: Add API endpoint for getting files by sha1 hash

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -37,6 +37,7 @@ func registerRoutes(router *gin.Engine, conf *config.Config) {
 		api.AddPhotoLabel(v1, conf)
 		api.RemovePhotoLabel(v1, conf)
 		api.GetMomentsTime(v1, conf)
+		api.GetFile(v1, conf)
 
 		api.GetLabels(v1, conf)
 		api.UpdateLabel(v1, conf)
