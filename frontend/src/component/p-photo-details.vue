19b96fe016d2943d5f2f2675475ac634cf6e8c22
Michael Mayer <michael@liquidbytes.net>
2020-04-08 18:26:17 +0200

    Frontend: Change opacity in photo views
    Signed-off-by: Michael Mayer <michael@liquidbytes.net>

--- frontend/src/component/p-photo-details.vue
+++ frontend/src/component/p-photo-details.vue
@@ -45,7 +45,7 @@
 
                             <v-btn v-if="hover || selection.length > 0" :flat="!hover" :ripple="false"
                                    icon large absolute
-                                   :class="hover ? 'p-photo-select' : 'p-photo-select opacity-70'"
+                                   :class="hover ? 'p-photo-select' : 'p-photo-select opacity-80'"
                                    @click.shift.prevent="$clipboard.addRange(index, photos)"
                                    @click.exact.stop.prevent="$clipboard.toggle(photo)">
                                 <v-icon v-if="selection.length && $clipboard.has(photo)" color="white"
@@ -56,7 +56,7 @@
 
                             <v-btn v-if="hover || photo.PhotoFavorite" :flat="!hover" :ripple="false"
                                    icon large absolute
-                                   :class="hover ? 'p-photo-like' : 'p-photo-like opacity-70'"
+                                   :class="hover ? 'p-photo-like' : 'p-photo-like opacity-80'"
                                    @click.stop.prevent="photo.toggleLike()">
                                 <v-icon v-if="photo.PhotoFavorite" color="white" class="t-like t-on">favorite</v-icon>
                                 <v-icon v-else color="accent lighten-3" class="t-like t-off">favorite_border</v-icon>
