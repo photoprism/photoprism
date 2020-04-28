825e629bb9cd176ffc81337bd793c5c3ad3af3ac
Theresa Gresch <theresagresch@gmail.com>
2020-04-28 14:51:01 +0200

    Backend: Add tests for pkg/colors/luminance.go

---
+++ pkg/colors/luminance_test.go
@@ -0,0 +1,12 @@
+package colors
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLuminance_Hex(t *testing.T) {
+	lum := Luminance(1)
+	assert.Equal(t, "1", lum.Hex())
+
+}
