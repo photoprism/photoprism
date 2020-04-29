426e5a9f9c74714b45bd6264ccf9c3b527fbae43
Theresa Gresch <theresagresch@gmail.com>
2020-04-29 14:09:44 +0200

    Backend: Add tests to internal/photoprism

---
+++ internal/photoprism/import_options_test.go
@@ -0,0 +1,24 @@
+package photoprism
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestImportOptionsCopy(t *testing.T) {
+	result := ImportOptionsCopy("xxx")
+	assert.Equal(t, "xxx", result.Path)
+	assert.Equal(t, false, result.Move)
+	assert.Equal(t, false, result.RemoveDotFiles)
+	assert.Equal(t, false, result.RemoveExistingFiles)
+	assert.Equal(t, false, result.RemoveEmptyDirectories)
+}
+
+func TestImportOptionsMove(t *testing.T) {
+	result := ImportOptionsMove("xxx")
+	assert.Equal(t, "xxx", result.Path)
+	assert.Equal(t, true, result.Move)
+	assert.Equal(t, true, result.RemoveDotFiles)
+	assert.Equal(t, true, result.RemoveExistingFiles)
+	assert.Equal(t, true, result.RemoveEmptyDirectories)
+}
