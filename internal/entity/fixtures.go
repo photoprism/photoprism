434948d57ae103f2f65a25d8afdb8546f7742662
Theresa Gresch <theresagresch@gmail.com>
2020-04-30 17:26:36 +0200

    Backend: Add test fixtures for photos

--- internal/entity/fixtures.go
+++ internal/entity/fixtures.go
@@ -1,6 +1,15 @@
 package entity
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 // CreateTestFixtures inserts all known entities into the database for testing.
+func CreateTestFixtures(db *gorm.DB) {
+	CreateLabelFixtures(db)
+	CreateCameraFixtures(db)
+	CreateCountryFixtures(db)
+	CreatePhotoFixtures(db)
 func CreateTestFixtures() {
 	CreateLabelFixtures()
 	CreateAccountFixtures()
