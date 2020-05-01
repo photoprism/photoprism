0008f68b7195aa226767e3beac59734d14e0c974
Theresa Gresch <theresagresch@gmail.com>
2020-05-01 13:11:02 +0200

    Backend: Add fixtures for links

---
+++ internal/entity/link_fixtures.go
@@ -0,0 +1,26 @@
+package entity
+
+import "time"
+
+var date = time.Date(2050, 3, 6, 2, 6, 51, 0, time.UTC)
+
+var LinkFixtures = map[string]Link{
+	"1jxf3jfn2k": {
+		LinkToken:    "1jxf3jfn2k",
+		LinkPassword: "somepassword",
+		LinkExpires:  &date,
+		ShareUUID:    "4",
+		CanComment:   true,
+		CanEdit:      false,
+		CreatedAt:    time.Date(2020, 3, 6, 2, 6, 51, 0, time.UTC),
+		UpdatedAt:    time.Date(2020, 3, 28, 14, 6, 0, 0, time.UTC),
+		DeletedAt:    nil,
+	},
+}
+
+// CreateLinkFixtures inserts known entities into the database for testing.
+func CreateLinkFixtures() {
+	for _, entity := range LinkFixtures {
+		Db().Create(&entity)
+	}
+}
