0ff34b6f43c6e3443ce65044055c39194e8a3a6b
Michael Mayer <michael@liquidbytes.net>
2020-05-03 18:05:05 +0200

    Backend: Change error message prefix in thumb package
    Signed-off-by: Michael Mayer <michael@liquidbytes.net>

--- internal/thumb/jpeg.go
+++ internal/thumb/jpeg.go
@@ -10,14 +10,14 @@ func Jpeg(srcFilename, jpgFilename string) (img image.Image, err error) {
 	img, err = imaging.Open(srcFilename, imaging.AutoOrientation(true))
 
 	if err != nil {
-		log.Errorf("thumbs: can't open %s", srcFilename)
+		log.Errorf("resample: can't open %s", srcFilename)
 		return img, err
 	}
 
 	saveOption := imaging.JPEGQuality(JpegQuality)
 
 	if err = imaging.Save(img, jpgFilename, saveOption); err != nil {
-		log.Errorf("thumbs: failed to save %s", jpgFilename)
+		log.Errorf("resample: failed to save %s", jpgFilename)
 		return img, err
 	}
 
