73e0d294fff9c8db7bd923eadbb92a34bbd8c944
Theresa Gresch <theresagresch@gmail.com>
2020-05-04 14:40:58 +0200

    Backend: Add tests to internal/api

--- internal/api/download_test.go
+++ internal/api/download_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/tidwall/gjson"
 	"net/http"
 	"testing"
 
@@ -8,20 +9,20 @@ import (
 )
 
 func TestGetDownload(t *testing.T) {
-	t.Run("could not find original", func(t *testing.T) {
+	t.Run("download not existing file", func(t *testing.T) {
 		app, router, conf := NewApiTest()
 
 		GetDownload(router, conf)
 
-		result := PerformRequest(app, "GET", "/api/v1/download/123xxx")
-		assert.Equal(t, http.StatusNotFound, result.Code)
+		r := PerformRequest(app, "GET", "/api/v1/download/123xxx")
+		val := gjson.Get(r.Body.String(), "error")
+		assert.Equal(t, "record not found", val.String())
+		assert.Equal(t, http.StatusNotFound, r.Code)
 	})
-	t.Run("download existing not existing file", func(t *testing.T) {
+	t.Run("could not find original", func(t *testing.T) {
 		app, router, conf := NewApiTest()
-
-		DownloadAlbum(router, conf)
-
-		result := PerformRequest(app, "GET", "/api/v1/download/555")
-		assert.Equal(t, http.StatusNotFound, result.Code)
+		GetDownload(router, conf)
+		r := PerformRequest(app, "GET", "/api/v1/download/3cad9168fa6acc5c5c2965ddf6ec465ca42fd818")
+		assert.Equal(t, http.StatusNotFound, r.Code)
 	})
 }
--- internal/api/file_test.go
+++ internal/api/file_test.go
@@ -12,16 +12,37 @@ func TestGetFile(t *testing.T) {
 	t.Run("search for existing file", func(t *testing.T) {
 		app, router, ctx := NewApiTest()
 		GetFile(router, ctx)
-		result := PerformRequest(app, "GET", "/api/v1/files/2cad9168fa6acc5c5c2965ddf6ec465ca42fd818")
-		assert.Equal(t, http.StatusOK, result.Code)
+		r := PerformRequest(app, "GET", "/api/v1/files/2cad9168fa6acc5c5c2965ddf6ec465ca42fd818")
+		assert.Equal(t, http.StatusOK, r.Code)
 
-		val := gjson.Get(result.Body.String(), "FileName")
+		val := gjson.Get(r.Body.String(), "FileName")
 		assert.Equal(t, "exampleFileName.jpg", val.String())
 	})
 	t.Run("search for not existing file", func(t *testing.T) {
 		app, router, ctx := NewApiTest()
 		GetFile(router, ctx)
-		result := PerformRequest(app, "GET", "/api/v1/files/111")
-		assert.Equal(t, http.StatusNotFound, result.Code)
+		r := PerformRequest(app, "GET", "/api/v1/files/111")
+		assert.Equal(t, http.StatusNotFound, r.Code)
 	})
+}
+
+func TestLinkFile(t *testing.T) {
+	t.Run("album not found", func(t *testing.T) {
+		app, router, ctx := NewApiTest()
+		LinkFile(router, ctx)
+		r := PerformRequest(app, "POST", "/api/v1/files/3cad9168fa6acc5c5c2965ddf6ec465ca42fd818/link")
+		t.Log(r.Body.String())
+		assert.Equal(t, http.StatusNotFound, r.Code)
+		val := gjson.Get(r.Body.String(), "error")
+		assert.Equal(t, "Album not found", val.String())
+	})
+	t.Run("invalid request", func(t *testing.T) {
+		app, router, ctx := NewApiTest()
+		LinkFile(router, ctx)
+		r := PerformRequest(app, "POST", "/api/v1/files/ft9es39w45bnlqdw/link")
+		assert.Equal(t, http.StatusBadRequest, r.Code)
+		val := gjson.Get(r.Body.String(), "error")
+		assert.Equal(t, "Invalid request", val.String())
+	})
+
 }
