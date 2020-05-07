5c0d12c4d94958c273aa4d0ac4ef4b733a445560
Theresa Gresch <theresagresch@gmail.com>
2020-05-07 18:08:12 +0200

    Backend: Add tests to internal/entity

--- internal/entity/file_test.go
+++ internal/entity/file_test.go
@@ -7,6 +7,21 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestFirstFileByHash(t *testing.T) {
+	t.Run("not existing file", func(t *testing.T) {
+		f, err := FirstFileByHash("xxx")
+		assert.Equal(t, "record not found", err.Error())
+		assert.Equal(t, uint(0), f.ID)
+	})
+	t.Run("existing file", func(t *testing.T) {
+		f, err := FirstFileByHash("2cad9168fa6acc5c5c2965ddf6ec465ca42fd818")
+		if err != nil {
+			t.Fatal("error")
+		}
+		assert.Equal(t, uint(0xf4240), f.ID)
+	})
+}
+
 func TestFile_DownloadFileName(t *testing.T) {
 	t.Run("photo with title", func(t *testing.T) {
 		photo := &Photo{TakenAtLocal: time.Date(2019, 01, 15, 0, 0, 0, 0, time.UTC), PhotoTitle: "Berlin / Morning Mood"}
@@ -32,5 +47,23 @@ func TestFile_DownloadFileName(t *testing.T) {
 		filename := file.ShareFileName()
 
 		assert.Equal(t, "123Hash.jpg", filename)
+	})
+}
+
+func TestFile_Changed(t *testing.T) {
+	t.Run("different modified times", func(t *testing.T) {
+		file := &File{Photo: nil, FileType: "jpg", FileSize: 500, FileModified: time.Date(2019, 01, 15, 0, 0, 0, 0, time.UTC)}
+		time := time.Date(2020, 01, 15, 0, 0, 0, 0, time.UTC)
+		assert.Equal(t, true, file.Changed(500, time))
+	})
+	t.Run("different sizes", func(t *testing.T) {
+		file := &File{Photo: nil, FileType: "jpg", FileSize: 600, FileModified: time.Date(2019, 01, 15, 0, 0, 0, 0, time.UTC)}
+		time := time.Date(2019, 01, 15, 0, 0, 0, 0, time.UTC)
+		assert.Equal(t, true, file.Changed(500, time))
+	})
+	t.Run("no change", func(t *testing.T) {
+		file := &File{Photo: nil, FileType: "jpg", FileSize: 500, FileModified: time.Date(2019, 01, 15, 0, 0, 0, 0, time.UTC)}
+		time := time.Date(2019, 01, 15, 0, 0, 0, 0, time.UTC)
+		assert.Equal(t, false, file.Changed(500, time))
 	})
 }
