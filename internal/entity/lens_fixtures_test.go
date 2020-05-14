156c7b295e156fdd749b2ec1749c85c78914cd79
Theresa Gresch <theresagresch@gmail.com>
2020-05-14 15:04:53 +0200

    Backend: Add tests to internal/entity

---
+++ internal/entity/lens_fixtures_test.go
@@ -0,0 +1,34 @@
+package entity
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLensMap_Get(t *testing.T) {
+	t.Run("get existing lens", func(t *testing.T) {
+		r := LensFixtures.Get("lens-f-380")
+		assert.Equal(t, uint(1000000), r.ID)
+		assert.Equal(t, "lens-f-380", r.LensSlug)
+		assert.IsType(t, Lens{}, r)
+	})
+	t.Run("get not existing lens", func(t *testing.T) {
+		r := LensFixtures.Get("Lens 123")
+		assert.Equal(t, "lens-123", r.LensSlug)
+		assert.IsType(t, Lens{}, r)
+	})
+}
+
+func TestLensMap_Pointer(t *testing.T) {
+	t.Run("get existing lens pointer", func(t *testing.T) {
+		r := LensFixtures.Pointer("lens-f-380")
+		assert.Equal(t, uint(1000000), r.ID)
+		assert.Equal(t, "lens-f-380", r.LensSlug)
+		assert.IsType(t, &Lens{}, r)
+	})
+	t.Run("get not existing lens pointer", func(t *testing.T) {
+		r := LensFixtures.Pointer("Lens new")
+		assert.Equal(t, "lens-new", r.LensSlug)
+		assert.IsType(t, &Lens{}, r)
+	})
+}
