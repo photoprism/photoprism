26f749cfdcf9a52e943269e83a52f93379d2e68d
Michael Mayer <michael@liquidbytes.net>
2020-05-26 07:14:50 +0200

    Folder selections should include sub directories #260
    Signed-off-by: Michael Mayer <michael@liquidbytes.net>

--- internal/query/selection.go
+++ internal/query/selection.go
@@ -19,11 +19,13 @@ func PhotoSelection(f form.Selection) (results Photos, err error) {
 		Where(`photos.photo_uid IN (?) 
 		OR photos.place_uid IN (?) 
 		OR photos.photo_uid IN (SELECT photo_uid FROM files WHERE file_uid IN (?))
-		OR photos.photo_path IN (SELECT path FROM folders WHERE folder_uid IN (?))
+		OR photos.photo_path IN (
+			SELECT a.path FROM folders a WHERE a.folder_uid IN (?) UNION
+			SELECT b.path FROM folders a JOIN folders b ON b.path LIKE CONCAT(a.path, '/%') WHERE a.folder_uid IN (?))
 		OR photos.photo_uid IN (SELECT photo_uid FROM photos_albums WHERE album_uid IN (?))
 		OR photos.id IN (SELECT pl.photo_id FROM photos_labels pl JOIN labels l ON pl.label_id = l.id AND l.deleted_at IS NULL WHERE l.label_uid IN (?))
 		OR photos.id IN (SELECT pl.photo_id FROM photos_labels pl JOIN categories c ON c.label_id = pl.label_id JOIN labels lc ON lc.id = c.category_id AND lc.deleted_at IS NULL WHERE lc.label_uid IN (?))`,
-			f.Photos, f.Places, f.Files, f.Files, f.Albums, f.Labels, f.Labels)
+			f.Photos, f.Places, f.Files, f.Files, f.Files, f.Albums, f.Labels, f.Labels)
 
 	if result := s.Scan(&results); result.Error != nil {
 		return results, result.Error
@@ -48,11 +50,13 @@ func FileSelection(f form.Selection) (results Files, err error) {
 		Where(`photos.photo_uid IN (?) 
 		OR photos.place_uid IN (?) 
 		OR photos.photo_uid IN (SELECT photo_uid FROM files WHERE file_uid IN (?))
-		OR photos.photo_path IN (SELECT path FROM folders WHERE folder_uid IN (?))
+		OR photos.photo_path IN (
+			SELECT a.path FROM folders a WHERE a.folder_uid IN (?) UNION
+			SELECT b.path FROM folders a JOIN folders b ON b.path LIKE CONCAT(a.path, '/%') WHERE a.folder_uid IN (?))
 		OR photos.photo_uid IN (SELECT photo_uid FROM photos_albums WHERE album_uid IN (?))
 		OR photos.id IN (SELECT pl.photo_id FROM photos_labels pl JOIN labels l ON pl.label_id = l.id AND l.deleted_at IS NULL WHERE l.label_uid IN (?))
 		OR photos.id IN (SELECT pl.photo_id FROM photos_labels pl JOIN categories c ON c.label_id = pl.label_id JOIN labels lc ON lc.id = c.category_id AND lc.deleted_at IS NULL WHERE lc.label_uid IN (?))`,
-			f.Photos, f.Places, f.Files, f.Files, f.Albums, f.Labels, f.Labels).
+			f.Photos, f.Places, f.Files, f.Files, f.Files, f.Albums, f.Labels, f.Labels).
 		Group("files.id")
 
 	if result := s.Scan(&results); result.Error != nil {
