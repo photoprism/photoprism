2adf5fe32b31b1a4b9d4592a1196e83f276fe6f6
Theresa Gresch <theresagresch@gmail.com>
2020-05-31 19:32:02 +0200

    Frontend: Add acceptance tests

--- frontend/tests/acceptance/places.js
+++ frontend/tests/acceptance/places.js
@@ -1,1 +1,21 @@
-// see places on map
+import { Selector } from 'testcafe';
+import { ClientFunction } from 'testcafe';
+import testcafeconfig from "./testcafeconfig.json";
+
+const getLocation = ClientFunction(() => document.location.href);
+
+fixture`Test places page`
+    .page`${testcafeconfig.url}`
+
+test('Test places', async t => {
+    await t
+        .click(Selector('.p-navigation-places'))
+        .expect(Selector('#map').exists, {timeout: 15000}).ok()
+        .expect(Selector('div.p-map-control').visible).ok();
+    await t
+        .typeText(Selector('input[aria-label="Search"]'), 'Berlin')
+        .pressKey('enter');
+    await t
+        .expect(Selector('div.p-map-control').visible).ok()
+        .expect(getLocation()).contains('Berlin');
+});
