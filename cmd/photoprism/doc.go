5d4b36b53cc5a67fe702127325232fd2d8ddd0ce
Michael Mayer <michael@liquidbytes.net>
2020-06-20 14:29:11 +0200

    Update copyright notice
    Signed-off-by: Michael Mayer <michael@liquidbytes.net>

--- cmd/photoprism/doc.go
+++ cmd/photoprism/doc.go
@@ -1,6 +1,6 @@
 /*
 
-Copyright (C) 2018 - 2020 PhotoPrism.org <hello@photoprism.org>
+Copyright (C) 2018 - 2020 Michael Mayer <hello@photoprism.org>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as published
