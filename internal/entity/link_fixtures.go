3e6c21a566444668708d90689bfdd631ce11e6c0
Theresa Gresch <theresagresch@gmail.com>
2020-07-08 17:06:23 +0200

    Backend: Add unit tests for internal/entity

--- internal/entity/link_fixtures.go
+++ internal/entity/link_fixtures.go
@@ -15,6 +15,14 @@ var LinkFixtures = LinkMap{
 		CanEdit:     false,
 		CreatedAt:   time.Date(2020, 3, 6, 2, 6, 51, 0, time.UTC),
 	},
+	"4jxf3jfn2k": {
+		LinkToken:   "4jxf3jfn2k",
+		LinkExpires: 0,
+		ShareUID:    "at9lxuqxpogaaba7",
+		CanComment:  true,
+		CanEdit:     false,
+		CreatedAt:   time.Date(2020, 3, 6, 2, 6, 51, 0, time.UTC),
+	},
 }
 
 // CreateLinkFixtures inserts known entities into the database for testing.
