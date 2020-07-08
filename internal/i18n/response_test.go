724953e4321f5817427b877b5416dae9dc3e6f24
Theresa Gresch <theresagresch@gmail.com>
2020-07-08 10:03:35 +0200

    Backend: Add unit tests for internal/i18n

--- internal/i18n/response_test.go
+++ internal/i18n/response_test.go
@@ -36,3 +36,59 @@ func TestNewResponse(t *testing.T) {
 		}
 	})
 }
+
+func TestResponse_String(t *testing.T) {
+	t.Run("error", func(t *testing.T) {
+		resp := Response{404, "Not found", "page not found", "xyz"}
+		assert.Equal(t, "Not found", resp.String())
+	})
+
+	t.Run("no error", func(t *testing.T) {
+		t.Run("error", func(t *testing.T) {
+			resp := Response{200, "", "Ok", "xyz"}
+			assert.Equal(t, "Ok", resp.String())
+		})
+	})
+}
+
+func TestResponse_LowerString(t *testing.T) {
+	t.Run("error", func(t *testing.T) {
+		resp := Response{404, "Not found", "page not found", "xyz"}
+		assert.Equal(t, "not found", resp.LowerString())
+	})
+
+	t.Run("no error", func(t *testing.T) {
+		t.Run("error", func(t *testing.T) {
+			resp := Response{200, "", "Ok", "xyz"}
+			assert.Equal(t, "ok", resp.LowerString())
+		})
+	})
+}
+
+func TestResponse_Error(t *testing.T) {
+	t.Run("error", func(t *testing.T) {
+		resp := Response{404, "Not found", "page not found", "xyz"}
+		assert.Equal(t, "Not found", resp.Error())
+	})
+
+	t.Run("no error", func(t *testing.T) {
+		t.Run("error", func(t *testing.T) {
+			resp := Response{200, "", "Ok", "xyz"}
+			assert.Equal(t, "", resp.Error())
+		})
+	})
+}
+
+func TestResponse_Success(t *testing.T) {
+	t.Run("error", func(t *testing.T) {
+		resp := Response{404, "Not found", "page not found", "xyz"}
+		assert.Equal(t, false, resp.Success())
+	})
+
+	t.Run("no error", func(t *testing.T) {
+		t.Run("error", func(t *testing.T) {
+			resp := Response{200, "", "Ok", "xyz"}
+			assert.Equal(t, true, resp.Success())
+		})
+	})
+}
