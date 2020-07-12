2bef7e7e9de09d25b00553f5cc9bc08414717b15
Michael Mayer <michael@liquidbytes.net>
2020-07-12 05:20:16 +0200

    Backend: Reduce location estimate time difference to 3 days
    Signed-off-by: Michael Mayer <michael@liquidbytes.net>

--- internal/entity/photo_optimize.go
+++ internal/entity/photo_optimize.go
@@ -73,7 +73,7 @@ func (m *Photo) EstimatePlace() {
 		log.Errorf("photo: %s (estimate place)", err.Error())
 		m.EstimateCountry()
 	} else {
-		if days := recentPhoto.TakenAt.Sub(m.TakenAt) / (time.Hour * 24); days < -7 || days > 7 {
+		if days := recentPhoto.TakenAt.Sub(m.TakenAt) / (time.Hour * 24); days < -3 || days > 3 {
 			log.Debugf("photo: can't estimate position of %s, %d days time difference", m, days)
 		} else if recentPhoto.HasPlace() {
 			m.Place = recentPhoto.Place
