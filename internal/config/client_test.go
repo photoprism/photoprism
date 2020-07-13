6cea8f0d3bc0e619abbc0d2607c72f591510e99f
Theresa Gresch <theresagresch@gmail.com>
2020-07-13 18:05:10 +0200

    Backend: Add unit tests for internal/config

--- internal/config/client_test.go
+++ internal/config/client_test.go
@@ -10,6 +10,10 @@ func TestConfig_PublicConfig(t *testing.T) {
 	result := config.PublicConfig()
 	assert.IsType(t, ClientConfig{}, result)
 	assert.Equal(t, true, result.Public)
+	c2 := NewTestErrorConfig()
+	result2 := c2.PublicConfig()
+	assert.IsType(t, ClientConfig{}, result2)
+	assert.Equal(t, false, result2.Public)
 }
 
 func TestConfig_GuestConfig(t *testing.T) {
---
+++ internal/config/filenames_test.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConfig_FindExecutable(t *testing.T) {
+	assert.Equal(t, "", findExecutable("yyy", "xxx"))
+}
+
+func TestConfig_SidecarJson(t *testing.T) {
+	c := NewTestConfig()
+	assert.Equal(t, false, c.SidecarJson())
+}
+
+func TestConfig_SidecarYaml(t *testing.T) {
+	c := NewTestConfig()
+	assert.Equal(t, false, c.SidecarYaml())
+}
+
+func TestConfig_SidecarPath(t *testing.T) {
+	c := NewTestConfig()
+	assert.Equal(t, ".photoprism", c.SidecarPath())
+}
+
+func TestConfig_SidecarPathIsAbs(t *testing.T) {
+	c := NewTestConfig()
+	assert.Equal(t, false, c.SidecarPathIsAbs())
+}
+
+func TestConfig_SidecarWritable(t *testing.T) {
+	c := NewTestConfig()
+	assert.Equal(t, true, c.SidecarWritable())
+}
+
+func TestConfig_FFmpegBin(t *testing.T) {
+	c := NewTestConfig()
+	assert.Equal(t, "/usr/bin/ffmpeg", c.FFmpegBin())
+}
+
+func TestConfig_TempPath(t *testing.T) {
+	c := NewTestConfig()
+	assert.Equal(t, "/go/src/github.com/photoprism/photoprism/storage/testdata/temp", c.TempPath())
+}
+
+func TestConfig_StoragePath(t *testing.T) {
+	c := NewTestConfig()
+	assert.Equal(t, "/go/src/github.com/photoprism/photoprism/storage/testdata", c.StoragePath())
+}
+
+func TestConfig_TestdataPath(t *testing.T) {
+	c := NewTestConfig()
+	assert.Equal(t, "/go/src/github.com/photoprism/photoprism/storage/testdata/testdata", c.TestdataPath())
+}
