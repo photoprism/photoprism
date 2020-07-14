c027e31304b7975873af05f89f706fd352ed58da
Theresa Gresch <theresagresch@gmail.com>
2020-07-14 17:02:39 +0200

    Backend: Add unit tests for internal/api

--- internal/api/share_preview_test.go
+++ internal/api/share_preview_test.go
@@ -14,4 +14,10 @@ func TestGetPreview(t *testing.T) {
 		r := PerformRequest(app, "GET", "/a/1jxf3jfn2k/st9lxuqxpogaaba7/preview")
 		assert.Equal(t, http.StatusNotFound, r.Code)
 	})
+	t.Run("invalid token", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		SharePreview(router)
+		r := PerformRequest(app, "GET", "/a/xxx/st9lxuqxpogaaba7/preview")
+		assert.Equal(t, http.StatusNotFound, r.Code)
+	})
 }
---
+++ internal/api/share_test.go
@@ -0,0 +1,36 @@
+package api
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestGetShares(t *testing.T) {
+	t.Run("invalid token or share", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		Shares(router)
+		r := PerformRequest(app, "GET", "/api/v1/1jxf3jfn2k/st9lxuqxpogaaba7")
+		assert.Equal(t, http.StatusTemporaryRedirect, r.Code)
+	})
+	//TODO Why does it panic?
+	/*t.Run("valid token and share", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		Shares(router)
+		r := PerformRequest(app, "GET", "/api/v1/4jxf3jfn2k/at9lxuqxpogaaba7")
+		assert.Equal(t, http.StatusTemporaryRedirect, r.Code)
+	})*/
+	t.Run("invalid token", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		Shares(router)
+		r := PerformRequest(app, "GET", "/api/v1/xxx")
+		assert.Equal(t, http.StatusTemporaryRedirect, r.Code)
+	})
+	//TODO Why does it panic?
+	/*t.Run("valid token", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		Shares(router)
+		r := PerformRequest(app, "GET", "/api/v1/4jxf3jfn2k")
+		assert.Equal(t, http.StatusTemporaryRedirect, r.Code)
+	})*/
+}
