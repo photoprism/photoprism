75ad59cd17a9ae34cefd7a2239b76bad2551805c
Theresa Gresch <theresagresch@gmail.com>
2020-07-14 14:00:39 +0200

    Backend: Add unit tests for internal/api

--- internal/api/link_test.go
+++ internal/api/link_test.go
@@ -137,7 +137,7 @@ func TestGetAlbumLinks(t *testing.T) {
 		r2 := PerformRequest(app, "GET", "/api/v1/albums/at9lxuqxpogaaba7/links")
 		len := gjson.Get(r2.Body.String(), "#")
 		assert.GreaterOrEqual(t, len.Int(), int64(1))
-		assert.Equal(t, http.StatusOK, r.Code)
+		assert.Equal(t, http.StatusOK, r2.Code)
 	})
 
 	t.Run("not found", func(t *testing.T) {
@@ -175,7 +175,7 @@ func TestCreatePhotoLink(t *testing.T) {
 
 		CreatePhotoLink(router)
 
-		resp := PerformRequestWithBody(app, "POST", "/api/v1/photos/xxx/link", `{"Password": "foobar", "Expires": 0, "CanEdit": true}`)
+		resp := PerformRequestWithBody(app, "POST", "/api/v1/photos/xxx/links", `{"Password": "foobar", "Expires": 0, "CanEdit": true}`)
 
 		if resp.Code != http.StatusNotFound {
 			t.Fatal(resp.Body.String())
@@ -186,6 +186,94 @@ func TestCreatePhotoLink(t *testing.T) {
 		CreatePhotoLink(router)
 		r := PerformRequestWithBody(app, "POST", "/api/v1/photos/pt9jtdre2lvl0yh7/links", `{"xxx": 123, "Expires": "abc", "CanEdit": "xxx"}`)
 		assert.Equal(t, http.StatusBadRequest, r.Code)
+	})
+}
+
+func TestUpdatePhotoLink(t *testing.T) {
+	app, router, _ := NewApiTest()
+
+	CreatePhotoLink(router)
+
+	r := PerformRequestWithBody(app, "POST", "/api/v1/photos/pt9jtdre2lvl0yh7/links", `{"Password": "foobar", "Expires": 0, "CanEdit": true}`)
+
+	if r.Code != http.StatusOK {
+		t.Fatal(r.Body.String())
+	}
+	val2 := gjson.Get(r.Body.String(), "Expires")
+	assert.Equal(t, "0", val2.String())
+	uid := gjson.Get(r.Body.String(), "UID").String()
+
+	t.Run("successful request", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		UpdatePhotoLink(router)
+		r := PerformRequestWithBody(app, "PUT", "/api/v1/photos/pt9jtdre2lvl0yh7/links/"+uid, `{"Token": "newToken", "Expires": 8000, "Password": "1234nhfhfd"}`)
+		val := gjson.Get(r.Body.String(), "Token")
+		assert.Equal(t, "newtoken", val.String())
+		val2 := gjson.Get(r.Body.String(), "Expires")
+		assert.Equal(t, "8000", val2.String())
+		assert.Equal(t, http.StatusOK, r.Code)
+	})
+
+	t.Run("bad request", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		UpdatePhotoLink(router)
+		r := PerformRequestWithBody(app, "PUT", "/api/v1/photos/pt9jtdre2lvl0yh7/links/"+uid, `{"Token": "newToken", "Expires": "vgft", "xxx": "xxx"}`)
+		assert.Equal(t, http.StatusBadRequest, r.Code)
+	})
+}
+
+//TODO Fully assert once functionality exists
+func TestDeletePhotoLink(t *testing.T) {
+	app, router, _ := NewApiTest()
+
+	CreatePhotoLink(router)
+
+	r := PerformRequestWithBody(app, "POST", "/api/v1/photos/pt9jtdre2lvl0yh7/links", `{"Password": "foobar", "Expires": 0, "CanEdit": true}`)
+
+	if r.Code != http.StatusOK {
+		t.Fatal(r.Body.String())
+	}
+	uid := gjson.Get(r.Body.String(), "UID").String()
+
+	//GetPhotoLinks(router)
+	//r2 := PerformRequest(app, "GET", "/api/v1/photos/pt9jtdre2lvl0yh7/links")
+	//len := gjson.Get(r2.Body.String(), "#")
+
+	t.Run("successful deletion", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		DeletePhotoLink(router)
+		r := PerformRequest(app, "DELETE", "/api/v1/photos/pt9jtdre2lvl0yh7/links/"+uid)
+		assert.Equal(t, http.StatusOK, r.Code)
+		GetPhotoLinks(router)
+		r2 := PerformRequest(app, "GET", "/api/v1/photos/pt9jtdre2lvl0yh7/links")
+		t.Log(r2)
+		//len2 := gjson.Get(r2.Body.String(), "#")
+		//assert.Greater(t, len.Int(), len2.Int())
+	})
+}
+
+func TestGetPhotoLinks(t *testing.T) {
+	t.Run("successful request", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+
+		CreatePhotoLink(router)
+
+		r := PerformRequestWithBody(app, "POST", "/api/v1/photos/pt9jtdre2lvl0yh7/links", `{"Password": "foobar", "Expires": 0, "CanEdit": true}`)
+		if r.Code != http.StatusOK {
+			t.Fatal(r.Body.String())
+		}
+		GetPhotoLinks(router)
+		r2 := PerformRequest(app, "GET", "/api/v1/photos/pt9jtdre2lvl0yh7/links")
+		//len := gjson.Get(r2.Body.String(), "#")
+		//assert.GreaterOrEqual(t, len.Int(), int64(1))
+		assert.Equal(t, http.StatusOK, r2.Code)
+	})
+
+	t.Run("not found", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		GetPhotoLinks(router)
+		r := PerformRequest(app, "GET", "/api/v1/photos/xxx/links")
+		assert.Equal(t, http.StatusNotFound, r.Code)
 	})
 }
 
@@ -225,5 +313,92 @@ func TestCreateLabelLink(t *testing.T) {
 		CreateLabelLink(router)
 		r := PerformRequestWithBody(app, "POST", "/api/v1/labels/lt9k3pw1wowuy3c2/links", `{"xxx": 123, "Expires": "abc", "CanEdit": "xxx"}`)
 		assert.Equal(t, http.StatusBadRequest, r.Code)
+	})
+}
+
+func TestUpdateLabelLink(t *testing.T) {
+	app, router, _ := NewApiTest()
+
+	CreateLabelLink(router)
+
+	r := PerformRequestWithBody(app, "POST", "/api/v1/labels/lt9k3pw1wowuy3c2/links", `{"Password": "foobar", "Expires": 0, "CanEdit": true}`)
+
+	if r.Code != http.StatusOK {
+		t.Fatal(r.Body.String())
+	}
+	val2 := gjson.Get(r.Body.String(), "Expires")
+	assert.Equal(t, "0", val2.String())
+	uid := gjson.Get(r.Body.String(), "UID").String()
+
+	t.Run("successful request", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		UpdateLabelLink(router)
+		r := PerformRequestWithBody(app, "PUT", "/api/v1/labels/lt9k3pw1wowuy3c2/links/"+uid, `{"Token": "newToken", "Expires": 8000, "Password": "1234nhfhfd"}`)
+		val := gjson.Get(r.Body.String(), "Token")
+		assert.Equal(t, "newtoken", val.String())
+		val2 := gjson.Get(r.Body.String(), "Expires")
+		assert.Equal(t, "8000", val2.String())
+		assert.Equal(t, http.StatusOK, r.Code)
+	})
+
+	t.Run("bad request", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		UpdateLabelLink(router)
+		r := PerformRequestWithBody(app, "PUT", "/api/v1/labels/lt9k3pw1wowuy3c2/links/"+uid, `{"Token": "newToken", "Expires": "vgft", "xxx": "xxx"}`)
+		assert.Equal(t, http.StatusBadRequest, r.Code)
+	})
+}
+
+func TestDeleteLabelLink(t *testing.T) {
+	app, router, _ := NewApiTest()
+
+	CreateLabelLink(router)
+
+	r := PerformRequestWithBody(app, "POST", "/api/v1/labels/lt9k3pw1wowuy3c2/links", `{"Password": "foobar", "Expires": 0, "CanEdit": true}`)
+
+	if r.Code != http.StatusOK {
+		t.Fatal(r.Body.String())
+	}
+	uid := gjson.Get(r.Body.String(), "UID").String()
+
+	//GetLabelLinks(router)
+	//r2 := PerformRequest(app, "GET", "/api/v1/labels/lt9k3pw1wowuy3c2/links")
+	//len := gjson.Get(r2.Body.String(), "#")
+
+	t.Run("successful deletion", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		DeleteLabelLink(router)
+		r := PerformRequest(app, "DELETE", "/api/v1/labels/lt9k3pw1wowuy3c2/links/"+uid)
+		assert.Equal(t, http.StatusOK, r.Code)
+		//GetLabelLinks(router)
+		//r2 := PerformRequest(app, "GET", "/api/v1/labels/lt9k3pw1wowuy3c2/links")
+		//len2 := gjson.Get(r2.Body.String(), "#")
+		//assert.Greater(t, len.Int(), len2.Int())
+	})
+}
+
+func TestGetLabelLinks(t *testing.T) {
+	t.Run("successful request", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+
+		CreateLabelLink(router)
+
+		r := PerformRequestWithBody(app, "POST", "/api/v1/labels/lt9k3pw1wowuy3c2/links", `{"Password": "foobar", "Expires": 0, "CanEdit": true}`)
+
+		if r.Code != http.StatusOK {
+			t.Fatal(r.Body.String())
+		}
+		GetLabelLinks(router)
+		r2 := PerformRequest(app, "GET", "/api/v1/labels/lt9k3pw1wowuy3c2/links")
+		//len := gjson.Get(r2.Body.String(), "#")
+		//assert.GreaterOrEqual(t, len.Int(), int64(1))
+		assert.Equal(t, http.StatusOK, r2.Code)
+	})
+
+	t.Run("not found", func(t *testing.T) {
+		app, router, _ := NewApiTest()
+		GetLabelLinks(router)
+		r := PerformRequest(app, "GET", "/api/v1/labels/xxx/links")
+		assert.Equal(t, http.StatusNotFound, r.Code)
 	})
 }
