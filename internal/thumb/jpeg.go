df0868e3d3b496aca733c143729762f2f81f43a7
Michael Mayer <michael@liquidbytes.net>
2020-07-18 16:54:13 +0200

    Backend: Adjust log levels and messages
    Signed-off-by: Michael Mayer <michael@liquidbytes.net>

--- internal/thumb/jpeg.go
+++ internal/thumb/jpeg.go
@@ -2,22 +2,24 @@ package thumb
 
 import (
 	"image"
+	"path/filepath"
 
 	"github.com/disintegration/imaging"
+	"github.com/photoprism/photoprism/pkg/txt"
 )
 
 func Jpeg(srcFilename, jpgFilename string) (img image.Image, err error) {
 	img, err = imaging.Open(srcFilename, imaging.AutoOrientation(true))
 
 	if err != nil {
-		log.Errorf("resample: can't open %s", srcFilename)
+		log.Errorf("resample: can't open %s", txt.Quote(filepath.Base(srcFilename)))
 		return img, err
 	}
 
 	saveOption := imaging.JPEGQuality(JpegQuality)
 
 	if err = imaging.Save(img, jpgFilename, saveOption); err != nil {
-		log.Errorf("resample: failed to save %s", jpgFilename)
+		log.Errorf("resample: failed to save %s", txt.Quote(filepath.Base(jpgFilename)))
 		return img, err
 	}
 
