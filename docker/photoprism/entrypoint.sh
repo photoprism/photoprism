43f105887cf68e9da7cfb77ea4bac5eeffef8f18
Kevin Tran <KTOmega@users.noreply.github.com>
2020-10-13 13:08:00 -0700

    Improve Docker entrypoint signal handling. (#543)
    * Config: Improve Docker entrypoint signal handling
    
    * Config: Modify CMD line in Dockerfile
    
    * Config: Uppercase PHOTOPRISM_PID
    
    * Config: Change other commands to array style

--- docker/photoprism/entrypoint.sh
+++ docker/photoprism/entrypoint.sh
@@ -7,10 +7,15 @@ fi
 if [[ ${UID} ]] && [[ ${GID} ]] && [[ ${UID} != "0" ]] && [[ $(id -u) = "0" ]]; then
   usermod -u ${UID} photoprism
   usermod -g ${GID} photoprism
-  exec gosu ${UID}:${GID} "$@"
+  gosu ${UID}:${GID} "$@" &
 elif [[ ${UID} ]] && [[ ${UID} != "0" ]] && [[ $(id -u) = "0" ]]; then
   usermod -u ${UID} photoprism
-  exec gosu ${UID} "$@"
+  gosu ${UID} "$@" &
 else
-  exec "$@"
+  "$@" &
 fi
+
+PHOTOPRISM_PID=$!
+
+trap "kill $PHOTOPRISM_PID" INT TERM
+wait
