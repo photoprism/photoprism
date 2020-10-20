ce4960e2b8fa0bed30c61830851b9108d58562b0
Theresa Gresch <theresagresch@gmail.com>
2020-10-20 12:06:16 +0200

    Backend: Add unit tests

--- internal/photoprism/index_test.go
+++ internal/photoprism/index_test.go
@@ -1,6 +1,7 @@
 package photoprism
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 
 	"github.com/photoprism/photoprism/internal/classify"
@@ -31,4 +32,23 @@ func TestIndex_Start(t *testing.T) {
 	indexOpt.Rescan = false
 
 	ind.Start(indexOpt)
+}
+
+func TestIndex_File(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+
+	conf := config.TestConfig()
+
+	conf.InitializeTestData(t)
+
+	tf := classify.New(conf.AssetsPath(), conf.TensorFlowOff())
+	nd := nsfw.New(conf.NSFWModelPath())
+	convert := NewConvert(conf)
+
+	ind := NewIndex(conf, tf, nd, convert)
+
+	err := ind.File("xxx")
+	assert.Equal(t, IndexFailed, err.Status)
 }
