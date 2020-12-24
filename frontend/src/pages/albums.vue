aefd181e680b3f5250992f90224053b75fcdb914
Michael Mayer <michael@liquidbytes.net>
2020-12-24 12:07:45 +0100

    Albums: Hide delete button in folders, months and moments context menu #738
    Signed-off-by: Michael Mayer <michael@liquidbytes.net>

--- frontend/src/pages/albums.vue
+++ frontend/src/pages/albums.vue
@@ -52,7 +52,7 @@
       <p-scroll-top></p-scroll-top>
 
       <p-album-clipboard :refresh="refresh" :selection="selection" :share="share" :edit="edit"
-                         :clear-selection="clearSelection"></p-album-clipboard>
+                         :clear-selection="clearSelection" :context="context"></p-album-clipboard>
 
       <v-container grid-list-xs fluid class="pa-2 p-albums p-albums-cards">
         <v-card v-if="results.length === 0" class="p-albums-empty secondary-light lighten-1 ma-1" flat>
@@ -214,6 +214,19 @@ export default {
   props: {
     staticFilter: Object,
     view: String,
+  },
+  computed: {
+    context: function () {
+      if (!this.staticFilter) {
+        return "album";
+      }
+
+      if (this.staticFilter.type) {
+        return this.staticFilter.type;
+      }
+
+      return "";
+    }
   },
   watch: {
     '$route'() {
