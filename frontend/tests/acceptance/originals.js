966d88d8a8b87770cc2b21f452e3dae1b39bf44d
theresa <theresagresch@gmail.com>
2021-01-13 14:39:15 +0100

    Tests: Adapt acceptance tests to css changes

--- frontend/tests/acceptance/originals.js
+++ frontend/tests/acceptance/originals.js
@@ -19,22 +19,22 @@ test('#1 Add originals files to album', async t => {
         .click(Selector('div.nav-library + div'))
         .click(Selector('.nav-originals'))
         .click(Selector('button').withText('Vacation'));
-    const FirstItemInVacation = await Selector('div.p-photo-desc').nth(0).innerText;
-    const KanadaUid = await Selector('div.p-photo-desc').nth(0).getAttribute('data-uid');
-    const SecondItemInVacation = await Selector('div.p-photo-desc').nth(1).innerText;
+    const FirstItemInVacation = await Selector('div.result').nth(0).innerText;
+    const KanadaUid = await Selector('div.result').nth(0).getAttribute('data-uid');
+    const SecondItemInVacation = await Selector('div.result').nth(1).innerText;
     await t
         .expect(FirstItemInVacation).contains('Kanada')
         .expect(SecondItemInVacation).contains('Korsika')
         .click(Selector('button').withText('Kanada'));
 
-    const FirstItemInKanada = await Selector('div.p-photo-desc').nth(0).innerText;
-    const SecondItemInKanada = await Selector('div.p-photo-desc').nth(1).innerText;
+    const FirstItemInKanada = await Selector('div.result').nth(0).innerText;
+    const SecondItemInKanada = await Selector('div.result').nth(1).innerText;
     await t
         .expect(FirstItemInKanada).contains('BotanicalGarden')
         .expect(SecondItemInKanada).contains('IMG')
         .click(Selector('button').withText('BotanicalGarden'))
         .click(Selector('a[href="/library/files/Vacation"]'));
-    await page.selectFromUID(KanadaUid);
+    await page.selectPhotoFromUID(KanadaUid);
     const clipboardCount = await Selector('span.count-clipboard', {timeout: 5000});
     await t
         .expect(clipboardCount.textContent).eql("1");
@@ -43,10 +43,10 @@ test('#1 Add originals files to album', async t => {
         .click(Selector('.nav-albums'))
         .typeText(Selector('.p-albums-search input'), 'KanadaVacation')
         .pressKey('enter');
-    const AlbumUid = await Selector('div.p-album').nth(0).getAttribute('data-uid');
+    const AlbumUid = await Selector('a.is-album').nth(0).getAttribute('data-uid');
     await t
-        .click(Selector('div.p-album').nth(0));
-    const PhotoCountAfterAdd = await Selector('div.p-photo', {timeout: 5000}).count;
+        .click(Selector('a.is-album').nth(0));
+    const PhotoCountAfterAdd = await Selector('div.is-photo', {timeout: 5000}).count;
     await t
         .expect(PhotoCountAfterAdd).eql(2)
         .click(Selector('.nav-albums'));
@@ -54,14 +54,13 @@ test('#1 Add originals files to album', async t => {
     await page.deleteSelected();
 });
 
-//TODO test download itself + clipboard count after download
 test('#2 Download original files', async t => {
     await page.openNav();
     await t
         .click(Selector('div.nav-library + div'))
         .click(Selector('.nav-originals'));
-    const FirstFile = await Selector('div.p-file').nth(0).getAttribute('data-uid');
-    await page.selectFromUID(FirstFile);
+    const FirstFile = await Selector('div.is-file').nth(0).getAttribute('data-uid');
+    await page.selectPhotoFromUID(FirstFile);
     const clipboardCount = await Selector('span.count-clipboard', {timeout: 5000});
     await t
         .expect(clipboardCount.textContent).eql("1")
