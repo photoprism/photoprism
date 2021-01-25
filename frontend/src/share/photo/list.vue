f0807877429c5729b1acb183b41a14e808b9142e
Michael Mayer <michael@lastzero.net>
2021-01-25 09:58:56 +0100

    UX: Revert play_circle_outline back to play_arrow in photo views

--- frontend/src/share/photo/list.vue
+++ frontend/src/share/photo/list.vue
@@ -54,7 +54,7 @@
                    flat icon large absolute class="input-open"
                    @click.stop.prevent="openPhoto(props.index, true)">
               <v-icon color="white" class="default-hidden action-live" :title="$gettext('Live')">$vuetify.icons.live_photo</v-icon>
-              <v-icon color="white" class="default-hidden action-play" :title="$gettext('Video')">play_circle_outline</v-icon>
+              <v-icon color="white" class="default-hidden action-play" :title="$gettext('Video')">play_arrow</v-icon>
             </v-btn>
           </v-img>
         </td>
