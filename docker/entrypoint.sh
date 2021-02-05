c51425067c80e13a128a8e2236f08803f8a6d7b0
Michael Mayer <michael@lastzero.net>
2021-02-05 17:39:48 +0100

    Entrypoint: Disable chown with PHOTOPRISM_DISABLE_CHOWN #1000

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,19 +1,39 @@
 #!/usr/bin/env bash
 
 if [[ ${UMASK} ]]; then
+  echo "entrypoint: umask ${UMASK}"
   umask "${UMASK}"
 fi
 
 if [[ ${UID} ]] && [[ ${GID} ]] && [[ ${UID} != "0" ]] && [[ $(id -u) = "0" ]]; then
   groupadd -f -g "${GID}" "${GID}"
   usermod -o -u "${UID}" -g "${GID}" photoprism
-  chown -Rf photoprism /photoprism/assets /photoprism/storage /photoprism/import /var/lib/photoprism /tmp/photoprism
+
+  if [[ -z ${PHOTOPRISM_DISABLE_CHOWN} ]] ; then
+    echo "entrypoint: updating storage permissions"
+    chown -Rf photoprism /photoprism/storage /photoprism/assets /var/lib/photoprism /tmp/photoprism
+  fi
+
+  echo "entrypoint: uid ${UID}:${GID}"
+  echo "entrypoint: starting ${@}"
+
   gosu "${UID}:${GID}" "$@" &
 elif [[ ${UID} ]] && [[ ${UID} != "0" ]] && [[ $(id -u) = "0" ]]; then
   usermod -o -u "${UID}" photoprism
-  chown -Rf photoprism /photoprism/assets /photoprism/storage /photoprism/import /var/lib/photoprism /tmp/photoprism
+
+  if [[ -z ${PHOTOPRISM_DISABLE_CHOWN} ]] ; then
+    echo "entrypoint: updating storage permissions"
+    chown -Rf photoprism /photoprism/storage /photoprism/assets /var/lib/photoprism /tmp/photoprism
+  fi
+
+  echo "entrypoint: uid ${UID}"
+  echo "entrypoint: starting ${@}"
+
   gosu "${UID}" "$@" &
 else
+  echo "entrypoint: uid $(id -u)"
+  echo "entrypoint: starting ${@}"
+
   "$@" &
 fi
 
