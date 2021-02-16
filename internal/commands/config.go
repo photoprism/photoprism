f77d9062a232465e4dd3ae2034682c698d72971d
Michael Mayer <michael@lastzero.net>
2021-02-16 09:02:34 +0100

    Video: Add ffmpeg-buffers to config command output #703

--- internal/commands/config.go
+++ internal/commands/config.go
@@ -113,6 +113,7 @@ func configAction(ctx *cli.Context) error {
 	fmt.Printf("%-25s %s\n", "heifconvert-bin", conf.HeifConvertBin())
 	fmt.Printf("%-25s %s\n", "ffmpeg-bin", conf.FFmpegBin())
 	fmt.Printf("%-25s %s\n", "ffmpeg-codec", conf.FFmpegCodec())
+	fmt.Printf("%-25s %d\n", "ffmpeg-buffers", conf.FFmpegBuffers())
 	fmt.Printf("%-25s %s\n", "exiftool-bin", conf.ExifToolBin())
 
 	// Thumbs, resampling and download security token.
