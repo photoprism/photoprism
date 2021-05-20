4b32bac76e996736e7264edf60347d1fed57dd6e
Michael Mayer <michael@lastzero.net>
2021-05-20 11:44:55 +0200

    HEIF: Test if output image was rotated based on "QuickTime:Rotation"

--- docker/scripts/heif-convert.sh
+++ docker/scripts/heif-convert.sh
@@ -9,3 +9,9 @@ fi
 # USAGE: heif-convert [-q quality 0..100] <filename> <output>
 
 /usr/bin/heif-convert -q 92 "$1" "$2"
+
+# Reset Exif orientation flag if output image was rotated based on "QuickTime:Rotation"
+
+if [[ $(exiftool -n -QuickTime:Rotation "$1") ]]; then
+    /usr/bin/exiftool -overwrite_original -P -n -Orientation=1 "$2"
+fi
