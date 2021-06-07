af11a184052913c2b9e6653e6847ff328d5f14ca
Micky <60691199+AliMickey@users.noreply.github.com>
2021-06-07 14:48:53 +1000

    Add homepage config options

--- frontend/src/options/options.js
+++ frontend/src/options/options.js
@@ -236,6 +236,65 @@ export const Themes = () => [
     disabled: false,
   },
 ];
+
+export const HomePages = () => [
+  {
+    text: $gettext("Search"),
+    value: "home",
+    disabled: false,
+  },
+  {
+    text: $gettext("Albums"),
+    value: "albums",
+    disabled: false,
+  },
+  {
+    text: $gettext("Videos"),
+    value: "videos",
+    disabled: false,
+  },
+  {
+    text: $gettext("Favorites"),
+    value: "favorites",
+    disabled: false,
+  },
+  {
+    text: $gettext("Moments"),
+    value: "moments",
+    disabled: false,
+  },
+  {
+    text: $gettext("Calender"),
+    value: "calender",
+    disabled: false,
+  },
+  {
+    text: $gettext("Places"),
+    value: "places",
+    disabled: false,
+  },
+  {
+    text: $gettext("Labels"),
+    value: "labels",
+    disabled: false,
+  },
+  {
+    text: $gettext("Folders"),
+    value: "folders",
+    disabled: false,
+  },
+  {
+    text: $gettext("Private"),
+    value: "private",
+    disabled: false,
+  },
+  {
+    text: $gettext("Library"),
+    value: "library",
+    disabled: false,
+  },
+];
+
 export const MapsAnimate = () => [
   {
     text: $gettext("None"),
