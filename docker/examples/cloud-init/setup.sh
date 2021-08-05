6122949babb4818184f99ab0c60adb6cdd0b59fc
Michael Mayer <michael@photoprism.app>
2021-08-05 20:18:08 +0200

    Docker: Improve cloud-init setup

--- docker/examples/cloud-init/setup.sh
+++ docker/examples/cloud-init/setup.sh
@@ -36,7 +36,8 @@ apt-get update
 apt-get -qq dist-upgrade
 
 # install dependencies
-apt-get -qq install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common
+apt-get -qq install -y --no-install-recommends apt-transport-https ca-certificates \
+        curl software-properties-common openssl
 
 # install docker if needed
 if ! command -v docker &> /dev/null
@@ -59,11 +60,35 @@ fi
 
 # create user
 useradd photoprism -u 1000 -G docker -o -m -d /photoprism || echo "User 'photoprism' already exists. Proceeding."
-mkdir -p /photoprism/originals /photoprism/import /photoprism/storage /photoprism/backup /photoprism/database
+mkdir -p /photoprism/originals /photoprism/import /photoprism/storage /photoprism/backup \
+      /photoprism/database /photoprism/traefik /photoprism/certs
+
+# download ssl config
+curl -fsSL https://dl.photoprism.org/docker/cloud-init/certs/ca.conf > /photoprism/certs/ca.conf
+curl -fsSL https://dl.photoprism.org/docker/cloud-init/certs/cert.conf > /photoprism/certs/cert.conf
+curl -fsSL https://dl.photoprism.org/docker/cloud-init/certs/config.yml > /photoprism/certs/config.yml
+curl -fsSL https://dl.photoprism.org/docker/cloud-init/certs/openssl.conf > /photoprism/certs/openssl.conf
+
+# create ca
+openssl genrsa -out /photoprism/certs/ca.key 4096
+openssl req -x509 -new -nodes -key /photoprism/certs/ca.key -sha256 -days 365 \
+        -out /photoprism/certs/ca.pem -config /photoprism/certs/ca.conf
+openssl x509 -outform der -in /photoprism/certs/ca.pem -out /photoprism/certs/ca.crt
+
+# create certs
+openssl genrsa -out /photoprism/certs/cert.key 4096
+openssl req -new -config /photoprism/certs/openssl.conf -key /photoprism/certs/cert.key \
+        -out /photoprism/certs/cert.csr
+openssl x509 -req -in /photoprism/certs/cert.csr -CA /photoprism/certs/ca.pem \
+        -CAkey /photoprism/certs/ca.key -CAcreateserial \
+        -out /photoprism/certs/cert.crt -days 365 -sha256 -extfile /photoprism/certs/cert.conf
+openssl pkcs12 -export -in /photoprism/certs/cert.crt -inkey /photoprism/certs/cert.key \
+        -out /photoprism/certs/cert.pfx -passout pass:
 
 # download service config
-curl -fsSL https://dl.photoprism.org/docker/cloud-init/docker-compose.yml> /photoprism/docker-compose.yml
+curl -fsSL https://dl.photoprism.org/docker/cloud-init/docker-compose.yml > /photoprism/docker-compose.yml
 curl -fsSL https://dl.photoprism.org/docker/cloud-init/jobs.ini > /photoprism/jobs.ini
+curl -fsSL https://dl.photoprism.org/docker/cloud-init/traefik.yaml > /photoprism/traefik.yaml
 chown -Rf photoprism:photoprism /photoprism
 
 # start services using docker-compose
