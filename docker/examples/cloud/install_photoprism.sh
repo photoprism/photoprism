dc8451dc3da9d638253baa65479440b9ed3a92ec
Michael Mayer <michael@photoprism.app>
2021-08-06 01:36:34 +0200

    Docker: Add scripts and packer config for DigitalOcean

--- docker/examples/cloud/install_photoprism.sh
+++ docker/examples/cloud/install_photoprism.sh
@@ -9,21 +9,15 @@
 #
 # so that it runs once when the server is booting for the first time:
 #
-#   apt update && apt dist-upgrade && apt autoclean && apt autoremove
 #   cd /var/lib/cloud/scripts/per-instance
 #   curl -fsSL https://dl.photoprism.org/docker/cloud/install_photoprism.sh > install_photoprism.sh
-#   curl -fsSL https://dl.photoprism.org/docker/cloud/enable_firewall.sh > enable_firewall.sh
-#   chmod 700 install_photoprism.sh enable_firewall.sh
-#   ./enable_firewall.sh
+#   chmod 700 install_photoprism.sh
 #
-# Installing the ufw firewall as shown above is optional but recommended.
+# To create a reusable image for DigitalOcean:
 #
-# When building a reusable image for DigitalOcean, you should run the following commands next:
+#   packer build digitalocean.json
 #
-#   bash <(curl -s https://raw.githubusercontent.com/digitalocean/marketplace-partners/master/scripts/90-cleanup.sh)
-#   rm -f /var/log/kern.log /var/log/ufw.log
-#   bash <(curl -s https://raw.githubusercontent.com/digitalocean/marketplace-partners/master/scripts/99-img-check.sh)
-#   shutdown -h now
+# Download packer from https://www.packer.io/downloads if not installed yet.
 #
 # Enjoy!
 
