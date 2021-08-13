6726a9ef2f6df3cf970f6eff0527de33bf625550
Michael Mayer <michael@photoprism.app>
2021-08-13 20:50:28 +0200

    Frontend: Fix "Português de Portugal" translation #1470

--- frontend/src/options/options.js
+++ frontend/src/options/options.js
@@ -147,14 +147,14 @@ export const Languages = () => [
     value: "pl",
   },
   {
+    text: "Português de Portugal",
+    translated: $gettext("Português de Portugal"),
+    value: "pt",
+  },
+  {
     text: "Português do Brasil",
     translated: $gettext("Brazilian Portuguese"),
     value: "pt_BR",
-  },
-  {
-    text: "Português de Portugal",
-    translated: $gettext("Português de Portugal"),
-    value: "pt_PT",
   },
   {
     text: "Русский",
