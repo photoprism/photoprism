2761dc2696a5dc5ef981fefba6c57ccb43528daa
Michael Mayer <michael@photoprism.app>
2021-08-13 23:07:17 +0200

    CI: Install qemu packages in scripts/install-qemu.sh #1158

--- scripts/install-qemu.sh
+++ scripts/install-qemu.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+apt-get update && apt-get -qq install -y qemu binfmt-support qemu-user-static
+
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 sleep 10
