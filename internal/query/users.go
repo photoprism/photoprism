4d915af2b5c57a32007ed0e3a0ca5c4230ba86b0
Michael Mayer <michael@photoprism.app>
2021-08-20 00:13:49 +0200

    Auth: Remove DeleteUserByName() #98

--- internal/query/users.go
+++ internal/query/users.go
@@ -1,26 +1,8 @@
 package query
 
 import (
-	"fmt"
-
 	"github.com/photoprism/photoprism/internal/entity"
-	"github.com/photoprism/photoprism/pkg/txt"
 )
-
-// DeleteUserByName deletes an existing user or returns error if not found.
-func DeleteUserByName(userName string) error {
-	if userName == "" {
-		return fmt.Errorf("can not delete user from db: empty username")
-	}
-	user := entity.FindUserByName(userName)
-	if err := Db().Where("user_name = ?", userName).Delete(&entity.User{}).Error; user == nil || err != nil {
-		return fmt.Errorf("user %s not found", txt.Quote(userName))
-	}
-	if err := Db().Where("uid = ?", user.UserUID).Delete(&entity.Password{}).Error; err != nil {
-		log.Debug(err)
-	}
-	return nil
-}
 
 // RegisteredUsers finds all registered users.
 func RegisteredUsers() (result entity.Users) {
--- internal/query/users_test.go
+++ internal/query/users_test.go
@@ -3,32 +3,8 @@ package query
 import (
 	"testing"
 
-	"github.com/photoprism/photoprism/internal/entity"
 	"github.com/stretchr/testify/assert"
 )
-
-func TestDeleteUserByName(t *testing.T) {
-	u := entity.FirstOrCreateUser(&entity.User{
-		ID:           877,
-		AddressID:    1,
-		UserName:     "delete",
-		FullName:     "Delete",
-		PrimaryEmail: "delete@example.com",
-	})
-
-	t.Run("delete empty username", func(t *testing.T) {
-		err := DeleteUserByName("")
-		assert.Error(t, err)
-	})
-	t.Run("delete fail", func(t *testing.T) {
-		err := DeleteUserByName("notmatching")
-		assert.Error(t, err)
-	})
-	t.Run("delete success", func(t *testing.T) {
-		err := DeleteUserByName(u.UserName)
-		assert.Nil(t, err)
-	})
-}
 
 func TestRegisteredUsers(t *testing.T) {
 	t.Run("success", func(t *testing.T) {
