feb0c557c1c12ec8fac62e9d409adb2499b082df
Michael Mayer <michael@photoprism.app>
2021-08-31 18:08:37 +0200

    Docker: Update multi-arch development image and config #1158
    Now comes with Go 1.17 pre-installed. To match production
    docker-compose.yml examples, the development database was
    renamed from "photoprism-db" to "mariadb".

--- scripts/install-qemu.sh
+++ scripts/install-qemu.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get update && apt-get -qq install -y qemu binfmt-support qemu-user-static qemu-system-arm qemu-efi-aarch64
+sudo apt-get update && sudo apt-get -qq install -y qemu binfmt-support qemu-user-static qemu-system-arm qemu-efi-aarch64
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
