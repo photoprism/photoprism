e7db639c5479fca378593c37143750798b2423ff
Timo Volkmann <git@timovolkmann.de>
2021-09-01 10:33:27 +0200

    Auth: Add http.Client Logger #782

--- internal/oidc/client.go
+++ internal/oidc/client.go
@@ -1,6 +1,7 @@
 package oidc
 
 import (
+	"bytes"
 	"errors"
 	"github.com/caos/oidc/pkg/client/rp"
 	"github.com/caos/oidc/pkg/oidc"
@@ -8,6 +9,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/photoprism/photoprism/internal/event"
 	"github.com/photoprism/photoprism/pkg/rnd"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"path"
@@ -23,9 +25,40 @@ type Client struct {
 
 const (
 	RedirectPath = "/auth/callback"
-	hashKey = "hashkey012345678"
-	encrKey = "encrkey012345678"
+	hashKey      = "hashkey012345678"
+	encrKey      = "encrkey012345678"
 )
+
+// This type implements the http.RoundTripper interface
+type LoggingRoundTripper struct {
+	Proxied http.RoundTripper
+}
+
+func (lrt LoggingRoundTripper) RoundTrip(req *http.Request) (res *http.Response, e error) {
+	// Do "before sending requests" actions here.
+	log.Debugf("Sending request to %v\n", req.URL)
+
+	// Send the request, get the response (or the error)
+	res, e = lrt.Proxied.RoundTrip(req)
+
+	// Handle the result.
+	if e != nil {
+		log.Errorf("Error: %v", e)
+	} else {
+		log.Debugf("Received %v response\n", res.Status)
+		all, err := ioutil.ReadAll(ioutil.NopCloser(res.Body))
+		if err != nil {
+			log.Errorf("Error: %v", err)
+		}
+		//io.Teereader, io.MultiReader()
+		log.Debugf("Body\n%s\n", all)
+
+		res.Body = ioutil.NopCloser(bytes.NewReader(all))
+	}
+
+	log.Debugf("END\n")
+	return
+}
 
 func NewClient(iss *url.URL, clientId, clientSecret, siteUrl string) *Client {
 	u, err := url.Parse(siteUrl)
@@ -37,9 +70,15 @@ func NewClient(iss *url.URL, clientId, clientSecret, siteUrl string) *Client {
 
 	cookieHandler := utils.NewCookieHandler([]byte(hashKey), []byte(encrKey), utils.WithUnsecure())
 
+	cl := &http.Client{
+		Transport: LoggingRoundTripper{http.DefaultTransport},
+	}
+
 	options := []rp.Option{
+		rp.WithHTTPClient(cl),
 		rp.WithCookieHandler(cookieHandler),
 		rp.WithVerifierOpts(rp.WithIssuedAtOffset(5 * time.Second)),
+		//rp.WithPKCE(cookieHandler),
 	}
 
 	scopes := strings.Split("openid profile email", " ")
@@ -52,6 +91,7 @@ func NewClient(iss *url.URL, clientId, clientSecret, siteUrl string) *Client {
 		log.Errorf("error creating provider: %s", err.Error())
 	}
 	log.Debug(provider)
+	log.Debugf("PKCE enabled: %v", provider.IsPKCE())
 
 	return &Client{
 		provider,
@@ -72,6 +112,9 @@ func (c *Client) AuthUrl() string {
 	tempstate = state()
 	return rp.AuthURL(tempstate, c)
 }
+func (c *Client) Available() bool {
+	return c.RelyingParty != nil
+}
 
 func (c *Client) CodeExchangeUserInfo(ctx *gin.Context) (oidc.UserInfo, error) {
 	var userinfo oidc.UserInfo
@@ -85,7 +128,16 @@ func (c *Client) CodeExchangeUserInfo(ctx *gin.Context) (oidc.UserInfo, error) {
 		userinfo = info
 	}
 
+	//you could also just take the access_token and id_token without calling the userinfo endpoint:
+	//
+	//tokeninfoClosure := func(w http.ResponseWriter, r *http.Request, tokens *oidc.Tokens, state string, rp rp.RelyingParty) {
+	//	log.Infof("IDTOKEN: %q\n\n" , tokens.IDToken)
+	//	log.Infof("ACCESSTOKEN: %q\n\n" , tokens.AccessToken)
+	//	log.Infof("REFRESHTOKEN: %q\n\n" , tokens.RefreshToken)
+	//}
+
 	handle := rp.CodeExchangeHandler(rp.UserinfoCallback(userinfoClosure), c)
+	//handle := rp.CodeExchangeHandler(tokeninfoClosure, c)
 	handle(ctx.Writer, ctx.Request)
 
 	log.Debugf("current request state: %v", ctx.Writer.Status())
