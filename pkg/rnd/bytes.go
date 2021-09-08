7e754e8cfa1ac65b0b62ebc64d478a531ec551fd
Timo Volkmann <git@timovolkmann.de>
2021-09-08 14:21:57 +0200

    Auth: Add simple random []byte seed #98

---
+++ pkg/rnd/bytes.go
@@ -0,0 +1,12 @@
+package rnd
+
+import "crypto/rand"
+
+func RandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
