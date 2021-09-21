c0eba718c933fff49cd25e72d2bdfee30d5618a4
Michael Mayer <michael@photoprism.app>
2021-09-21 16:57:52 +0200

    Docker: Upgrade Go in development base image
    Go 1.17.1 (released 2021-09-09) includes a security fix to the
    archive/zip package, as well as bug fixes to the compiler, linker,
    the go command, and to the crypto/rand, embed, go/types, html/template,
    and net/http packages (see https://golang.org/doc/devel/release#go1.17)

--- docker/scripts/install-go.sh
+++ docker/scripts/install-go.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-GOLANG_VERSION=1.17
+GOLANG_VERSION=1.17.1
 
 if [[ -z $1 ]]; then
     echo "architecture required: amd64, arm64, or arm" 1>&2
@@ -11,13 +11,13 @@ else
     set -eux;
     if [[ $1 == "amd64" ]]; then
         URL="https://golang.org/dl/go${GOLANG_VERSION}.linux-$1.tar.gz"
-        CHECKSUM="6bf89fc4f5ad763871cf7eac80a2d594492de7a818303283f1366a7f6a30372d *go.tgz"
+        CHECKSUM="dab7d9c34361dc21ec237d584590d72500652e7c909bf082758fb63064fca0ef *go.tgz"
     elif [[ $1 == "arm64" ]]; then
         URL="https://golang.org/dl/go${GOLANG_VERSION}.linux-$1.tar.gz"
-        CHECKSUM="01a9af009ada22122d3fcb9816049c1d21842524b38ef5d5a0e2ee4b26d7c3e7 *go.tgz"
+        CHECKSUM="53b29236fa03ed862670a5e5e2ab2439a2dc288fe61544aa392062104ac0128c *go.tgz"
     elif [[ $1 == "arm" ]]; then
         URL="https://golang.org/dl/go${GOLANG_VERSION}.linux-armv6l.tar.gz"
-        CHECKSUM="ae89d33f4e4acc222bdb04331933d5ece4ae71039812f6ccd7493cb3e8ddfb4e *go.tgz"
+        CHECKSUM="ed3e4dbc9b80353f6482c441d65b51808290e94ff1d15d56da5f4a7be7353758 *go.tgz"
     else
         echo "unsupported architecture" 1>&2
         exit 1
