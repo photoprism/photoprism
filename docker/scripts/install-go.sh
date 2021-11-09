ff19ab89513cf55eb55f80a3dfb6adbb1c4b9000
Michael Mayer <michael@photoprism.app>
2021-11-09 15:02:21 +0100

    Go: Upgrade from 1.17.2 to 1.17.3

--- docker/scripts/install-go.sh
+++ docker/scripts/install-go.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-GOLANG_VERSION=1.17.2
+GOLANG_VERSION=1.17.3
 
 if [[ -z $1 ]]; then
     echo "architecture required: amd64, arm64, or arm" 1>&2
@@ -11,13 +11,13 @@ else
     set -eux;
     if [[ $1 == "amd64" ]]; then
         URL="https://golang.org/dl/go${GOLANG_VERSION}.linux-$1.tar.gz"
-        CHECKSUM="f242a9db6a0ad1846de7b6d94d507915d14062660616a61ef7c808a76e4f1676 *go.tgz"
+        CHECKSUM="550f9845451c0c94be679faf116291e7807a8d78b43149f9506c1b15eb89008c *go.tgz"
     elif [[ $1 == "arm64" ]]; then
         URL="https://golang.org/dl/go${GOLANG_VERSION}.linux-$1.tar.gz"
-        CHECKSUM="a5a43c9cdabdb9f371d56951b14290eba8ce2f9b0db48fb5fc657943984fd4fc *go.tgz"
+        CHECKSUM="06f505c8d27203f78706ad04e47050b49092f1b06dc9ac4fbee4f0e4d015c8d4 *go.tgz"
     elif [[ $1 == "arm" ]]; then
         URL="https://golang.org/dl/go${GOLANG_VERSION}.linux-armv6l.tar.gz"
-        CHECKSUM="04d16105008230a9763005be05606f7eb1c683a3dbf0fbfed4034b23889cb7f2 *go.tgz"
+        CHECKSUM="aa0d5516c8bd61654990916274d27491cfa229d322475502b247a8dc885adec5 *go.tgz"
     else
         echo "unsupported architecture" 1>&2
         exit 1
