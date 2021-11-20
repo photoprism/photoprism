85fd1f0b716e0948371bd2c81ba40b41bcad0067
Michael Mayer <michael@photoprism.app>
2021-11-20 16:37:55 +0100

    Backend: Code clean-up

--- internal/entity/album.go
+++ internal/entity/album.go
@@ -10,9 +10,9 @@ import (
 	"github.com/gosimple/slug"
 	"github.com/jinzhu/gorm"
 
-	"github.com/photoprism/photoprism/internal/maps"
 	"github.com/photoprism/photoprism/internal/event"
 	"github.com/photoprism/photoprism/internal/form"
+	"github.com/photoprism/photoprism/internal/maps"
 	"github.com/photoprism/photoprism/pkg/rnd"
 	"github.com/photoprism/photoprism/pkg/txt"
 	"github.com/ulule/deepcopier"
