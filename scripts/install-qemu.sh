eae3817381859c6aa4d5eed6645380782b5aabcb
Michael Mayer <michael@photoprism.app>
2021-12-10 08:51:52 +0100

    Scripts: Add qemu packages in install-qemu.sh

--- scripts/install-qemu.sh
+++ scripts/install-qemu.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sudo apt-get update && sudo apt-get -qq install -y qemu binfmt-support qemu-user-static qemu-system-arm qemu-efi-aarch64
+sudo apt-get update && sudo apt-get -qq install -y \
+  qemu binfmt-support qemu-kvm qemu-system qemu-user qemu-utils \
+  qemu-efi-arm qemu-efi-aarch64 libvirt-daemon-driver-qemu
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
