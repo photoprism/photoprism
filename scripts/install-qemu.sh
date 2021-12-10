3d1a96fd0a4fb473fe0f768527cbc05e89b68976
Michael Mayer <michael@photoprism.app>
2021-12-10 09:01:41 +0100

    Scripts: Add qemu-user-binfmt and qemu-user-static to install-qemu.sh

--- scripts/install-qemu.sh
+++ scripts/install-qemu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo apt-get update && sudo apt-get -qq install -y \
-  qemu binfmt-support qemu-kvm qemu-system qemu-user qemu-utils \
+sudo apt-get update && sudo apt-get -qq install -y qemu binfmt-support qemu-kvm qemu-system \
+  qemu-user qemu-user-binfmt qemu-user-static qemu-utils \
   qemu-efi-arm qemu-efi-aarch64 libvirt-daemon-driver-qemu
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
