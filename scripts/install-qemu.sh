05087b6d1b9084667dd6815b94a51d3c2de8626e
Michael Mayer <michael@liquidbytes.net>
2021-12-11 18:32:03 +0100

    Update install-qemu.sh

--- scripts/install-qemu.sh
+++ scripts/install-qemu.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 sudo apt-get update && sudo apt-get -qq install -y qemu binfmt-support qemu-kvm qemu-system \
-  qemu-user qemu-user-binfmt qemu-user-static qemu-utils \
-  qemu-efi-arm qemu-efi-aarch64 libvirt-daemon-driver-qemu
+  qemu-user qemu-user-binfmt qemu-utils qemu-efi-arm qemu-efi-aarch64
 
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+# sudo apt-get update && sudo apt-get -qq install -y qemu-user-binfmt
+# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 sleep 10
