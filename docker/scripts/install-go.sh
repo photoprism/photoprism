068feb58fc09b37c1db308bc588ac493cfc88840
Michael Mayer <michael@photoprism.app>
2021-12-18 15:40:30 +0100

    Docker: Fix Go download URL in docker/scripts/install-go.sh

--- docker/scripts/install-go.sh
+++ docker/scripts/install-go.sh
@@ -10,10 +10,10 @@ if [[ -z $1 ]]; then
 else
     set -eux;
     if [[ $1 == "amd64" ]]; then
-        URL="https://go.dev/dl/go${GOLANG_VERSION}.linux-$1.tar.gz"
+        URL="https://go.dev/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz"
         CHECKSUM="bd78114b0d441b029c8fe0341f4910370925a4d270a6a590668840675b0c653e *go.tgz"
     elif [[ $1 == "arm64" ]]; then
-        URL="https://go.dev/dl/go{GOLANG_VERSION}.linux-$1.tar.gz"
+        URL="https://go.dev/dl/go${GOLANG_VERSION}.linux-arm64.tar.gz"
         CHECKSUM="6f95ce3da40d9ce1355e48f31f4eb6508382415ca4d7413b1e7a3314e6430e7e *go.tgz"
     elif [[ $1 == "arm" ]]; then
         URL="https://go.dev/dl/go${GOLANG_VERSION}.linux-armv6l.tar.gz"
