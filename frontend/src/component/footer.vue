8512a86d4e811ce5b0bd01c1d7e5818b337edf82
Michael Mayer <michael@photoprism.app>
2022-01-17 09:23:55 +0100

    Frontend: Update footer

--- frontend/src/component/footer.vue
+++ frontend/src/component/footer.vue
@@ -4,11 +4,11 @@
       <v-layout wrap align-top pt-3>
         <v-flex xs12 sm6 class="px-0 pb-2 body-1 text-selectable">
           PhotoPrism® {{ $config.get("version") }}<br>
-          <router-link to="/about">© 2018-2022 Michael Mayer</router-link>
+          <router-link to="/about">100% self-funded and independent</router-link>
         </v-flex>
 
         <v-flex xs12 sm6 class="px-0 pb-2 body-1 text-xs-left text-sm-right">
-          <a href="https://photoprism.app/team/" target="_blank">100% self-funded and independent</a>
+          <a href="https://photoprism.app/team/" target="_blank">© 2018-2022 Michael Mayer</a>
           <br>
           <a href="https://raw.githubusercontent.com/photoprism/photoprism/develop/NOTICE"
              target="_blank">
