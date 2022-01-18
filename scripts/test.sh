3e44f44262229849c00ca30552d4affab6a92104
Michael Mayer <michael@photoprism.app>
2022-01-18 18:14:57 +0100

    CI: Update test script #1953

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,5 +8,5 @@ scripts/docker/login.sh
 # Run tests
 docker-compose -f docker-compose.ci.yml pull
 docker-compose -f docker-compose.ci.yml build
-trap "docker rm -f -v photoprism-test " INT TERM
+trap "docker rm -f -v photoprism-test" INT TERM
 docker-compose -f docker-compose.ci.yml run --name photoprism-test --rm -T photoprism make all test install migrate
