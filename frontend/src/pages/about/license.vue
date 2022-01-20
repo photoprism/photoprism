7d628ef89624b93bf59e4980a2cf83318ae652a5
Michael Mayer <michael@photoprism.app>
2022-01-20 18:44:07 +0100

    Frontend: Update license.vue
    Removes "How to Apply These Terms to Your New Programs"

--- frontend/src/pages/about/license.vue
+++ frontend/src/pages/about/license.vue
@@ -663,49 +663,7 @@
 
       <p>END OF TERMS AND CONDITIONS</p>
 
-      <h3><a name="howto"></a>How to Apply These Terms to Your New Programs</h3>
-
-      <p>If you develop a new program, and you want it to be of the greatest
-        possible use to the public, the best way to achieve this is to make it
-        free software which everyone can redistribute and change under these terms.</p>
-
-      <p>To do so, attach the following notices to the program. It is safest
-        to attach them to the start of each source file to most effectively
-        state the exclusion of warranty; and each file should have at least
-        the "copyright" line and a pointer to where the full notice is found.</p>
-
-      <pre>    &lt;one line to give the program's name and a brief idea of what it does.&gt;
-    Copyright (C) &lt;year&gt;  &lt;name of author&gt;
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see &lt;https://www.gnu.org/licenses/&gt;.
-</pre>
-
-      <p class="mt-3">Also add information on how to contact you by electronic and paper mail.</p>
-
-      <p>If your software can interact with users remotely through a computer
-        network, you should also make sure that it provides a way for users to
-        get its source. For example, if your program is a web application, its
-        interface could display a "Source" link that leads users to an archive
-        of the code. There are many ways you could offer source, and different
-        solutions will be better for different programs; see section 13 for the
-        specific requirements.</p>
-
-      <p>You should also get your employer (if you work as a programmer) or school,
-        if any, to sign a "copyright disclaimer" for the program, if necessary.
-        For more information on this, and how to apply and follow the GNU AGPL, see
-        &lt;<a target="_blank" href="https://www.gnu.org/licenses/">https://www.gnu.org/licenses/</a>&gt;.</p>
-
+      <h3 style="text-align: center;"><a name="additional-terms"></a>ADDITIONAL TERMS</h3>
       <h3><a name="trademark"></a>PhotoPrism® Trademark</h3>
 
       <p>
