ef2b07f8d53100622a4e0dffd3c12278256d3d42
Michael Mayer <michael@photoprism.app>
2022-02-16 18:13:18 +0100

    Docker: Add cleanup script for preview and release builds

---
+++ docker/scripts/cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexit
+
+if [[ ! -d /tmp ]]; then
+  mkdir /tmp
+fi
+chmod 1777 /tmp
+
+apt-get -y autoremove
+apt-get -y autoclean
+rm -rf /var/lib/apt/lists/*
+rm -rf /tmp/* /var/tmp/*
+history -c
+cat /dev/null > /root/.bash_history
+unset HISTFILE
+find /var/log -mtime -1 -type f -exec truncate -s 0 {} \;
+rm -rf /var/log/*.gz /var/log/*.log /var/log/*.[0-9] /var/log/*-????????
+rm -rf /var/lib/cloud/instances/*
+rm -f /root/.ssh/* /etc/ssh/*key*
