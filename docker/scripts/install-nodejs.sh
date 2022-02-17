c1222b2572224eb9986fe62db4cd26265fe0a565
Michael Mayer <michael@photoprism.app>
2022-02-17 13:54:42 +0100

    Docker: Add install-nodejs.sh script

---
+++ docker/scripts/install-nodejs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# abort if the user is not root
+if [[ $(id -u) != "0" ]]; then
+  echo "Usage: run install-nodejs.sh as root" 1>&2
+  exit 1
+fi
+
+# install from nodesource.com
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
+apt-get update && apt-get -qq install nodejs
+npm install --unsafe-perm=true --allow-root -g npm
+npm config set cache ~/.cache/npm
