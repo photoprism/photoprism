b52bf1bd76f204ae24a70514a378bf9ec9d3a9b5
Michael Mayer <michael@photoprism.app>
2022-02-19 21:38:07 +0100

    Docker: Improve doctor.sh script

--- scripts/dist/doctor.sh
+++ scripts/dist/doctor.sh
@@ -5,5 +5,5 @@ STORAGE_PATH=${PHOTOPRISM_STORAGE_PATH:-/photoprism/storage}
 URL="https://docs.photoprism.app/getting-started/troubleshooting/docker/#file-permissions"
 
 # shellcheck disable=SC2028
-touch "${STORAGE_PATH}/.writable" 2>/dev/null || \
+(touch "${STORAGE_PATH}/.writable" 2>/dev/null && rm "${STORAGE_PATH}/.writable") || \
   (printf "The storage folder \"%s\" is not writable, please fix filesystem permissions: %s\n" "$STORAGE_PATH" "$URL"; exit 1)
