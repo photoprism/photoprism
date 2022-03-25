eaecedf4bb9093ef0be81c33939cb16ac6cd0300
Michael Mayer <michael@photoprism.app>
2022-03-25 10:14:48 +0100

    Docker: Update scripts/dist/tensorflow-driver.sh

--- scripts/dist/tensorflow-driver.sh
+++ scripts/dist/tensorflow-driver.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 PATH="/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin:/scripts"
-CPU_DETECTED=$(/usr/bin/lshw -c processor -json 2>/dev/null)
+
+CPU_DETECTED=$(lshw -c processor -json 2>/dev/null)
 
 if [[ $(echo "${CPU_DETECTED}" | jq -r '.[].capabilities.avx2') == "true" ]]; then
   echo "avx2"
