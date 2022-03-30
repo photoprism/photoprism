68094e9b79da3b3d78ef800158b5e9c1eb84a613
Michael Mayer <michael@photoprism.app>
2022-03-30 20:25:24 +0200

    Sharing: Fix UI component import paths #2199

--- frontend/src/share/components.js
+++ frontend/src/share/components.js
@@ -23,27 +23,31 @@ Additional information can be found in our Developer Guide:
 
 */
 
-import PNotify from "component/notify.vue";
 import PNavigation from "navigation.vue";
+
+import PNotify from "component/notify.vue";
 import PScrollTop from "component/scroll-top.vue";
 import PLoadingBar from "component/loading-bar.vue";
 import PVideoPlayer from "component/video/player.vue";
 import PPhotoViewer from "component/photo/viewer.vue";
-import PPhotoCards from "component/photo/cards.vue";
-import PPhotoMosaic from "component/photo/mosaic.vue";
-import PPhotoList from "component/photo/list.vue";
-import PPhotoClipboard from "component/photo/clipboard.vue";
-import PAlbumClipboard from "component/album/clipboard.vue";
+
+import PPhotoCards from "photo/cards.vue";
+import PPhotoMosaic from "photo/mosaic.vue";
+import PPhotoList from "photo/list.vue";
+import PPhotoClipboard from "photo/clipboard.vue";
+import PAlbumClipboard from "album/clipboard.vue";
 
 const components = {};
 
 components.install = (Vue) => {
-  Vue.component("PNotify", PNotify);
   Vue.component("PNavigation", PNavigation);
+
+  Vue.component("PNotify", PNotify);
   Vue.component("PScrollTop", PScrollTop);
   Vue.component("PLoadingBar", PLoadingBar);
   Vue.component("PVideoPlayer", PVideoPlayer);
   Vue.component("PPhotoViewer", PPhotoViewer);
+
   Vue.component("PPhotoCards", PPhotoCards);
   Vue.component("PPhotoMosaic", PPhotoMosaic);
   Vue.component("PPhotoList", PPhotoList);
