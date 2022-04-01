b07ec6429a1079413f756a8c878febaf7feb07ab
theresa <theresagresch@gmail.com>
2022-04-01 16:25:37 +0200

    Tests: Add unit tests for search

---
+++ internal/search/photos_filter_subject_test.go
@@ -0,0 +1,492 @@
+package search
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/photoprism/photoprism/internal/form"
+)
+
+func TestPhotosFilterSubject(t *testing.T) {
+	t.Run("StartsWithPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "%gold"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("CenterPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "I love % dog"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("EndsWithPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "sale%"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	//TODO error
+	/*t.Run("StartsWithAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "&IlikeFood"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})*/
+	t.Run("CenterAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "Pets & Dogs"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	//TODO error
+	/*t.Run("EndsWithAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "Light&"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})*/
+	t.Run("StartsWithSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "'Family"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("CenterSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "Father's type"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("EndsWithSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "Ice Cream'"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("StartsWithAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "*Forrest"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("CenterAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "My*Kids"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("EndsWithAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "Yoga***"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("StartsWithPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "|Banana"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("CenterPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "Red|Green"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("EndsWithPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "Blue|"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("StartsWithNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "345 Shirt"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("CenterNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "type555 Blue"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("EndsWithNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Subject = "Route 66"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+}
+
+func TestPhotosQuerySubject(t *testing.T) {
+	t.Run("StartsWithPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"%gold\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("CenterPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"I love % dog\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("EndsWithPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"sale%\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	//TODO error
+	/*t.Run("StartsWithAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"&IlikeFood\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})*/
+	t.Run("CenterAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"Pets & Dogs\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	//TODO error
+	/*t.Run("EndsWithAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"Light&\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})*/
+	t.Run("StartsWithSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"'Family\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("CenterSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"Father's type\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("EndsWithSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"Ice Cream'\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("StartsWithAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"*Forrest\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("CenterAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"My*Kids\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("EndsWithAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"Yoga***\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("StartsWithPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"|Banana\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("CenterPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"Red|Green\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("EndsWithPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"Blue|\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("StartsWithNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"345 Shirt\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("CenterNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"type555 Blue\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("EndsWithNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "subject:\"Route 66\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 0)
+	})
+}
---
+++ internal/search/photos_filter_title_test.go
@@ -0,0 +1,675 @@
+package search
+
+import (
+	"testing"
+
+	"github.com/photoprism/photoprism/internal/form"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPhotosFilterTitle(t *testing.T) {
+	t.Run("StartsWithPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "%Salad"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("CenterPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "hon%ey"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("EndsWithPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "cloud%"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("StartsWithAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "&dad"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("CenterAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "leader&ship"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("EndsWithAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "mom&"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("StartsWithSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "'poetry'"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("CenterSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "amaz'ing"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("EndsWithSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "pollution'"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("StartsWithAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "*area"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("CenterAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "farm*animal"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("EndsWithAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "engine*"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("StartsWithPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "|football"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Greater(t, len(photos), 1)
+	})
+	t.Run("CenterPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "pain|ting"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, 0, len(photos))
+	})
+	t.Run("EndsWithPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "supermarket|"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Greater(t, len(photos), 1)
+	})
+	t.Run("StartsWithNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "123community"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("CenterNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "cli44mate"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("EndsWithNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "guest456"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("StartsWithDoubleQuotes", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "\"member"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("CenterDoubleQuotes", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "sol\"ution"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("EndsWithDoubleQuotes", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "desk\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	//TODO should work
+	/*t.Run("OrSearch", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "guest456 | engine*"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 2)
+	})
+	t.Run("OrSearch2", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "guest456 | amaz'ing"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Greater(t, len(photos), 1)
+	})
+	t.Run("OrSearch3", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Title = "farm*animal | amaz'ing"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Greater(t, len(photos), 1)
+	})*/
+}
+
+func TestPhotosQueryTitle(t *testing.T) {
+	t.Run("StartsWithPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"%Salad\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("CenterPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"hon%ey\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("EndsWithPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"cloud%\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("StartsWithAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"&dad\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("CenterAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"leader&ship\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("EndsWithAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"mom&\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("StartsWithSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"'poetry'\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("CenterSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"amaz'ing\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("EndsWithSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"pollution'\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("StartsWithAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"*area\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("CenterAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"farm*animal\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("EndsWithAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"engine*\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("StartsWithPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"|football\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		//TODO
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("CenterPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"pain|ting\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, 0, len(photos))
+	})
+	t.Run("EndsWithPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"supermarket|\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		//TODO
+		assert.Equal(t, len(photos), 0)
+	})
+	t.Run("StartsWithNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"123community\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("CenterNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"cli44mate\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("EndsWithNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"guest456\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), 1)
+	})
+	t.Run("StartsWithDoubleQuotes", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"\"member\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		// TODO Finds all?
+		assert.Greater(t, len(photos), 1)
+	})
+	t.Run("CenterDoubleQuotes", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"sol\"ution\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		// TODO Finds all?
+		assert.Greater(t, len(photos), 1)
+	})
+	t.Run("EndsWithDoubleQuotes", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"desk\"\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		// TODO Finds all?
+		assert.Greater(t, len(photos), 1)
+	})
+	//TODO should work
+	/*t.Run("OrSearch", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"guest456 | engine*\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), 2)
+	})
+	t.Run("OrSearch2", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"guest456 | amaz'ing\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Greater(t, len(photos), 1)
+	})
+	t.Run("OrSearch3", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "title:\"farm*animal | amaz'ing\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Greater(t, len(photos), 1)
+	})*/
+}
