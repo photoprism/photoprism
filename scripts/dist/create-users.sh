df6ca40a827443367a3b5c6a6ab2e5c68f88557c
Michael Mayer <michael@photoprism.app>
2022-04-18 19:12:26 +0200

    Docker: Add matching groups for default users #2262

--- scripts/dist/create-users.sh
+++ scripts/dist/create-users.sh
@@ -10,17 +10,29 @@ fi
 
 echo "Creating default users and groups..."
 
-groupadd -f -r -g 44 video
-groupadd -f -r -g 109 render
-groupadd -f -g 1000 photoprism
+groupadd -f -r -g 44 video 1>&2
+groupadd -f -r -g 109 render 1>&2
+
+groupdel -f 1000 >/dev/null 2>&1
+userdel -f photoprism >/dev/null 2>&1
+userdel -f 1000 >/dev/null 2>&1
+
+groupadd -f -g 1000 photoprism 1>&2
+useradd -N -o -u 1000 -g photoprism -G video,render -s /bin/bash -m -d "/home/photoprism" photoprism
 
 add_user()
 {
-  useradd -u "$1" -g photoprism -G video,render -s /bin/bash -m -d "/home/user-$1" "user-$1" 2>/dev/null
+  userdel -f "$1" >/dev/null 2>&1
+  groupdel -f "group-$1" >/dev/null 2>&1
+  groupdel -f "$1" >/dev/null 2>&1
+  groupadd -f -g "$1" "group-$1"
+  useradd -u "$1" -g "$1" -G photoprism,video,render -s /bin/bash -m -d "/home/user-$1" "user-$1" 2>/dev/null
 }
 
 for i in $(seq 50 99); do add_user "$i"; done
 for i in $(seq 500 549); do add_user "$i"; done
-for i in $(seq 1000 1099); do add_user "$i"; done
+for i in $(seq 1001 1099); do add_user "$i"; done
+
+chgrp -f -R 1000 /home
 
 echo "Done."
