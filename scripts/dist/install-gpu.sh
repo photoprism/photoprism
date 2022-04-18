7f32ff46e8222b7621604d49b5624a60c066b58b
Michael Mayer <michael@photoprism.app>
2022-04-18 14:30:47 +0200

    Video: Show what transcoding hardware was detected #2125 #2222 #2228
    Also installs the following, additional packages:
    - libcuda1
    - libnvcuvid1
    - mesa-va-drivers

--- scripts/dist/install-gpu.sh
+++ scripts/dist/install-gpu.sh
@@ -27,18 +27,22 @@ apt-get -qq install lshw jq
 # shellcheck disable=SC2207
 GPU_DETECTED=($(lshw -c display -json 2>/dev/null | jq -r '.[].configuration.driver'))
 
+echo "GPU detected: ${GPU_DETECTED[*]}"
+
 # shellcheck disable=SC2068
 for t in ${GPU_DETECTED[@]}; do
   case $t in
     i915)
-      apt-get -qq install intel-opencl-icd intel-media-va-driver-non-free i965-va-driver-shaders libmfx1 libva2 vainfo libva-wayland2
+      echo "Installing Intel Drivers..."
+      apt-get -qq install intel-opencl-icd intel-media-va-driver-non-free i965-va-driver-shaders mesa-va-drivers libmfx1 libva2 vainfo libva-wayland2
       ;;
 
     nvidia)
-      apt-get -qq install nvidia-opencl-icd nvidia-vdpau-driver nvidia-driver-libs nvidia-kernel-dkms libva2 vainfo libva-wayland2 libnvidia-encode1
+      echo "Installing Nvidia Drivers..."
+      apt-get -qq install libcuda1 libnvcuvid1 libnvidia-encode1 nvidia-opencl-icd nvidia-vdpau-driver nvidia-driver-libs nvidia-kernel-dkms libva2 vainfo libva-wayland2
       ;;
 
-    null)
+    "null")
       # ignore
       ;;
 
