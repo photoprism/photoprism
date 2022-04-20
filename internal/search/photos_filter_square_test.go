bc838fd92d5ee61881e425b3426f3df7f30f9401
theresa <theresagresch@gmail.com>
2022-04-20 14:48:55 +0200

    Tests: Add unit tests for landscape and square search filters #2169

---
+++ internal/search/photos_filter_square_test.go
@@ -0,0 +1,311 @@
+package search
+
+import (
+	"testing"
+
+	"github.com/photoprism/photoprism/internal/form"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPhotosQuerySquare(t *testing.T) {
+	var f0 form.SearchPhotos
+
+	f0.Query = "square:true"
+	f0.Merged = true
+
+	photos0, _, err := Photos(f0)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, len(photos0), 1)
+
+	t.Run("square:yes", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:yes"
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("StartsWithPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"%gold\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("CenterPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"I love % dog\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("EndsWithPercent", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"sale%\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("StartsWithAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"&IlikeFood\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("CenterAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"Pets & Dogs\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("EndsWithAmpersand", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"Light&\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("StartsWithSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"'Family\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("CenterSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"Mother's Day\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("EndsWithSingleQuote", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"Ice Cream'\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("StartsWithAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"*Forrest\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("CenterAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"My*Kids\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("EndsWithAsterisk", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"Yoga***\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("StartsWithPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"|Banana\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("CenterPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"Red|Green\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("EndsWithPipe", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"Blue|\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("StartsWithNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"345 Shirt\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("CenterNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"Color555 Blue\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("EndsWithNumber", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"Route 66\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("AndSearch", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"Route 66 & Father's Day\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+	t.Run("OrSearch", func(t *testing.T) {
+		var f form.SearchPhotos
+
+		f.Query = "square:\"Route %66 | *Father's Day\""
+		f.Merged = true
+
+		photos, _, err := Photos(f)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, len(photos), len(photos0))
+	})
+}
