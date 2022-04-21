7fac0f2931acdc79ede6de4229398f48e1ac8955
Michael Mayer <michael@photoprism.app>
2022-04-21 19:19:47 +0200

    CI: Update test.sh script

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,4 +10,5 @@ docker-compose -f docker-compose.ci.yml down --remove-orphans
 docker-compose -f docker-compose.ci.yml pull
 docker-compose -f docker-compose.ci.yml build
 trap "docker-compose -f docker-compose.ci.yml down --remove-orphans" INT TERM
-docker-compose -f docker-compose.ci.yml run --name photoprism-test --rm -T photoprism make all test install migrate
+docker-compose -f docker-compose.ci.yml run --rm -T photoprism make all test install migrate
+docker-compose -f docker-compose.ci.yml down --remove-orphans
