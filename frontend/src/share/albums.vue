3e33af7d1d5cc608f993162c20c62223c90f8095
heikomat <heiko.mathes@gmail.com>
2022-08-27 22:39:00 +0200

    fix too many images getting pre-loaded on filter change

--- frontend/src/share/albums.vue
+++ frontend/src/share/albums.vue
@@ -190,9 +190,6 @@ export default {
     }
   },
   created() {
-    if (!window.popStateDetected) {
-      this.setOffset(0);
-    }
     const token = this.$route.params.token;
 
     if (this.$session.hasToken(token)) {
@@ -385,6 +382,15 @@ export default {
       return params;
     },
     search() {
+      /**
+       * re-creating the last scroll-position should only ever happen when using
+       * back-navigation. We therefore reset the remembered scroll-position
+       * in any other scenario
+       */
+      if (!window.backwardsNavigationDetected) {
+        this.setOffset(0);
+      }
+
       this.scrollDisabled = true;
 
       // Don't query the same data more than once
