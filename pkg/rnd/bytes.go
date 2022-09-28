b32a970aaba649b3bc962b7dd608e907fdcdac0c
Michael Mayer <michael@photoprism.app>
2022-09-28 13:38:08 +0200

    Auth: Add RandomBytes() to generate secure random bytes #98 #782
    Signed-off-by: Michael Mayer <michael@photoprism.app>

---
+++ pkg/rnd/bytes.go
@@ -0,0 +1,13 @@
+package rnd
+
+import "crypto/rand"
+
+// RandomBytes returns cryptographically secure random bytes.
+func RandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	return b, nil
+}
