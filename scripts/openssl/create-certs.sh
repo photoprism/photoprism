155d96b149138b4997926868d81576eebbda33ed
Michael Mayer <michael@photoprism.app>
2022-10-19 20:25:10 +0200

    Security: Improved scripts for creating local TLS certificates
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- scripts/openssl/create-certs.sh
+++ scripts/openssl/create-certs.sh
@@ -2,18 +2,15 @@
 
 # shellcheck disable=SC2164
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-CERTS_PATH="${SCRIPT_PATH}/../../storage/config/certs"
-
-echo "OpenSSL Scripts: ${SCRIPT_PATH}"
-echo "HTTPS Cert Path: ${CERTS_PATH}"
+CERTS_PATH="${SCRIPT_PATH}/../../storage/config/certificates"
 
 mkdir -p "${CERTS_PATH}"
 
-openssl genrsa -out "$CERTS_PATH/local.key" 4096
+openssl genrsa -out "$CERTS_PATH/photoprism.local.key" 4096
 
-openssl req -new -config "$SCRIPT_PATH/openssl.conf" -key "$CERTS_PATH/local.key" -out "$CERTS_PATH/local.csr"
+openssl req -new -config "$SCRIPT_PATH/openssl.conf" -key "$CERTS_PATH/photoprism.local.key" -out "$CERTS_PATH/photoprism.local.csr"
 
-openssl x509 -req -in "$CERTS_PATH/local.csr" -CA "$CERTS_PATH/ca.pem" -CAkey "$CERTS_PATH/ca.key" -CAcreateserial \
--out "$CERTS_PATH/local.crt" -days 365 -sha256 -extfile "$SCRIPT_PATH/local.conf"
+openssl x509 -req -in "$CERTS_PATH/photoprism.local.csr" -CA "$CERTS_PATH/photoprism.local.issuer.pem" -CAkey "$CERTS_PATH/photoprism.local.issuer.key" -CAcreateserial \
+-out "$CERTS_PATH/photoprism.local.crt" -days 365 -sha256 -extfile "$SCRIPT_PATH/local.conf"
 
-openssl pkcs12 -export -in "$CERTS_PATH/local.crt" -inkey "$CERTS_PATH/local.key" -out "$CERTS_PATH/local.pfx" -passin pass: -passout pass:
+openssl pkcs12 -export -in "$CERTS_PATH/photoprism.local.crt" -inkey "$CERTS_PATH/photoprism.local.key" -out "$CERTS_PATH/photoprism.local.pfx" -passin pass: -passout pass:
