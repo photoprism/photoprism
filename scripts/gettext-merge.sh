74e020d5689bf53573370e3c3e4024a1653a2b56
Michael Mayer <michael@photoprism.app>
2022-12-13 04:29:32 +0100

    Translations: Update gettext-merge.sh script
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- scripts/gettext-merge.sh
+++ scripts/gettext-merge.sh
@@ -4,6 +4,6 @@ echo "Merging backend translations..."
 for file in ./assets/locales/**/*.po; do msgmerge --previous --no-fuzzy-matching --update "${file}" ./assets/locales/messages.pot; done
 
 echo "Merging frontend translations..."
-for file in ./frontend/src/locales/*.po; do msgmerge --previous --no-fuzzy-matching --update "${file}" ./frontend/src/locales/translations.pot; done
+for file in ./frontend/src/locales/*.po; do msgmerge --previous --no-fuzzy-matching --no-wrap --update "${file}" ./frontend/src/locales/translations.pot; done
 
 echo "Done."
