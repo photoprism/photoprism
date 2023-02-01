f91499fb05f34a005ff1dc2578813964c27bf299
Michael Mayer <michael@photoprism.app>
2023-02-01 13:01:32 +0100

    CI: Change gpg keyring path in install scripts
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- scripts/dist/install-caddy.sh
+++ scripts/dist/install-caddy.sh
@@ -8,7 +8,7 @@ set -e
 echo "Installing Caddy..."
 
 sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/caddy.gpg
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
 sudo apt update
 sudo apt install caddy
