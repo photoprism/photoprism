35651311216d28cc42322cb35f56f74adc463a51
Michael Mayer <michael@photoprism.app>
2023-02-19 18:21:32 +0100

    Advanced Settings: Change "Options" to "Global Options" #2207
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- frontend/src/page/settings/advanced.vue
+++ frontend/src/page/settings/advanced.vue
@@ -6,7 +6,7 @@
       <v-card flat tile class="mt-0 px-1 application">
         <v-card-title primary-title class="pb-0">
           <h3 class="body-2 mb-0">
-            <translate>Options</translate>
+            <translate>Global Options</translate>
           </h3>
         </v-card-title>
 
