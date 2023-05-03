b9181bee6fae127a1a394a975a714f17336df6a6
Michael Mayer <michael@photoprism.app>
2023-05-03 12:57:41 +0200

    Config: Update cli_flags_test.go
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- internal/config/cli_flags_test.go
+++ internal/config/cli_flags_test.go
@@ -16,10 +16,10 @@ func TestCliFlags_Cli(t *testing.T) {
 func TestCliFlags_Find(t *testing.T) {
 	cliFlags := Flags.Cli()
 	standard := Flags.Find([]string{})
-	sponsor := Flags.Find([]string{EnvSponsor})
+	essentials := Flags.Find([]string{Essentials})
 	other := Flags.Find([]string{"other"})
 
 	assert.Equal(t, len(standard), len(other))
-	assert.Equal(t, len(cliFlags), len(sponsor))
-	assert.Less(t, len(other), len(sponsor))
+	assert.Equal(t, len(cliFlags), len(essentials))
+	assert.Less(t, len(other), len(essentials))
 }
