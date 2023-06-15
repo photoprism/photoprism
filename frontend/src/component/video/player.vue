df0d93b1e492a37b6279490f38ee6e4dc7f8475e
Michael Mayer <michael@photoprism.app>
2023-06-15 18:16:42 +0200

    Videos: Play videos in full size (work in progress)
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- frontend/src/component/video/player.vue
+++ frontend/src/component/video/player.vue
@@ -85,7 +85,8 @@ export default {
       return this.$el.getElementsByTagName('video')[0];
     },
     updateStyle() {
-      this.style = `width: ${this.width.toString()}px; height: ${this.height.toString()}px`;
+      // this.style = `width: ${this.width.toString()}px; height: ${this.height.toString()}px`;
+      this.style = `width:100%; height: 100%`;
       this.$el.style.cssText = this.style;
     },
     render() {
