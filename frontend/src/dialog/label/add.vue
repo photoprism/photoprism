7f257e750032e4d9a0a19d1b6773cc9df69cf914
Reuben Dowle <reubendowle0@gmail.com>
2023-07-14 11:40:50 +1200

    Frontend: Add warning that batch operation applies to all selected files

--- frontend/src/dialog/label/add.vue
+++ frontend/src/dialog/label/add.vue
@@ -1,16 +1,37 @@
 <template>
-  <v-dialog :value="show" lazy persistent max-width="356" class="p-label-add-dialog" @keydown.esc="cancel">
+  <v-dialog :value="show" lazy persistent max-width="356" class="p-label-add-dialog" @keydown.esc="cancel" @keydown.enter="confirm">
     <v-card raised elevation="24">
+      <v-card-title primary-title class="pb-0">
+        <v-layout row wrap>
+          <v-flex xs8>
+            <h3 class="headline mb-0">
+              <translate>Add Label</translate>
+            </h3>
+          </v-flex>
+          <v-flex xs4 text-xs-right>
+            <v-icon>new_label</v-icon>
+          </v-flex>
+        </v-layout>
+      </v-card-title>
+
       <v-card-text class="pt-3 px-3">
         <v-layout row wrap>
           <v-flex xs9 text-xs-left align-self-center>
+              <v-layout row wrap>
+                <v-flex xs2>
+                  <v-icon>warning</v-icon>
+                </v-flex>
+                <v-flex xs10>
+                  <translate>Change is applied to all currently selected photos</translate>
+                </v-flex>
+              </v-layout>
             <v-text-field
                   v-model="label"
                   :rules="[nameRule]"
                   :label="$gettext('Label')"
                   color="secondary-dark"
                   class="input-rename background-inherit elevation-0"
-                  single-line autofocus solo hide-details
+                  autofocus hide-details
             ></v-text-field>
           </v-flex>
         </v-layout>
@@ -24,7 +45,7 @@
             <v-btn depressed color="primary-button"
                    class="action-confirm white--text compact mx-0"
                    @click.stop="confirm">
-              <translate>Add Label</translate>
+              <translate>Apply</translate>
             </v-btn>
           </v-flex>
         </v-layout>
--- frontend/src/dialog/photo/setlocation.vue
+++ frontend/src/dialog/photo/setlocation.vue
@@ -1,22 +1,49 @@
 <template>
-  <v-dialog :value="show" lazy persistent max-width="356" class="p-photo-set-location-dialog" @keydown.esc="cancel">
+  <v-dialog :value="show" persistent max-width="500" class="p-photo-set-location-dialog" @keydown.esc="cancel" @keydown.enter="confirm">
     <v-card raised elevation="24">
-      <v-card-text class="pt-3 px-3">
+      <v-card-title primary-title class="pb-0">
         <v-layout row wrap>
-          <v-flex xs9 text-xs-left align-self-center>
+          <v-flex xs10>
+            <h3 class="headline mb-0">
+              <translate>Set Location</translate>
+            </h3>
+          </v-flex>
+          <v-flex xs2 text-xs-right>
+            <v-icon>edit_location</v-icon>
+          </v-flex>
+        </v-layout>
+      </v-card-title>
+      <v-card-text fluid class="pt-3 px-3">
+        <v-layout fluid row wrap>
+          <v-layout row wrap>
+            <v-flex xs2>
+              <v-icon>warning</v-icon>
+            </v-flex>
+            <v-flex xs10>
+              <translate>Change is applied to all currently selected photos</translate>
+            </v-flex>
+          </v-layout>
+          <v-flex fluid text-xs-left align-self-center>
+            <v-container fluid fill-height>
+              <div id="map" style="width: 300px; height: 300px;">
+                <v-sheet fluid></v-sheet>
+              </div>
+            </v-container>
             <v-text-field
                   v-model="latitude"
                   :label="$gettext('Latitude')"
+                  placeholder=""
                   color="secondary-dark"
                   class="input-latitude background-inherit elevation-0"
-                  single-line autofocus solo hide-details
+                  autofocus hide-details
             ></v-text-field>
             <v-text-field
                   v-model="longitude"
                   :label="$gettext('Longitude')"
+                  placeholder=""
                   color="secondary-dark"
-                  class="input-longitude background-inherit elevation-0"
-                  single-line autofocus solo hide-details
+                  class="iniput-longitude background-inherit elevation-0"
+                  hide-details
             ></v-text-field>
           </v-flex>
         </v-layout>
@@ -30,7 +57,7 @@
             <v-btn depressed color="primary-button"
                    class="action-confirm white--text compact mx-0"
                    @click.stop="confirm">
-              <translate>Set Location</translate>
+              <translate>Apply</translate>
             </v-btn>
           </v-flex>
         </v-layout>
