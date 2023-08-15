3212724aae2f764d9746b6da27ffd8f096ed91bb
Michael Mayer <michael@photoprism.app>
2023-08-15 15:19:12 +0200

    Live Photos: Update cards.vue #3559
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- frontend/src/component/photo/cards.vue
+++ frontend/src/component/photo/cards.vue
@@ -205,8 +205,8 @@
                 </button>
                 <button v-else-if="photo.Type === 'live'" :title="$gettext('Live')"
                         @click.exact="openPhoto(index)">
-                  <i>phone_iphone</i>
-                  {{ photo.getPhotoInfo() }}
+                  <i>movie</i>
+                  {{ photo.getVideoInfo() }}
                 </button>
                 <button v-else-if="photo.Type === 'animated'" :title="$gettext('Animated')+' GIF'"
                         @click.exact="openPhoto(index)">
