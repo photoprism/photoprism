eb44d637f3594f328131fdd0e3e987f5ca41e8bd
graciousgrey <theresagresch@gmail.com>
2023-10-09 17:00:11 +0200

    Tests: Add acceptance tests

--- frontend/tests/acceptance/page-model/places.js
+++ frontend/tests/acceptance/page-model/places.js
@@ -3,7 +3,8 @@ import { Selector, t } from "testcafe";
 export default class Page {
   constructor() {
     this.placesSearch = Selector('input[aria-label="Search"]');
-    this.openClusterInSearch = Selector("button.action-browse");
+    this.openClusterInSearch = Selector("div.cluster-control-container button.action-browse");
+    this.closeCluster = Selector("div.cluster-control-container button.action-close");
     this.clearLocation = Selector("button.action-clear-location");
     this.zoomOut = Selector("button.maplibregl-ctrl-zoom-out");
   }
