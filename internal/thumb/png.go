d0ea838b377315232d521f6dfd0aa5e18e18691a
Michael Mayer <michael@photoprism.app>
2023-10-31 14:37:47 +0100

    Thumbnails: Add panic handler for unexpected image encoding errors #3858
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- internal/thumb/png.go
+++ internal/thumb/png.go
@@ -1,9 +1,11 @@
 package thumb
 
 import (
+	"fmt"
 	"image"
 	"image/png"
 	"path/filepath"
+	"runtime/debug"
 
 	"github.com/disintegration/imaging"
 
@@ -13,6 +15,13 @@ import (
 
 // Png converts an image to PNG, saves it, and returns it.
 func Png(srcFile, pngFile string, orientation int) (img image.Image, err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("png: %s (panic)\nstack: %s", r, debug.Stack())
+			log.Error(err)
+		}
+	}()
+
 	// Resolve symlinks.
 	if srcFile, err = fs.Resolve(srcFile); err != nil {
 		log.Debugf("png: %s in %s (resolve filename)", err, clean.Log(srcFile))
