4c308f5b3337e13304aace8fe6a543ec5445312d
graciousgrey <theresagresch@gmail.com>
2023-12-07 14:49:52 +0100

    Tests: Update karma.conf.js

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -80,7 +80,7 @@ module.exports = (config) => {
 
     coverageIstanbulReporter: {
       // reports can be any that are listed here: https://github.com/istanbuljs/istanbuljs/tree/aae256fb8b9a3d19414dcf069c592e88712c32c6/packages/istanbul-reports/lib
-      reports: ["lcovonly", "text-summary"],
+      reports: ["lcov", "text-summary"],
 
       // base output directory. If you include %browser% in the path it will be replaced with the karma browser name
       dir: path.join(__dirname, "coverage"),
