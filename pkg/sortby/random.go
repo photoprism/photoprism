0fd3c1790c75e85ba168e40a62663d316b4ac3ed
Michael Mayer <michael@photoprism.app>
2024-01-01 16:22:09 +0100

    Search: Add inline documentation to sortby.RandomExpr()
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- pkg/sortby/random.go
+++ pkg/sortby/random.go
@@ -15,8 +15,12 @@ const (
 func RandomExpr(dialect gorm.Dialect) *gorm.SqlExpr {
 	switch dialect.GetName() {
 	case MySQL:
+		// A seed integer can be passed as an argument, e.g. "RAND(2342)", to generate
+		// reproducible pseudo-random values, see https://mariadb.com/kb/en/rand/.
 		return gorm.Expr("RAND()")
 	case SQLite3:
+		// SQLite does not support specifying a seed to generate a deterministic sequence
+		// of pseudo-random values, see https://www.sqlite.org/lang_corefunc.html#random.
 		return gorm.Expr("RANDOM()")
 	default:
 		return gorm.Expr("RAND()")
