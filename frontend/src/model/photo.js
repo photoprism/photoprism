af077e2ecc609a3e0af95645182955675c5e8417
Bastian Wagener <bastian.wagener@posteo.de>
2024-01-02 09:24:23 +0000

    Removed title to avoid WhatsApp using only the title.

--- frontend/src/model/photo.js
+++ frontend/src/model/photo.js
@@ -1240,7 +1240,6 @@ export class Photo extends RestModel {
         const filesArray = [Util.JSFileFromPhoto(blob, this.mainFile())];
         const shareData = {
           files: filesArray,
-          title: this.getTitle(),
         };
         return navigator.share(shareData);
       });
