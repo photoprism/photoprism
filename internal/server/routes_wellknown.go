64b8ed572d4656d1b3b94a7225bdb07f8d3059b8
Michael Mayer <michael@photoprism.app>
2024-01-08 17:25:07 +0100

    API: Add .well-known/openid-configuration route handler #808 #3943
    This adds an initial "/.well-known/openid-configuration" service
    discovery endpoint for use by OpenID Connect clients.
    
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- internal/server/routes_wellknown.go
+++ internal/server/routes_wellknown.go
@@ -13,11 +13,12 @@ import (
 
 // registerWellknownRoutes configures the ".well-known" service discovery routes.
 func registerWellknownRoutes(router *gin.Engine, conf *config.Config) {
+	// Registers the "/.well-known/oauth-authorization-server" service discovery endpoint for OAuth2 clients.
 	router.Any(conf.BaseUri("/.well-known/oauth-authorization-server"), func(c *gin.Context) {
 		response := &wellknown.OAuthAuthorizationServer{
 			Issuer:                                    conf.SiteUrl(),
+			AuthorizationEndpoint:                     "",
 			TokenEndpoint:                             fmt.Sprintf("%sapi/v1/oauth/token", conf.SiteUrl()),
-			EndSessionEndpoint:                        fmt.Sprintf("%sapi/v1/oauth/logout", conf.SiteUrl()),
 			ScopesSupported:                           acl.Resources.Resources(),
 			ResponseTypesSupported:                    []string{"token"},
 			GrantTypesSupported:                       []string{"client_credentials"},
@@ -28,6 +29,7 @@ func registerWellknownRoutes(router *gin.Engine, conf *config.Config) {
 			CodeChallengeMethodsSupported:             []string{},
 			IntrospectionEndpointAuthMethodsSupported: []string{},
 			RevocationEndpointAuthMethodsSupported:    []string{},
+			EndSessionEndpoint:                        fmt.Sprintf("%sapi/v1/oauth/logout", conf.SiteUrl()),
 			RequestParameterSupported:                 false,
 			RequestObjectSigningAlgValuesSupported:    []string{},
 			DeviceAuthorizationEndpoint:               "",
@@ -36,4 +38,37 @@ func registerWellknownRoutes(router *gin.Engine, conf *config.Config) {
 
 		c.JSON(http.StatusOK, response)
 	})
+
+	// Registers the "/.well-known/openid-configuration" service discovery endpoint for OpenID Connect clients.
+	router.Any(conf.BaseUri("/.well-known/openid-configuration"), func(c *gin.Context) {
+		response := &wellknown.OpenIDConfiguration{
+			Issuer:                                    conf.SiteUrl(),
+			AuthorizationEndpoint:                     "",
+			TokenEndpoint:                             fmt.Sprintf("%sapi/v1/oauth/token", conf.SiteUrl()),
+			UserinfoEndpoint:                          "",
+			RegistrationEndpoint:                      "",
+			JwksUri:                                   "",
+			ResponseTypesSupported:                    []string{"token"},
+			ResponseModesSupported:                    []string{},
+			GrantTypesSupported:                       []string{"client_credentials"},
+			SubjectTypesSupported:                     []string{},
+			IdTokenSigningAlgValuesSupported:          []string{},
+			ScopesSupported:                           acl.Resources.Resources(),
+			TokenEndpointAuthMethodsSupported:         []string{"client_secret_basic", "client_secret_post"},
+			ClaimsSupported:                           []string{},
+			CodeChallengeMethodsSupported:             []string{},
+			IntrospectionEndpoint:                     "",
+			IntrospectionEndpointAuthMethodsSupported: []string{},
+			RevocationEndpoint:                        "",
+			RevocationEndpointAuthMethodsSupported:    []string{},
+			EndSessionEndpoint:                        fmt.Sprintf("%sapi/v1/oauth/logout", conf.SiteUrl()),
+			RequestParameterSupported:                 false,
+			RequestObjectSigningAlgValuesSupported:    []string{},
+			DeviceAuthorizationEndpoint:               "",
+			DpopSigningAlgValuesSupported:             []string{},
+		}
+
+		c.JSON(http.StatusOK, response)
+	})
+
 }
---
+++ internal/server/wellknown/openid.go
@@ -0,0 +1,29 @@
+package wellknown
+
+// OpenIDConfiguration represents the values returned by the "/.well-known/openid-configuration" endpoint.
+type OpenIDConfiguration struct {
+	Issuer                                    string   `json:"issuer"`
+	AuthorizationEndpoint                     string   `json:"authorization_endpoint"`
+	TokenEndpoint                             string   `json:"token_endpoint"`
+	UserinfoEndpoint                          string   `json:"userinfo_endpoint"`
+	RegistrationEndpoint                      string   `json:"registration_endpoint"`
+	JwksUri                                   string   `json:"jwks_uri"`
+	ResponseTypesSupported                    []string `json:"response_types_supported"`
+	ResponseModesSupported                    []string `json:"response_modes_supported"`
+	GrantTypesSupported                       []string `json:"grant_types_supported"`
+	SubjectTypesSupported                     []string `json:"subject_types_supported"`
+	IdTokenSigningAlgValuesSupported          []string `json:"id_token_signing_alg_values_supported"`
+	ScopesSupported                           []string `json:"scopes_supported"`
+	TokenEndpointAuthMethodsSupported         []string `json:"token_endpoint_auth_methods_supported"`
+	ClaimsSupported                           []string `json:"claims_supported"`
+	CodeChallengeMethodsSupported             []string `json:"code_challenge_methods_supported"`
+	IntrospectionEndpoint                     string   `json:"introspection_endpoint"`
+	IntrospectionEndpointAuthMethodsSupported []string `json:"introspection_endpoint_auth_methods_supported"`
+	RevocationEndpoint                        string   `json:"revocation_endpoint"`
+	RevocationEndpointAuthMethodsSupported    []string `json:"revocation_endpoint_auth_methods_supported"`
+	EndSessionEndpoint                        string   `json:"end_session_endpoint"`
+	RequestParameterSupported                 bool     `json:"request_parameter_supported"`
+	RequestObjectSigningAlgValuesSupported    []string `json:"request_object_signing_alg_values_supported"`
+	DeviceAuthorizationEndpoint               string   `json:"device_authorization_endpoint"`
+	DpopSigningAlgValuesSupported             []string `json:"dpop_signing_alg_values_supported"`
+}
