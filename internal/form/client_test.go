c478025513b2a4df3a35200c058ddf60ce1a7701
graciousgrey <theresagresch@gmail.com>
2024-01-16 18:10:42 +0100

    Tests: Add unit tests #3943

--- internal/form/client_test.go
+++ internal/form/client_test.go
@@ -1,7 +1,10 @@
 package form
 
 import (
+	"flag"
 	"testing"
+
+	"github.com/urfave/cli"
 
 	"github.com/photoprism/photoprism/pkg/authn"
 
@@ -14,5 +17,24 @@ func TestNewClient(t *testing.T) {
 		assert.Equal(t, authn.MethodOAuth2, client.Method())
 		assert.Equal(t, "", client.Scope())
 		assert.Equal(t, "", client.Name())
+	})
+}
+
+func TestNewClientFromCli(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		globalSet := flag.NewFlagSet("test", 0)
+		globalSet.String("name", "Test", "")
+		globalSet.String("scope", "*", "")
+		globalSet.String("method", "totp", "")
+
+		app := cli.NewApp()
+		app.Version = "0.0.0"
+
+		c := cli.NewContext(app, globalSet, nil)
+
+		client := NewClientFromCli(c)
+		assert.Equal(t, authn.Method2FA, client.Method())
+		assert.Equal(t, "webdav", client.Scope())
+		assert.Equal(t, "Test", client.Name())
 	})
 }
