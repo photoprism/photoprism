d478f08718d2c6a573348bb617b4dddf1380ccc9
graciousgrey <theresagresch@gmail.com>
2024-01-25 16:36:38 +0100

    Tests: Add unit tests

---
+++ internal/commands/version_test.go
@@ -0,0 +1,25 @@
+package commands
+
+import (
+	"testing"
+
+	"github.com/photoprism/photoprism/pkg/capture"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVersionCommand(t *testing.T) {
+	var err error
+
+	// Create test context with flags and arguments.
+	ctx := NewTestContext([]string{""})
+
+	// Run command with test context.
+	output := capture.Output(func() {
+		err = VersionCommand.Run(ctx)
+	})
+
+	// Check command output for plausibility.
+	// t.Logf(output)
+	assert.NoError(t, err)
+	assert.Contains(t, output, "test")
+}
