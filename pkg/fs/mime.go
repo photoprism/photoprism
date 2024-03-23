8b6f43ad65fc0aae9a091337b4960186a51f0c60
Michael Mayer <michael@photoprism.app>
2024-03-23 11:44:10 +0100

    Index: Skip JPEGs files with a ".heic" extension #4118
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- pkg/fs/mime.go
+++ pkg/fs/mime.go
@@ -9,6 +9,7 @@ import (
 
 const (
 	MimeTypeUnknown = ""
+	MimeTypeBinary  = "application/octet-stream"
 	MimeTypeJPEG    = "image/jpeg"
 	MimeTypeJPEGXL  = "image/jxl"
 	MimeTypePNG     = "image/png"
@@ -26,7 +27,7 @@ const (
 	MimeTypeMOV     = "video/quicktime"
 	MimeTypeSVG     = "image/svg+xml"
 	MimeTypeAI      = "application/vnd.adobe.illustrator"
-	MimeTypePS      = "application/ps"
+	MimeTypePS      = "application/postscript"
 	MimeTypeEPS     = "image/eps"
 	MimeTypeXML     = "text/xml"
 	MimeTypeJSON    = "application/json"
@@ -38,36 +39,63 @@ func MimeType(filename string) (mimeType string) {
 		return MimeTypeUnknown
 	}
 
-	// Workaround for types that cannot be reliably detected.
-	switch Extensions[strings.ToLower(filepath.Ext(filename))] {
-	case ImageDNG:
-		return MimeTypeDNG
-	case ImageAVIF:
-		return MimeTypeAVIF
-	case ImageAVIFS:
-		return MimeTypeAVIFS
-	case ImageHEIC:
-		return MimeTypeHEIC
-	case ImageHEICS:
-		return MimeTypeHEICS
-	case VideoMP4:
-		return MimeTypeMP4
+	// Detect file type based on the filename extension.
+	fileType := Extensions[strings.ToLower(filepath.Ext(filename))]
+
+	// Determine mime type based on the extension for the following
+	// formats, which otherwise cannot be reliably distinguished:
+	switch fileType {
+	// Apple QuickTime Container
 	case VideoMOV:
 		return MimeTypeMOV
-	case VectorSVG:
-		return MimeTypeSVG
-	case VectorAI:
-		return MimeTypeAI
+	// Adobe Digital Negative
+	case ImageDNG:
+		return MimeTypeDNG
+	// Adobe PostScript
 	case VectorPS:
 		return MimeTypePS
+	// Adobe Embedded PostScript
 	case VectorEPS:
 		return MimeTypeEPS
+	// Adobe Illustrator
+	case VectorAI:
+		return MimeTypeAI
+	// Scalable Vector Graphics
+	case VectorSVG:
+		return MimeTypeSVG
 	}
 
-	if t, err := mimetype.DetectFile(filename); err != nil {
-		return MimeTypeUnknown
-	} else {
-		mimeType, _, _ = strings.Cut(t.String(), ";")
+	// Detect mime type based on the file content.
+	detectedType, err := mimetype.DetectFile(filename)
+
+	if detectedType != nil && err == nil {
+		mimeType, _, _ = strings.Cut(detectedType.String(), ";")
+	}
+
+	// Treat "application/octet-stream" as unknown.
+	if mimeType == MimeTypeBinary {
+		mimeType = MimeTypeUnknown
+	}
+
+	// If it could be detected, try to determine mime type from extension:
+	if mimeType == MimeTypeUnknown {
+		switch fileType {
+		// MP4 Multimedia Container
+		case VideoMP4:
+			return MimeTypeMP4
+		// AV1 Image File
+		case ImageAVIF:
+			return MimeTypeAVIF
+		// AV1 Image File Sequence
+		case ImageAVIFS:
+			return MimeTypeAVIFS
+		// High Efficiency Image Container
+		case ImageHEIC:
+			return MimeTypeHEIC
+		// High Efficiency Image Container Sequence
+		case ImageHEICS:
+			return MimeTypeHEICS
+		}
 	}
 
 	return mimeType
--- pkg/fs/mime_test.go
+++ pkg/fs/mime_test.go
@@ -7,71 +7,71 @@ import (
 )
 
 func TestMimeType(t *testing.T) {
-	t.Run("jpg", func(t *testing.T) {
+	t.Run("MP4", func(t *testing.T) {
+		filename := Abs("./testdata/test.mp4")
+		mimeType := MimeType(filename)
+		assert.Equal(t, "video/mp4", mimeType)
+	})
+	t.Run("MOV", func(t *testing.T) {
+		filename := Abs("./testdata/test.mov")
+		mimeType := MimeType(filename)
+		assert.Equal(t, "video/quicktime", mimeType)
+	})
+	t.Run("JPEG", func(t *testing.T) {
 		filename := Abs("./testdata/test.jpg")
 		mimeType := MimeType(filename)
 		assert.Equal(t, "image/jpeg", mimeType)
 	})
-	t.Run("not existing filename", func(t *testing.T) {
+	t.Run("InvalidFilename", func(t *testing.T) {
 		filename := Abs("./testdata/xxx.jpg")
 		mimeType := MimeType(filename)
 		assert.Equal(t, "", mimeType)
 	})
-	t.Run("Empty", func(t *testing.T) {
+	t.Run("EmptyFilename", func(t *testing.T) {
 		mimeType := MimeType("")
 		assert.Equal(t, "", mimeType)
 	})
-	t.Run("avif", func(t *testing.T) {
+	t.Run("AVIF", func(t *testing.T) {
 		filename := Abs("./testdata/test.avif")
 		mimeType := MimeType(filename)
 		assert.Equal(t, "image/avif", mimeType)
+	})
+	t.Run("AVIFS", func(t *testing.T) {
+		filename := Abs("./testdata/test.avifs")
+		mimeType := MimeType(filename)
+		assert.Equal(t, "image/avif-sequence", mimeType)
+	})
+	t.Run("HEIC", func(t *testing.T) {
+		filename := Abs("./testdata/test.heic")
+		mimeType := MimeType(filename)
+		assert.Equal(t, "image/heic", mimeType)
+	})
+	t.Run("HEICS", func(t *testing.T) {
+		filename := Abs("./testdata/test.heics")
+		mimeType := MimeType(filename)
+		assert.Equal(t, "image/heic-sequence", mimeType)
 	})
 	t.Run("DNG", func(t *testing.T) {
 		filename := Abs("./testdata/test.dng")
 		mimeType := MimeType(filename)
 		assert.Equal(t, "image/dng", mimeType)
 	})
-	t.Run("avifs", func(t *testing.T) {
-		filename := Abs("./testdata/test.avifs")
-		mimeType := MimeType(filename)
-		assert.Equal(t, "image/avif-sequence", mimeType)
-	})
-	t.Run("Heic", func(t *testing.T) {
-		filename := Abs("./testdata/test.heic")
-		mimeType := MimeType(filename)
-		assert.Equal(t, "image/heic", mimeType)
-	})
-	t.Run("Heics", func(t *testing.T) {
-		filename := Abs("./testdata/test.heics")
-		mimeType := MimeType(filename)
-		assert.Equal(t, "image/heic-sequence", mimeType)
-	})
-	t.Run("Mp4", func(t *testing.T) {
-		filename := Abs("./testdata/test.mp4")
-		mimeType := MimeType(filename)
-		assert.Equal(t, "video/mp4", mimeType)
-	})
-	t.Run("Mov", func(t *testing.T) {
-		filename := Abs("./testdata/test.mov")
-		mimeType := MimeType(filename)
-		assert.Equal(t, "video/quicktime", mimeType)
-	})
-	t.Run("Svg", func(t *testing.T) {
+	t.Run("SVG", func(t *testing.T) {
 		filename := Abs("./testdata/test.svg")
 		mimeType := MimeType(filename)
 		assert.Equal(t, "image/svg+xml", mimeType)
 	})
-	t.Run("Ai", func(t *testing.T) {
+	t.Run("AI", func(t *testing.T) {
 		filename := Abs("./testdata/test.ai")
 		mimeType := MimeType(filename)
 		assert.Equal(t, "application/vnd.adobe.illustrator", mimeType)
 	})
-	t.Run("ps", func(t *testing.T) {
+	t.Run("PS", func(t *testing.T) {
 		filename := Abs("./testdata/test.ps")
 		mimeType := MimeType(filename)
-		assert.Equal(t, "application/ps", mimeType)
+		assert.Equal(t, "application/postscript", mimeType)
 	})
-	t.Run("eps", func(t *testing.T) {
+	t.Run("EPS", func(t *testing.T) {
 		filename := Abs("./testdata/test.eps")
 		mimeType := MimeType(filename)
 		assert.Equal(t, "image/eps", mimeType)
