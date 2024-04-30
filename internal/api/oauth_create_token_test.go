fa4f983600bba4ac5cd932ac43c47e000319f825
graciousgrey <theresagresch@gmail.com>
2024-04-30 14:48:19 +0200

    Tests: Add unit tests

--- internal/api/oauth_create_token_test.go
+++ internal/api/oauth_create_token_test.go
@@ -7,15 +7,14 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/photoprism/photoprism/internal/config"
 	"github.com/photoprism/photoprism/pkg/authn"
 	"github.com/photoprism/photoprism/pkg/header"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateOAuthToken(t *testing.T) {
-	t.Run("Success", func(t *testing.T) {
+	t.Run("ClientSuccess", func(t *testing.T) {
 		app, router, conf := NewApiTest()
 		conf.SetAuthMode(config.AuthModePasswd)
 		defer conf.SetAuthMode(config.AuthModePublic)
@@ -94,7 +93,7 @@ func TestCreateOAuthToken(t *testing.T) {
 		t.Logf("BODY: %s", w.Body.String())
 		assert.Equal(t, http.StatusUnauthorized, w.Code)
 	})
-	t.Run("WrongClient", func(t *testing.T) {
+	t.Run("NotExistingClient", func(t *testing.T) {
 		app, router, conf := NewApiTest()
 		conf.SetAuthMode(config.AuthModePasswd)
 		defer conf.SetAuthMode(config.AuthModePublic)
@@ -103,7 +102,6 @@ func TestCreateOAuthToken(t *testing.T) {
 
 		var method = "POST"
 		var path = "/api/v1/oauth/token"
-
 		data := url.Values{
 			"grant_type":    {authn.GrantClientCredentials.String()},
 			"client_id":     {"cs5cpu17n6gj2yy6"},
@@ -121,7 +119,7 @@ func TestCreateOAuthToken(t *testing.T) {
 		t.Logf("BODY: %s", w.Body.String())
 		assert.Equal(t, http.StatusUnauthorized, w.Code)
 	})
-	t.Run("InvalidSecret", func(t *testing.T) {
+	t.Run("InvalidClientSecret", func(t *testing.T) {
 		app, router, conf := NewApiTest()
 		conf.SetAuthMode(config.AuthModePasswd)
 		defer conf.SetAuthMode(config.AuthModePublic)
@@ -148,7 +146,7 @@ func TestCreateOAuthToken(t *testing.T) {
 		t.Logf("BODY: %s", w.Body.String())
 		assert.Equal(t, http.StatusUnauthorized, w.Code)
 	})
-	t.Run("AuthNotEnabled", func(t *testing.T) {
+	t.Run("ClientAuthNotEnabled", func(t *testing.T) {
 		app, router, conf := NewApiTest()
 		conf.SetAuthMode(config.AuthModePasswd)
 		defer conf.SetAuthMode(config.AuthModePublic)
@@ -175,7 +173,7 @@ func TestCreateOAuthToken(t *testing.T) {
 		t.Logf("BODY: %s", w.Body.String())
 		assert.Equal(t, http.StatusUnauthorized, w.Code)
 	})
-	t.Run("UnknownAuthMethod", func(t *testing.T) {
+	t.Run("ClientUnknownAuthMethod", func(t *testing.T) {
 		app, router, conf := NewApiTest()
 		conf.SetAuthMode(config.AuthModePasswd)
 		defer conf.SetAuthMode(config.AuthModePublic)
@@ -187,11 +185,10 @@ func TestCreateOAuthToken(t *testing.T) {
 
 		data := url.Values{
 			"grant_type":    {authn.GrantClientCredentials.String()},
-			"client_id":     {"cs5cpu17n6gj2jh6"},
-			"client_secret": {"aaCbOrw6I0vcoXzhnOmXhjpVSyFq0l0e"},
+			"client_id":     {"cs7pvt5h8rw9he34"},
+			"client_secret": {"1831986451da7acf34690b703ff528f67bcf255e005270e9"},
 			"scope":         {"*"},
 		}
-
 		req, _ := http.NewRequest(method, path, strings.NewReader(data.Encode()))
 		req.Header.Add(header.ContentType, header.ContentTypeForm)
 
@@ -202,7 +199,7 @@ func TestCreateOAuthToken(t *testing.T) {
 		t.Logf("BODY: %s", w.Body.String())
 		assert.Equal(t, http.StatusUnauthorized, w.Code)
 	})
-	t.Run("GrantPasswordNoSession", func(t *testing.T) {
+	t.Run("UserNoSession", func(t *testing.T) {
 		app, router, conf := NewApiTest()
 		conf.SetAuthMode(config.AuthModePasswd)
 		defer conf.SetAuthMode(config.AuthModePublic)
@@ -230,7 +227,7 @@ func TestCreateOAuthToken(t *testing.T) {
 		t.Logf("BODY: %s", w.Body.String())
 		assert.Equal(t, http.StatusUnauthorized, w.Code)
 	})
-	t.Run("GrantPasswordSuccess", func(t *testing.T) {
+	t.Run("UserSuccess", func(t *testing.T) {
 		app, router, conf := NewApiTest()
 		conf.SetAuthMode(config.AuthModePasswd)
 		defer conf.SetAuthMode(config.AuthModePublic)
@@ -260,5 +257,96 @@ func TestCreateOAuthToken(t *testing.T) {
 		t.Logf("Header: %s", w.Header())
 		t.Logf("BODY: %s", w.Body.String())
 		assert.Equal(t, http.StatusOK, w.Code)
+	})
+	t.Run("UnregisteredUser", func(t *testing.T) {
+		app, router, conf := NewApiTest()
+		conf.SetAuthMode(config.AuthModePasswd)
+		defer conf.SetAuthMode(config.AuthModePublic)
+
+		CreateOAuthToken(router)
+
+		var method = "POST"
+		var path = "/api/v1/oauth/token"
+
+		data := url.Values{
+			"grant_type":  {authn.GrantPassword.String()},
+			"client_name": {"Visitor"},
+			"username":    {"visitor"},
+			"password":    {"69be27ac5ca305b394046a83f6fda18167ca3d3f2dbe7ac3"},
+			"scope":       {"*"},
+		}
+
+		req, _ := http.NewRequest(method, path, strings.NewReader(data.Encode()))
+		req.Header.Add(header.ContentType, header.ContentTypeForm)
+		req.Header.Add(header.XAuthToken, "69be27ac5ca305b394046a83f6fda18167ca3d3f2dbe7ac3")
+
+		w := httptest.NewRecorder()
+		app.ServeHTTP(w, req)
+
+		t.Logf("Header: %s", w.Header())
+		t.Logf("BODY: %s", w.Body.String())
+		assert.Equal(t, http.StatusUnauthorized, w.Code)
+	})
+	t.Run("UsersDontMatch", func(t *testing.T) {
+		app, router, conf := NewApiTest()
+		conf.SetAuthMode(config.AuthModePasswd)
+		defer conf.SetAuthMode(config.AuthModePublic)
+
+		sessId := AuthenticateUser(app, router, "alice", "Alice123!")
+
+		CreateOAuthToken(router)
+
+		var method = "POST"
+		var path = "/api/v1/oauth/token"
+
+		data := url.Values{
+			"grant_type":  {authn.GrantPassword.String()},
+			"client_name": {"AppPasswordBob"},
+			"username":    {"bob"},
+			"password":    {"Bobbob123!"},
+			"scope":       {"*"},
+		}
+
+		req, _ := http.NewRequest(method, path, strings.NewReader(data.Encode()))
+		req.Header.Add(header.ContentType, header.ContentTypeForm)
+		req.Header.Add(header.XAuthToken, sessId)
+
+		w := httptest.NewRecorder()
+		app.ServeHTTP(w, req)
+
+		t.Logf("Header: %s", w.Header())
+		t.Logf("BODY: %s", w.Body.String())
+		assert.Equal(t, http.StatusUnauthorized, w.Code)
+	})
+	t.Run("DeletedUser", func(t *testing.T) {
+		app, router, conf := NewApiTest()
+		conf.SetAuthMode(config.AuthModePasswd)
+		defer conf.SetAuthMode(config.AuthModePublic)
+
+		sessId := AuthenticateUser(app, router, "deleted", "Deleted123!")
+
+		CreateOAuthToken(router)
+
+		var method = "POST"
+		var path = "/api/v1/oauth/token"
+
+		data := url.Values{
+			"grant_type":  {authn.GrantPassword.String()},
+			"client_name": {"AppPasswordDeleted"},
+			"username":    {"deleted"},
+			"password":    {"Deleted123!"},
+			"scope":       {"*"},
+		}
+
+		req, _ := http.NewRequest(method, path, strings.NewReader(data.Encode()))
+		req.Header.Add(header.ContentType, header.ContentTypeForm)
+		req.Header.Add(header.XAuthToken, sessId)
+
+		w := httptest.NewRecorder()
+		app.ServeHTTP(w, req)
+
+		t.Logf("Header: %s", w.Header())
+		t.Logf("BODY: %s", w.Body.String())
+		assert.Equal(t, http.StatusUnauthorized, w.Code)
 	})
 }
