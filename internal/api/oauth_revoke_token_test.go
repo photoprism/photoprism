38a1616acad52822557c742e89b8a726b61c58fa
graciousgrey <theresagresch@gmail.com>
2024-05-07 16:24:20 +0200

    Tests: Add unit tests

--- internal/api/oauth_revoke_token_test.go
+++ internal/api/oauth_revoke_token_test.go
@@ -20,7 +20,7 @@ func TestRevokeOAuthToken(t *testing.T) {
 	const tokenPath = "/api/v1/oauth/token"
 	const revokePath = "/api/v1/oauth/revoke"
 
-	t.Run("Success", func(t *testing.T) {
+	t.Run("ClientSuccess", func(t *testing.T) {
 		app, router, conf := NewApiTest()
 		conf.SetAuthMode(config.AuthModePasswd)
 		defer conf.SetAuthMode(config.AuthModePublic)
@@ -113,5 +113,46 @@ func TestRevokeOAuthToken(t *testing.T) {
 		t.Logf("Header: %s", revokeResp.Header())
 		t.Logf("BODY: %s", revokeResp.Body.String())
 		assert.Equal(t, http.StatusForbidden, revokeResp.Code)
+	})
+	t.Run("UserSuccess", func(t *testing.T) {
+		app, router, conf := NewApiTest()
+		conf.SetAuthMode(config.AuthModePasswd)
+		defer conf.SetAuthMode(config.AuthModePublic)
+
+		sessId := AuthenticateUser(app, router, "alice", "Alice123!")
+
+		CreateOAuthToken(router)
+		RevokeOAuthToken(router)
+
+		data := url.Values{
+			"grant_type":  {"password"},
+			"client_name": {"AppPasswordAlice"},
+			"username":    {"alice"},
+			"password":    {"Alice123!"},
+			"scope":       {"metrics"},
+		}
+
+		createToken, _ := http.NewRequest("POST", tokenPath, strings.NewReader(data.Encode()))
+		createToken.Header.Add(header.ContentType, header.ContentTypeForm)
+		createToken.Header.Add(header.XAuthToken, sessId)
+
+		createResp := httptest.NewRecorder()
+		app.ServeHTTP(createResp, createToken)
+
+		t.Logf("Header: %s", createResp.Header())
+		t.Logf("BODY: %s", createResp.Body.String())
+		assert.Equal(t, http.StatusOK, createResp.Code)
+		authToken := gjson.Get(createResp.Body.String(), "access_token").String()
+
+		revokeToken, _ := http.NewRequest("POST", revokePath, nil)
+		revokeToken.Header.Add(header.XAuthToken, authToken)
+		createToken.Header.Add(header.XAuthToken, sessId)
+
+		revokeResp := httptest.NewRecorder()
+		app.ServeHTTP(revokeResp, revokeToken)
+
+		t.Logf("Header: %s", revokeResp.Header())
+		t.Logf("BODY: %s", revokeResp.Body.String())
+		assert.Equal(t, http.StatusOK, revokeResp.Code)
 	})
 }
