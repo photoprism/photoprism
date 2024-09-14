78f116e74b245b0ade08dc1954bac3179c451924
Keith Martin <keith.martin@outlook.com.au>
2024-09-14 23:02:53 +1000

    Correct detection to fma missing as VirtualBox doesn't show within docker.

--- scripts/dist/install-tensorflow.sh
+++ scripts/dist/install-tensorflow.sh
@@ -57,16 +57,20 @@ mkdir -p "$DESTDIR"
 if [[ $TF_DRIVER == "auto" ]]; then
   echo "Detecting driver..."
 
-  CPU_DETECTED=$(lshw -c system -c processor -json 2>/dev/null)
+  CPU_DETECTED=$(lshw -c processor -json 2>/dev/null)
 
-  if [[ $(echo "${CPU_DETECTED}" | jq -r '.[0].product') == "VirtualBox" ]]; then
-    TF_DRIVER="cpu"
+  if [[ $(echo "${CPU_DETECTED}" | jq -r '.[].capabilities.fma') != "true" ]]; then
+    TF_DRIVER=""
+    echo "No driver selected as fma is missing"
   elif [[ $(echo "${CPU_DETECTED}" | jq -r '.[].capabilities.avx2') == "true" ]]; then
     TF_DRIVER="avx2"
+    echo "Driver avx2 detected"
   elif [[ $(echo "${CPU_DETECTED}" | jq -r '.[].capabilities.avx') == "true" ]]; then
     TF_DRIVER="avx"
+    echo "Driver avx detected"
   else
     TF_DRIVER=""
+    echo "No drivers detected"
   fi
 fi
 
