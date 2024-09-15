149f1dfe8406fee06f3cee9e1a2f27eb32c8382b
Michael Mayer <michael@photoprism.app>
2024-09-15 16:42:08 +0200

    Develop: Upgrade NodeJS from v20 to v22 and TestCafe from 3.6.0 to 3.6.2
    Signed-off-by: Michael Mayer <michael@photoprism.app>

--- scripts/dist/install-nodejs.sh
+++ scripts/dist/install-nodejs.sh
@@ -15,21 +15,22 @@ set -e
 
 . /etc/os-release
 
-# NodeJS version.
-NODE_MAJOR=20
+# NodeJS version to be installed.
+NODE_MAJOR=22
 
-# Create /etc/apt/keyrings/nodesource.gpg
+# Download the signature key to "/etc/apt/keyrings/nodesource.gpg".
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-# Create /etc/apt/sources.list.d/nodesource.list
+# Add node repository source to "/etc/apt/sources.list.d/nodesource.list".
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 echo "Installing NodeJS and NPM from deb.nodesource.com..."
 apt-get update && apt-get -qq install nodejs
 
+# Upgrade NPM and install development dependencies.
 echo "Installing TestCafe..."
 npm config set cache ~/.cache/npm
-npm install --unsafe-perm=true --allow-root -g npm@latest npm-check-updates@latest license-report@latest n@latest testcafe@3.6.0
+npm install --unsafe-perm=true --allow-root -g npm@latest npm-check-updates@latest license-report@latest n@latest testcafe@3.6.2
 
 echo "Done."
