05db6b2d3fd8a4d0a7c98ebd94f9b13662991cfc
Keith Martin <keith.martin@outlook.com.au>
2024-09-25 12:19:59 +1000

    Enable SoftDelete on Folder

--- internal/entity/folder.go
+++ internal/entity/folder.go
@@ -23,28 +23,28 @@ type Folders []Folder
 
 // Folder represents a file system directory.
 type Folder struct {
-	Path              string     `gorm:"type:bytes;size:1024;uniqueIndex:idx_folders_path_root;" json:"Path" yaml:"Path"`
-	Root              string     `gorm:"type:bytes;size:16;default:'';uniqueIndex:idx_folders_path_root;" json:"Root" yaml:"Root,omitempty"`
-	FolderUID         string     `gorm:"type:bytes;size:42;primaryKey;" json:"UID,omitempty" yaml:"UID,omitempty"`
-	FolderType        string     `gorm:"type:bytes;size:16;" json:"Type" yaml:"Type,omitempty"`
-	FolderTitle       string     `gorm:"type:VARCHAR(200);" json:"Title" yaml:"Title,omitempty"`
-	FolderCategory    string     `gorm:"type:VARCHAR(100);index;" json:"Category" yaml:"Category,omitempty"`
-	FolderDescription string     `gorm:"type:VARCHAR(2048);" json:"Description,omitempty" yaml:"Description,omitempty"`
-	FolderOrder       string     `gorm:"type:bytes;size:32;" json:"Order" yaml:"Order,omitempty"`
-	FolderCountry     string     `gorm:"type:bytes;size:2;index:idx_folders_country_year_month;default:'zz'" json:"Country" yaml:"Country,omitempty"`
-	FolderYear        int        `gorm:"index:idx_folders_country_year_month;" json:"Year" yaml:"Year,omitempty"`
-	FolderMonth       int        `gorm:"index:idx_folders_country_year_month;" json:"Month" yaml:"Month,omitempty"`
-	FolderDay         int        `json:"Day" yaml:"Day,omitempty"`
-	FolderFavorite    bool       `json:"Favorite" yaml:"Favorite,omitempty"`
-	FolderPrivate     bool       `json:"Private" yaml:"Private,omitempty"`
-	FolderIgnore      bool       `json:"Ignore" yaml:"Ignore,omitempty"`
-	FolderWatch       bool       `json:"Watch" yaml:"Watch,omitempty"`
-	FileCount         int        `gorm:"-" json:"FileCount" yaml:"-"`
-	CreatedAt         time.Time  `json:"-" yaml:"-"`
-	UpdatedAt         time.Time  `json:"-" yaml:"-"`
-	ModifiedAt        time.Time  `json:"ModifiedAt,omitempty" yaml:"-"`
-	PublishedAt       *time.Time `sql:"index" json:"PublishedAt,omitempty" yaml:"PublishedAt,omitempty"`
-	DeletedAt         *time.Time `sql:"index" json:"-"`
+	Path              string         `gorm:"type:bytes;size:1024;uniqueIndex:idx_folders_path_root;" json:"Path" yaml:"Path"`
+	Root              string         `gorm:"type:bytes;size:16;default:'';uniqueIndex:idx_folders_path_root;" json:"Root" yaml:"Root,omitempty"`
+	FolderUID         string         `gorm:"type:bytes;size:42;primaryKey;" json:"UID,omitempty" yaml:"UID,omitempty"`
+	FolderType        string         `gorm:"type:bytes;size:16;" json:"Type" yaml:"Type,omitempty"`
+	FolderTitle       string         `gorm:"type:VARCHAR(200);" json:"Title" yaml:"Title,omitempty"`
+	FolderCategory    string         `gorm:"type:VARCHAR(100);index;" json:"Category" yaml:"Category,omitempty"`
+	FolderDescription string         `gorm:"type:VARCHAR(2048);" json:"Description,omitempty" yaml:"Description,omitempty"`
+	FolderOrder       string         `gorm:"type:bytes;size:32;" json:"Order" yaml:"Order,omitempty"`
+	FolderCountry     string         `gorm:"type:bytes;size:2;index:idx_folders_country_year_month;default:'zz'" json:"Country" yaml:"Country,omitempty"`
+	FolderYear        int            `gorm:"index:idx_folders_country_year_month;" json:"Year" yaml:"Year,omitempty"`
+	FolderMonth       int            `gorm:"index:idx_folders_country_year_month;" json:"Month" yaml:"Month,omitempty"`
+	FolderDay         int            `json:"Day" yaml:"Day,omitempty"`
+	FolderFavorite    bool           `json:"Favorite" yaml:"Favorite,omitempty"`
+	FolderPrivate     bool           `json:"Private" yaml:"Private,omitempty"`
+	FolderIgnore      bool           `json:"Ignore" yaml:"Ignore,omitempty"`
+	FolderWatch       bool           `json:"Watch" yaml:"Watch,omitempty"`
+	FileCount         int            `gorm:"-" json:"FileCount" yaml:"-"`
+	CreatedAt         time.Time      `json:"-" yaml:"-"`
+	UpdatedAt         time.Time      `json:"-" yaml:"-"`
+	ModifiedAt        time.Time      `json:"ModifiedAt,omitempty" yaml:"-"`
+	PublishedAt       *time.Time     `sql:"index" json:"PublishedAt,omitempty" yaml:"PublishedAt,omitempty"`
+	DeletedAt         gorm.DeletedAt `sql:"index" json:"-"`
 }
 
 // TableName returns the entity table name.
