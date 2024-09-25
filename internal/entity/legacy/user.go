78508fabfab9db554b9ff6f8c7c3baa5f7a3c6f0
Keith Martin <keith.martin@outlook.com.au>
2024-09-25 18:16:55 +1000

    Enable SoftDelete on User

--- internal/entity/legacy/user.go
+++ internal/entity/legacy/user.go
@@ -5,67 +5,68 @@ import (
 
 	"github.com/photoprism/photoprism/pkg/clean"
 	"github.com/photoprism/photoprism/pkg/rnd"
+	"gorm.io/gorm"
 )
 
 type Users []User
 
 // User represents a person that may optionally log in as user.
 type User struct {
-	ID             int        `gorm:"primaryKey;" json:"-" yaml:"-"`
-	AddressID      int        `gorm:"default:1" json:"-" yaml:"-"`
-	UserUID        string     `gorm:"type:bytes;size:42;uniqueIndex;" json:"UID" yaml:"UID"`
-	MotherUID      string     `gorm:"type:bytes;size:42;" json:"MotherUID" yaml:"MotherUID,omitempty"`
-	FatherUID      string     `gorm:"type:bytes;size:42;" json:"FatherUID" yaml:"FatherUID,omitempty"`
-	GlobalUID      string     `gorm:"type:bytes;size:42;index;" json:"GlobalUID" yaml:"GlobalUID,omitempty"`
-	FullName       string     `gorm:"size:128;" json:"FullName" yaml:"FullName,omitempty"`
-	NickName       string     `gorm:"size:64;" json:"NickName" yaml:"NickName,omitempty"`
-	MaidenName     string     `gorm:"size:64;" json:"MaidenName" yaml:"MaidenName,omitempty"`
-	ArtistName     string     `gorm:"size:64;" json:"ArtistName" yaml:"ArtistName,omitempty"`
-	UserName       string     `gorm:"size:64;" json:"UserName" yaml:"UserName,omitempty"`
-	UserStatus     string     `gorm:"size:32;" json:"UserStatus" yaml:"UserStatus,omitempty"`
-	UserDisabled   bool       `json:"UserDisabled" yaml:"UserDisabled,omitempty"`
-	UserSettings   string     `gorm:"type:LONGTEXT;" json:"-" yaml:"-"`
-	PrimaryEmail   string     `gorm:"size:255;index;" json:"PrimaryEmail" yaml:"PrimaryEmail,omitempty"`
-	EmailConfirmed bool       `json:"EmailConfirmed" yaml:"EmailConfirmed,omitempty"`
-	BackupEmail    string     `gorm:"size:255;" json:"BackupEmail" yaml:"BackupEmail,omitempty"`
-	PersonURL      string     `gorm:"type:bytes;size:255;" json:"PersonURL" yaml:"PersonURL,omitempty"`
-	PersonPhone    string     `gorm:"size:32;" json:"PersonPhone" yaml:"PersonPhone,omitempty"`
-	PersonStatus   string     `gorm:"size:32;" json:"PersonStatus" yaml:"PersonStatus,omitempty"`
-	PersonAvatar   string     `gorm:"type:bytes;size:255;" json:"PersonAvatar" yaml:"PersonAvatar,omitempty"`
-	PersonLocation string     `gorm:"size:128;" json:"PersonLocation" yaml:"PersonLocation,omitempty"`
-	PersonBio      string     `gorm:"type:TEXT;" json:"PersonBio" yaml:"PersonBio,omitempty"`
-	PersonAccounts string     `gorm:"type:LONGTEXT;" json:"-" yaml:"-"`
-	BusinessURL    string     `gorm:"type:bytes;size:255;" json:"BusinessURL" yaml:"BusinessURL,omitempty"`
-	BusinessPhone  string     `gorm:"size:32;" json:"BusinessPhone" yaml:"BusinessPhone,omitempty"`
-	BusinessEmail  string     `gorm:"size:255;" json:"BusinessEmail" yaml:"BusinessEmail,omitempty"`
-	CompanyName    string     `gorm:"size:128;" json:"CompanyName" yaml:"CompanyName,omitempty"`
-	DepartmentName string     `gorm:"size:128;" json:"DepartmentName" yaml:"DepartmentName,omitempty"`
-	JobTitle       string     `gorm:"size:64;" json:"JobTitle" yaml:"JobTitle,omitempty"`
-	BirthYear      int        `json:"BirthYear" yaml:"BirthYear,omitempty"`
-	BirthMonth     int        `json:"BirthMonth" yaml:"BirthMonth,omitempty"`
-	BirthDay       int        `json:"BirthDay" yaml:"BirthDay,omitempty"`
-	TermsAccepted  bool       `json:"TermsAccepted" yaml:"TermsAccepted,omitempty"`
-	IsArtist       bool       `json:"IsArtist" yaml:"IsArtist,omitempty"`
-	IsSubject      bool       `json:"IsSubject" yaml:"IsSubject,omitempty"`
-	RoleAdmin      bool       `json:"RoleAdmin" yaml:"RoleAdmin,omitempty"`
-	RoleGuest      bool       `json:"RoleGuest" yaml:"RoleGuest,omitempty"`
-	RoleChild      bool       `json:"RoleChild" yaml:"RoleChild,omitempty"`
-	RoleFamily     bool       `json:"RoleFamily" yaml:"RoleFamily,omitempty"`
-	RoleFriend     bool       `json:"RoleFriend" yaml:"RoleFriend,omitempty"`
-	WebDAV         bool       `gorm:"column:webdav" json:"WebDAV" yaml:"WebDAV,omitempty"`
-	StoragePath    string     `gorm:"type:bytes;size:500;column:storage_path;" json:"StoragePath" yaml:"StoragePath,omitempty"`
-	CanInvite      bool       `json:"CanInvite" yaml:"CanInvite,omitempty"`
-	InviteToken    string     `gorm:"type:bytes;size:32;" json:"-" yaml:"-"`
-	InvitedBy      string     `gorm:"type:bytes;size:32;" json:"-" yaml:"-"`
-	ConfirmToken   string     `gorm:"type:bytes;size:64;" json:"-" yaml:"-"`
-	ResetToken     string     `gorm:"type:bytes;size:64;" json:"-" yaml:"-"`
-	ApiToken       string     `gorm:"type:bytes;size:128;column:api_token;" json:"-" yaml:"-"`
-	ApiSecret      string     `gorm:"type:bytes;size:128;column:api_secret;" json:"-" yaml:"-"`
-	LoginAttempts  int        `json:"-" yaml:"-"`
-	LoginAt        *time.Time `json:"-" yaml:"-"`
-	CreatedAt      time.Time  `json:"CreatedAt" yaml:"-"`
-	UpdatedAt      time.Time  `json:"UpdatedAt" yaml:"-"`
-	DeletedAt      *time.Time `sql:"index" json:"DeletedAt,omitempty" yaml:"-"`
+	ID             int            `gorm:"primaryKey;" json:"-" yaml:"-"`
+	AddressID      int            `gorm:"default:1" json:"-" yaml:"-"`
+	UserUID        string         `gorm:"type:bytes;size:42;uniqueIndex;" json:"UID" yaml:"UID"`
+	MotherUID      string         `gorm:"type:bytes;size:42;" json:"MotherUID" yaml:"MotherUID,omitempty"`
+	FatherUID      string         `gorm:"type:bytes;size:42;" json:"FatherUID" yaml:"FatherUID,omitempty"`
+	GlobalUID      string         `gorm:"type:bytes;size:42;index;" json:"GlobalUID" yaml:"GlobalUID,omitempty"`
+	FullName       string         `gorm:"size:128;" json:"FullName" yaml:"FullName,omitempty"`
+	NickName       string         `gorm:"size:64;" json:"NickName" yaml:"NickName,omitempty"`
+	MaidenName     string         `gorm:"size:64;" json:"MaidenName" yaml:"MaidenName,omitempty"`
+	ArtistName     string         `gorm:"size:64;" json:"ArtistName" yaml:"ArtistName,omitempty"`
+	UserName       string         `gorm:"size:64;" json:"UserName" yaml:"UserName,omitempty"`
+	UserStatus     string         `gorm:"size:32;" json:"UserStatus" yaml:"UserStatus,omitempty"`
+	UserDisabled   bool           `json:"UserDisabled" yaml:"UserDisabled,omitempty"`
+	UserSettings   string         `gorm:"type:LONGTEXT;" json:"-" yaml:"-"`
+	PrimaryEmail   string         `gorm:"size:255;index;" json:"PrimaryEmail" yaml:"PrimaryEmail,omitempty"`
+	EmailConfirmed bool           `json:"EmailConfirmed" yaml:"EmailConfirmed,omitempty"`
+	BackupEmail    string         `gorm:"size:255;" json:"BackupEmail" yaml:"BackupEmail,omitempty"`
+	PersonURL      string         `gorm:"type:bytes;size:255;" json:"PersonURL" yaml:"PersonURL,omitempty"`
+	PersonPhone    string         `gorm:"size:32;" json:"PersonPhone" yaml:"PersonPhone,omitempty"`
+	PersonStatus   string         `gorm:"size:32;" json:"PersonStatus" yaml:"PersonStatus,omitempty"`
+	PersonAvatar   string         `gorm:"type:bytes;size:255;" json:"PersonAvatar" yaml:"PersonAvatar,omitempty"`
+	PersonLocation string         `gorm:"size:128;" json:"PersonLocation" yaml:"PersonLocation,omitempty"`
+	PersonBio      string         `gorm:"type:TEXT;" json:"PersonBio" yaml:"PersonBio,omitempty"`
+	PersonAccounts string         `gorm:"type:LONGTEXT;" json:"-" yaml:"-"`
+	BusinessURL    string         `gorm:"type:bytes;size:255;" json:"BusinessURL" yaml:"BusinessURL,omitempty"`
+	BusinessPhone  string         `gorm:"size:32;" json:"BusinessPhone" yaml:"BusinessPhone,omitempty"`
+	BusinessEmail  string         `gorm:"size:255;" json:"BusinessEmail" yaml:"BusinessEmail,omitempty"`
+	CompanyName    string         `gorm:"size:128;" json:"CompanyName" yaml:"CompanyName,omitempty"`
+	DepartmentName string         `gorm:"size:128;" json:"DepartmentName" yaml:"DepartmentName,omitempty"`
+	JobTitle       string         `gorm:"size:64;" json:"JobTitle" yaml:"JobTitle,omitempty"`
+	BirthYear      int            `json:"BirthYear" yaml:"BirthYear,omitempty"`
+	BirthMonth     int            `json:"BirthMonth" yaml:"BirthMonth,omitempty"`
+	BirthDay       int            `json:"BirthDay" yaml:"BirthDay,omitempty"`
+	TermsAccepted  bool           `json:"TermsAccepted" yaml:"TermsAccepted,omitempty"`
+	IsArtist       bool           `json:"IsArtist" yaml:"IsArtist,omitempty"`
+	IsSubject      bool           `json:"IsSubject" yaml:"IsSubject,omitempty"`
+	RoleAdmin      bool           `json:"RoleAdmin" yaml:"RoleAdmin,omitempty"`
+	RoleGuest      bool           `json:"RoleGuest" yaml:"RoleGuest,omitempty"`
+	RoleChild      bool           `json:"RoleChild" yaml:"RoleChild,omitempty"`
+	RoleFamily     bool           `json:"RoleFamily" yaml:"RoleFamily,omitempty"`
+	RoleFriend     bool           `json:"RoleFriend" yaml:"RoleFriend,omitempty"`
+	WebDAV         bool           `gorm:"column:webdav" json:"WebDAV" yaml:"WebDAV,omitempty"`
+	StoragePath    string         `gorm:"type:bytes;size:500;column:storage_path;" json:"StoragePath" yaml:"StoragePath,omitempty"`
+	CanInvite      bool           `json:"CanInvite" yaml:"CanInvite,omitempty"`
+	InviteToken    string         `gorm:"type:bytes;size:32;" json:"-" yaml:"-"`
+	InvitedBy      string         `gorm:"type:bytes;size:32;" json:"-" yaml:"-"`
+	ConfirmToken   string         `gorm:"type:bytes;size:64;" json:"-" yaml:"-"`
+	ResetToken     string         `gorm:"type:bytes;size:64;" json:"-" yaml:"-"`
+	ApiToken       string         `gorm:"type:bytes;size:128;column:api_token;" json:"-" yaml:"-"`
+	ApiSecret      string         `gorm:"type:bytes;size:128;column:api_secret;" json:"-" yaml:"-"`
+	LoginAttempts  int            `json:"-" yaml:"-"`
+	LoginAt        *time.Time     `json:"-" yaml:"-"`
+	CreatedAt      time.Time      `json:"CreatedAt" yaml:"-"`
+	UpdatedAt      time.Time      `json:"UpdatedAt" yaml:"-"`
+	DeletedAt      gorm.DeletedAt `sql:"index" json:"DeletedAt,omitempty" yaml:"-"`
 }
 
 // TableName returns the entity database table name.
@@ -109,11 +110,7 @@ var Guest = User{
 
 // Deleted tests if the entity is marked as deleted.
 func (m *User) Deleted() bool {
-	if m.DeletedAt == nil {
-		return false
-	}
-
-	return !m.DeletedAt.IsZero()
+	return m.DeletedAt.Valid
 }
 
 // String returns an identifier that can be used in logs.
