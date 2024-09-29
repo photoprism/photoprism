c0cc672419729506b84cf496aa5e396f9178a051
Keith Martin <keith.martin@outlook.com.au>
2024-09-30 00:04:33 +1000

    Change truncate to individual deletes in foreign key order

--- internal/entity/entity_tables.go
+++ internal/entity/entity_tables.go
@@ -92,14 +92,164 @@ func (list Tables) Truncate(db *gorm.DB) {
 		}
 	}()
 
-	for name = range list {
-		if err := db.Exec(fmt.Sprintf("DELETE FROM %s WHERE 1", name)).Error; err == nil {
-			// log.Debugf("entity: removed all data from %s", name)
-			break
-		} else if err.Error() != "record not found" {
-			log.Debugf("migrate: %s in %s", err, clean.Log(name))
-		}
+	log.Info("migrate: truncate all data tables")
+
+	// Delete tables based on referential integrity.
+	log.Info("migrate: truncate Details")
+	if res := UnscopedDb().Where("1=1").Delete(Details{}); res.Error != nil {
+		log.Errorf("Delete of Details failed with %v", res.Error)
 	}
+	log.Info("migrate: truncate FileShare")
+	if res := UnscopedDb().Where("1=1").Delete(FileShare{}); res.Error != nil {
+		log.Errorf("Delete of FileShare failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate FileSync")
+	if res := UnscopedDb().Where("1=1").Delete(FileSync{}); res.Error != nil {
+		log.Errorf("Delete of FileSync failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate File")
+	if res := UnscopedDb().Where("1=1").Delete(File{}); res.Error != nil {
+		log.Errorf("Delete of Files failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate PhotoKeyword")
+	if res := UnscopedDb().Where("1=1").Delete(PhotoKeyword{}); res.Error != nil {
+		log.Errorf("Delete of PhotoKeyword failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate PhotoLabel")
+	if res := UnscopedDb().Where("1=1").Delete(PhotoLabel{}); res.Error != nil {
+		log.Errorf("Delete of PhotoLabel failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate PhotoAlbum")
+	if res := UnscopedDb().Where("1=1").Delete(PhotoAlbum{}); res.Error != nil {
+		log.Errorf("Delete of PhotoAlbum failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Photo")
+	if res := UnscopedDb().Where("1=1").Delete(Photo{}); res.Error != nil {
+		log.Errorf("Delete of Photo failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Error")
+	if res := UnscopedDb().Where("1=1").Delete(Error{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Password")
+	if res := UnscopedDb().Where("1=1").Delete(Password{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Passcode")
+	if res := UnscopedDb().Where("1=1").Delete(Passcode{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate UserDetails")
+	if res := UnscopedDb().Where("1=1").Delete(UserDetails{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate UserSettings")
+	if res := UnscopedDb().Where("1=1").Delete(UserSettings{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate UserShare")
+	if res := UnscopedDb().Where("1=1").Delete(UserShare{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate User")
+	if res := UnscopedDb().Where("1=1").Delete(User{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Session")
+	if res := UnscopedDb().Where("1=1").Delete(Session{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Client")
+	if res := UnscopedDb().Where("1=1").Delete(Client{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Service")
+	if res := UnscopedDb().Where("1=1").Delete(Service{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Folder")
+	if res := UnscopedDb().Where("1=1").Delete(Folder{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Duplicate")
+	if res := UnscopedDb().Where("1=1").Delete(Duplicate{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate PhotoUser")
+	if res := UnscopedDb().Where("1=1").Delete(PhotoUser{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Cell")
+	if res := UnscopedDb().Where("1=1").Delete(Cell{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Place")
+	if res := UnscopedDb().Where("1=1").Delete(Place{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Camera")
+	if res := UnscopedDb().Where("1=1").Delete(Camera{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Lens")
+	if res := UnscopedDb().Where("1=1").Delete(Lens{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Country")
+	if res := UnscopedDb().Where("1=1").Delete(Country{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate AlbumUser")
+	if res := UnscopedDb().Where("1=1").Delete(AlbumUser{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Albums")
+	if res := UnscopedDb().Where("1=1").Delete(Albums{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Category")
+	if res := UnscopedDb().Where("1=1").Delete(Category{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Label")
+	if res := UnscopedDb().Where("1=1").Delete(Label{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Keyword")
+	if res := UnscopedDb().Where("1=1").Delete(Keyword{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Link")
+	if res := UnscopedDb().Where("1=1").Delete(Link{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Subjects")
+	if res := UnscopedDb().Where("1=1").Delete(Subjects{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Face")
+	if res := UnscopedDb().Where("1=1").Delete(Face{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Marker")
+	if res := UnscopedDb().Where("1=1").Delete(Marker{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+	log.Info("migrate: truncate Reaction")
+	if res := UnscopedDb().Where("1=1").Delete(Reaction{}); res.Error != nil {
+		log.Errorf("Delete failed with %v", res.Error)
+	}
+
+	/*
+		for name = range list {
+			if err := db.Exec(fmt.Sprintf("DELETE FROM %s WHERE 1", name)).Error; err == nil {
+				// log.Debugf("entity: removed all data from %s", name)
+				break
+			} else if err.Error() != "record not found" {
+				log.Debugf("migrate: %s in %s", err, clean.Log(name))
+			}
+		}
+	*/
 }
 
 // Migrate migrates all database tables of registered entities.
