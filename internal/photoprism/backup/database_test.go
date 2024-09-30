3d3a83800b2e1a30f5d996b36354d3072db6c92f
Keith Martin <keith.martin@outlook.com.au>
2024-09-30 22:57:56 +1000

    Improve backup testing

--- internal/photoprism/backup/database_test.go
+++ internal/photoprism/backup/database_test.go
@@ -7,13 +7,16 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/photoprism/photoprism/internal/entity"
+	"github.com/photoprism/photoprism/internal/config"
+	"github.com/photoprism/photoprism/internal/photoprism/get"
 	"github.com/photoprism/photoprism/pkg/fs"
 )
 
 func TestDatabase(t *testing.T) {
 	t.Run("DatabaseNotFoundToStdOut", func(t *testing.T) {
-		if os.Getenv("PHOTOPRISM_TEST_DRIVER") != entity.SQLite3 {
+		c := get.Config()
+
+		if c.DatabaseDriver() != config.SQLite3 {
 			t.Skip("Not executing against sqlite")
 		}
 		backupPath, err := filepath.Abs("./testdata/sqlite")
@@ -35,7 +38,9 @@ func TestDatabase(t *testing.T) {
 		}
 	})
 	t.Run("DatabaseNotFound", func(t *testing.T) {
-		if os.Getenv("PHOTOPRISM_TEST_DRIVER") != entity.SQLite3 {
+		c := get.Config()
+
+		if c.DatabaseDriver() != config.SQLite3 {
 			t.Skip("Not executing against sqlite")
 		}
 		backupPath, err := filepath.Abs("./testdata/sqlite")
@@ -56,4 +61,29 @@ func TestDatabase(t *testing.T) {
 			t.Fatal(err)
 		}
 	})
+	t.Run("Success", func(t *testing.T) {
+		c := get.Config()
+
+		if c.DatabaseDriver() == config.SQLite3 {
+			t.Skip("Executing against sqlite")
+		}
+		backupPath, err := filepath.Abs("./testdata/sqlite")
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err = os.MkdirAll(backupPath, fs.ModeDir); err != nil {
+			t.Fatal(err)
+		}
+
+		err = Database(backupPath, "", false, true, 2)
+
+		assert.Nil(t, err)
+
+		if err = os.RemoveAll(backupPath); err != nil {
+			t.Fatal(err)
+		}
+	})
+
 }
