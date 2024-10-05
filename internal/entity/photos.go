faa416738b307e3a13dbe168688807ef5044333a
Keith Martin <keith.martin@outlook.com.au>
2024-10-05 21:59:34 +1000

    Add Preloaded Find and First

--- internal/entity/photos.go
+++ internal/entity/photos.go
@@ -1,5 +1,7 @@
 package entity
 
+import "gorm.io/gorm"
+
 // Photos represents a list of photos.
 type Photos []Photo
 
@@ -23,4 +25,40 @@ func (m Photos) UIDs() []string {
 	}
 
 	return result
+}
+
+// UnscopedSearchPhotos populates the photos that match the results of a Where(query, values) including soft delete records
+func UnscopedSearchPhotos(photos *Photos, query string, values ...interface{}) (tx *gorm.DB) {
+	// Preload related entities if a matching record is found.
+	stmt := UnscopedDb().
+		Preload("Labels", func(db *gorm.DB) *gorm.DB {
+			return db.Order("photos_labels.uncertainty ASC, photos_labels.label_id DESC")
+		}).
+		Preload("Labels.Label").
+		Preload("Camera").
+		Preload("Lens").
+		Preload("Details").
+		Preload("Place").
+		Preload("Cell").
+		Preload("Cell.Place")
+
+	return stmt.Where(query, values...).Find(photos)
+}
+
+// ScopedSearchPhotos populates the photos that match the results of a Where(query, values) excluding soft delete records
+func ScopedSearchPhotos(photos *Photos, query string, values ...interface{}) (tx *gorm.DB) {
+	// Preload related entities if a matching record is found.
+	stmt := Db().
+		Preload("Labels", func(db *gorm.DB) *gorm.DB {
+			return db.Order("photos_labels.uncertainty ASC, photos_labels.label_id DESC")
+		}).
+		Preload("Labels.Label").
+		Preload("Camera").
+		Preload("Lens").
+		Preload("Details").
+		Preload("Place").
+		Preload("Cell").
+		Preload("Cell.Place")
+
+	return stmt.Where(query, values...).Find(photos)
 }
