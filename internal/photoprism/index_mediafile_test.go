4565bc163630ac0f1711afd2b25fd31f4f834c1e
Keith Martin <keith.martin@outlook.com.au>
2024-10-05 16:00:32 +1000

    Add Primary and Secondary file load test

--- internal/photoprism/index_mediafile_test.go
+++ internal/photoprism/index_mediafile_test.go
@@ -77,6 +77,72 @@ func TestIndex_MediaFile(t *testing.T) {
 		assert.Equal(t, "Blue Gopher", mediaFile.metaData.Title)
 		assert.Equal(t, IndexStatus("added"), result.Status)
 	})
+
+	t.Run("twoFiles", func(t *testing.T) {
+		cfg := config.TestConfig()
+
+		cfg.InitializeTestData()
+
+		tf := classify.New(cfg.AssetsPath(), cfg.DisableTensorFlow())
+		nd := nsfw.New(cfg.NSFWModelPath())
+		fn := face.NewNet(cfg.FaceNetModelPath(), "", cfg.DisableTensorFlow())
+		convert := NewConvert(cfg)
+
+		ind := NewIndex(cfg, tf, nd, fn, convert, NewFiles(), NewPhotos())
+		indexOpt := IndexOptionsAll()
+		mediaFile, err := NewMediaFile(cfg.ExamplesPath() + "/beach_sand.jpg")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, "", mediaFile.metaData.Title)
+		assert.Equal(t, "", mediaFile.metaData.CameraMake)
+
+		result := ind.UserMediaFile(mediaFile, indexOpt, "beach_sand.jpg", "", entity.Admin.GetUID())
+
+		assert.Equal(t, "", mediaFile.metaData.Title)
+		assert.Equal(t, "Apple", mediaFile.metaData.CameraMake)
+		log.Debugf("mediaFile.metaData = %v", mediaFile.metaData)
+
+		photo := entity.Photo{}
+		entity.Db().Debug().Model(entity.Photo{}).Preload("Details").Where("original_name = 'beach_sand'").First(&photo)
+		assert.Equal(t, "beach_sand", photo.OriginalName)
+		quality := photo.PhotoQuality
+		cameraid := photo.CameraID
+		placeid := photo.PlaceID
+		assert.Contains(t, photo.Details.Keywords, "beach")
+		assert.Contains(t, photo.Details.Keywords, "sand")
+		assert.Contains(t, photo.Details.Keywords, "blue")
+		assert.Equal(t, IndexStatus("added"), result.Status)
+
+		mediaFile, err = NewMediaFile(cfg.ExamplesPath() + "/beach_sand.json")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Equal(t, "", mediaFile.metaData.CameraMake)
+		assert.Equal(t, "", mediaFile.metaData.Title)
+
+		result = ind.UserMediaFile(mediaFile, indexOpt, "beach_sand.jpg", "", entity.Admin.GetUID())
+
+		// This isn't a Primary file, so these should NOT be updated.
+		assert.Equal(t, "", mediaFile.metaData.CameraMake)
+		assert.Equal(t, "", mediaFile.metaData.Title)
+
+		photo = entity.Photo{}
+		entity.Db().Model(entity.Photo{}).Preload("Details").Where("original_name = 'beach_sand'").First(&photo)
+		assert.Equal(t, "beach_sand", photo.OriginalName)
+		assert.Contains(t, photo.Details.Keywords, "beach")
+		assert.Contains(t, photo.Details.Keywords, "sand")
+		assert.Contains(t, photo.Details.Keywords, "blue")
+		// Make sure that reading in a json file with the same details as the photo hasn't changed the data.
+		assert.Equal(t, quality, photo.PhotoQuality)
+		assert.Equal(t, cameraid, photo.CameraID)
+		assert.Equal(t, placeid, photo.PlaceID)
+
+		assert.Equal(t, IndexStatus("added"), result.Status)
+	})
+
 	t.Run("error", func(t *testing.T) {
 		cfg := config.TestConfig()
 
