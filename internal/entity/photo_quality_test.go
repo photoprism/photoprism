c2382b95bad874000ea9d8a87344b75df5bc650d
Keith Martin <keith.martin@outlook.com.au>
2024-10-05 13:06:49 +1000

    Add test for Save with Unknown in Struct vs Valid ID

--- internal/entity/photo_quality_test.go
+++ internal/entity/photo_quality_test.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
 func TestPhoto_QualityScore(t *testing.T) {
@@ -22,6 +23,92 @@ func TestPhoto_QualityScore(t *testing.T) {
 	})
 	t.Run("PhotoFixturePhoto15 - description with non-photographic", func(t *testing.T) {
 		assert.Equal(t, 2, PhotoFixtures.Pointer("Photo15").QualityScore())
+	})
+	// digikam test that fails in gorm2.
+	t.Run("digikam test", func(t *testing.T) {
+		//digikam := NewPhoto(true)
+		digikam := Photo{ //ID: 9800000,
+			//PhotoUID:         "PSKS0MPLV8V124RH",
+			TakenAt:          time.Date(2020, 10, 17, 17, 48, 0, 0, time.UTC),
+			TakenAtLocal:     time.Date(2020, 10, 17, 12, 48, 0, 0, time.UTC),
+			TakenSrc:         "meta",
+			PhotoType:        "image",
+			TypeSrc:          "",
+			PhotoTitle:       "Bismarckviertel / Berlin / 2020",
+			TitleSrc:         "",
+			PhotoDescription: "",
+			DescriptionSrc:   "meta",
+			PhotoPath:        "2020/10",
+			PhotoName:        "20201017_154824_43C25EB3",
+			OriginalName:     "digikam",
+			PhotoFavorite:    false,
+			//PhotoSingle
+			PhotoPrivate:     false,
+			PhotoScan:        false,
+			PhotoPanorama:    false,
+			TimeZone:         "Europe/Berlin",
+			Place:            PlaceFixtures.Pointer("Germany"),
+			PlaceID:          PlaceFixtures.Pointer("Germany").ID,
+			PlaceSrc:         "xmp",
+			Cell:             CellFixtures.Pointer("Neckarbrücke"),
+			CellID:           CellFixtures.Pointer("Neckarbrücke").ID,
+			CellAccuracy:     0,
+			PhotoAltitude:    84,
+			PhotoLat:         52.46052169777778,
+			PhotoLng:         13.331401824722223,
+			PhotoCountry:     "de",
+			PhotoYear:        2020,
+			PhotoMonth:       10,
+			PhotoDay:         17,
+			PhotoIso:         100,
+			PhotoExposure:    "1/50",
+			PhotoFNumber:     1.8,
+			PhotoFocalLength: 27,
+			PhotoQuality:     0,
+			PhotoResolution:  0,
+			Camera:           CameraFixtures.Pointer("canon-eos-6d"),
+			CameraID:         CameraFixtures.Pointer("canon-eos-6d").ID,
+			CameraSerial:     "",
+			CameraSrc:        "meta",
+			Lens:             LensFixtures.Pointer("lens-f-380"),
+			LensID:           LensFixtures.Pointer("lens-f-380").ID,
+			Details:          nil, // DetailsFixtures.Pointer("lake", 1000000),
+			Keywords: []Keyword{
+				KeywordFixtures.Get("bridge"),
+			},
+			Albums: []Album{
+				AlbumFixtures.Get("holiday-2030"),
+			},
+			Files: []File{},
+			/*			Labels: []PhotoLabel{
+						LabelFixtures.PhotoLabel(1000000, "flower", 38, "image"),
+						LabelFixtures.PhotoLabel(1000000, "cake", 38, "manual"),
+					},*/
+			CreatedAt:  time.Now(),
+			UpdatedAt:  time.Now(),
+			EditedAt:   nil,
+			CheckedAt:  nil,
+			DeletedAt:  gorm.DeletedAt{},
+			PhotoColor: 9,
+			PhotoStack: 0,
+			PhotoFaces: 0,
+		}
+		// Create a valid record
+		digikam.Create()
+		// validate that the QualityScore is correct
+		assert.Equal(t, 3, digikam.QualityScore())
+
+		// Replicate the data scenario for the json file.
+		// As created in UserMediaFile for a non primary file
+		digikam.Camera = &UnknownCamera
+		digikam.Cell = &UnknownLocation
+		digikam.Place = &UnknownPlace
+		// Save the record, which doesn't do anything in GormV1
+		digikam.Save()
+
+		// Validate that the QualityScore stays the same.
+		assert.Equal(t, 3, digikam.QualityScore())
+
 	})
 }
 
