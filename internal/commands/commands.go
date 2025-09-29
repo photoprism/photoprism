d16f9d9f3380821b803120bf7a4f214e6d6efe19
Keith Martin <keith.martin@outlook.com.au>
2025-09-29 22:31:06 +1000

    Commands: improve non interactive handling

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -41,11 +41,10 @@ import (
 const NONINTERACTIVE = "noninteractive"
 
 var log = event.Log
-var cliMode = strings.ToLower(os.Getenv(config.EnvVar("cli")))
 
 // RunNonInteractively checks if command should run non-interactively.
 func RunNonInteractively(confirmed bool) bool {
-	return confirmed || cliMode == NONINTERACTIVE
+	return confirmed || strings.ToLower(os.Getenv(config.EnvVar("cli"))) == NONINTERACTIVE
 }
 
 // PhotoPrism contains the photoprism CLI (sub-)commands.
