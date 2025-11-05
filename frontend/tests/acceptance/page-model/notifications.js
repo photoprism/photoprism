c727d19411d9457df388b4b501d26d63ab0261a4
Keith Martin <keith.martin@outlook.com.au>
2025-11-05 20:21:58 +1000

    Tests: add missing showLogs to console.trace

--- frontend/tests/acceptance/page-model/notifications.js
+++ frontend/tests/acceptance/page-model/notifications.js
@@ -44,7 +44,7 @@ export default class Page {
         } catch {
           // ignore the error as the item may not show up
           showLogs && this.logMessage("After  Click In Catch in waitForSpecficEvent");
-          console.trace("notify close missed in waitForSpecficEvent " + event)
+          showLogs && console.trace("notify close missed in waitForSpecficEvent " + event)
         } finally {
           return
         }
