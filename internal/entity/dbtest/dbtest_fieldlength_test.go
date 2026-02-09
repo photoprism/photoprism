b1d91870229e24ee294c31f0ec83c1e4ae1165a8
Keith Martin <keith.martin@outlook.com.au>
2026-02-09 22:05:17 +1000

    Tests: Validate 160 chars into Slug columns

--- internal/entity/dbtest/dbtest_fieldlength_test.go
+++ internal/entity/dbtest/dbtest_fieldlength_test.go
@@ -5,12 +5,16 @@ import (
 	"testing"
 	"time"
 
+	"github.com/gosimple/slug"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"gorm.io/gorm"
 	"gorm.io/gorm/clause"
 	"gorm.io/gorm/schema"
 
 	"github.com/photoprism/photoprism/internal/entity"
+	"github.com/photoprism/photoprism/pkg/rnd"
+	"github.com/photoprism/photoprism/pkg/txt"
 )
 
 var checkedTime = time.Date(2021, 1, 1, 0, 0, 0, 0, time.UTC)
@@ -1704,6 +1708,239 @@ func TestInitDBLengths(t *testing.T) {
 		assert.NoError(t, result2.Error, "UnscopedDb().Delete()")
 
 		assert.Equal(t, expectedCount, actualCount)
+	})
+
+	t.Run("AlbumSlug", func(t *testing.T) {
+		s := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et purus pulvinar, iaculis enim at, placerat mauris. Phasellus lacus turpis, egestas id cursus condimentum, facilisis malesuada dui cras."
+		sTxt := slug.Make(s)
+
+		a := entity.NewAlbum("PhotosPrism", entity.AlbumFolder)
+		a.AlbumSlug = txt.Clip(sTxt, 80)
+		assert.Len(t, a.AlbumSlug, 80)
+		assert.NotEqual(t, sTxt, a.AlbumSlug)
+		require.NoError(t, a.Create())
+		require.NoError(t, entity.UnscopedDb().Delete(&a).Error)
+
+		a = entity.NewAlbum("PhotosPrism", entity.AlbumFolder)
+		a.AlbumSlug = txt.Clip(sTxt, 160)
+		assert.Len(t, a.AlbumSlug, 160)
+		assert.NotEqual(t, sTxt, a.AlbumSlug)
+		require.NoError(t, a.Create())
+		require.NoError(t, entity.UnscopedDb().Delete(&a).Error)
+
+		a = entity.NewAlbum("PhotosPrism", entity.AlbumFolder)
+		a.AlbumSlug = txt.Clip(sTxt, 161)
+		assert.Len(t, a.AlbumSlug, 161)
+		assert.NotEqual(t, sTxt, a.AlbumSlug)
+		switch entity.DbDialect() {
+		case entity.MySQL:
+			require.ErrorContains(t, a.Create(), "Data too long")
+		default:
+			require.NoError(t, a.Create())
+			require.NoError(t, entity.UnscopedDb().Delete(&a).Error)
+		}
+	})
+
+	t.Run("CameraSlug", func(t *testing.T) {
+		s := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et purus pulvinar, iaculis enim at, placerat mauris. Phasellus lacus turpis, egestas id cursus condimentum, facilisis malesuada dui cras."
+		sTxt := slug.Make(s)
+		c := entity.NewCamera("PhotosPrism", "PhotoPrism Test Model 80")
+		c.CameraSlug = txt.Clip(sTxt, 80)
+		assert.Len(t, c.CameraSlug, 80)
+		assert.NotEqual(t, sTxt, c.CameraSlug)
+		require.NoError(t, c.Create())
+		require.NoError(t, entity.UnscopedDb().Delete(&c).Error)
+
+		c = entity.NewCamera("PhotosPrism", "PhotoPrism Test Model 160")
+		c.CameraSlug = txt.Clip(sTxt, 160)
+		assert.Len(t, c.CameraSlug, 160)
+		assert.NotEqual(t, sTxt, c.CameraSlug)
+		require.NoError(t, c.Create())
+		require.NoError(t, entity.UnscopedDb().Delete(&c).Error)
+
+		c = entity.NewCamera("PhotosPrism", "PhotoPrism Test Model 162")
+		c.CameraSlug = txt.Clip(sTxt, 162)
+		assert.Len(t, c.CameraSlug, 162)
+		assert.NotEqual(t, sTxt, c.CameraSlug)
+		switch entity.DbDialect() {
+		case entity.MySQL:
+			require.ErrorContains(t, c.Create(), "Data too long")
+		default:
+			require.NoError(t, c.Create())
+			require.NoError(t, entity.UnscopedDb().Delete(&c).Error)
+		}
+
+		// The following string is randomly generated via https://pinkylam.me/generator/chinese-lorem-ipsum/
+		s = "息戊玩員方大！飛女節兄抓央更三黑院都亮到，向友服面那故收共穴哪戶國色？六回多丟，每告晚友苦貓常和辛是汁去苦卜「犬丁風門反一支蝴」話怪更這校！蝸隻少七話就直弓「活吉天」氣久背真掃朋拉牙送別飛兄。林午頁坡急蛋奶就。手陽土，長七上步蝶訴石尼文邊鼻抄打時尺聽重道，尤蛋珠。"
+		sTxt = slug.Make(s)
+		c = entity.NewCamera("PhotosPrism", "PhotoPrism Test Model 162 Unicode")
+		c.CameraSlug = txt.Clip(slug.Make(s), 162)
+		assert.Len(t, c.CameraSlug, 162)
+		switch entity.DbDialect() {
+		case entity.MySQL:
+			require.ErrorContains(t, c.Create(), "Data too long")
+		default:
+			require.NoError(t, c.Create())
+			require.NoError(t, entity.UnscopedDb().Delete(&c).Error)
+		}
+	})
+
+	t.Run("CountrySlug", func(t *testing.T) {
+		s := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et purus pulvinar, iaculis enim at, placerat mauris. Phasellus lacus turpis, egestas id cursus condimentum, facilisis malesuada dui cras."
+		sTxt := slug.Make(s)
+
+		c := entity.NewCountry("TC", "Test Country")
+		c.CountrySlug = txt.Clip(sTxt, 80)
+		assert.Len(t, c.CountrySlug, 80)
+		assert.NotEqual(t, sTxt, c.CountrySlug)
+		require.NoError(t, c.Create())
+		require.NoError(t, entity.UnscopedDb().Delete(&c).Error)
+
+		c = entity.NewCountry("TC", "Test Country")
+		c.CountrySlug = txt.Clip(sTxt, 160)
+		assert.Len(t, c.CountrySlug, 160)
+		assert.NotEqual(t, sTxt, c.CountrySlug)
+		require.NoError(t, c.Create())
+		require.NoError(t, entity.UnscopedDb().Delete(&c).Error)
+
+		c = entity.NewCountry("TC", "Test Country")
+		c.CountrySlug = txt.Clip(sTxt, 161)
+		assert.Len(t, c.CountrySlug, 161)
+		assert.NotEqual(t, sTxt, c.CountrySlug)
+		switch entity.DbDialect() {
+		case entity.MySQL:
+			require.ErrorContains(t, c.Create(), "Data too long")
+		default:
+			require.NoError(t, c.Create())
+			require.NoError(t, entity.UnscopedDb().Delete(&c).Error)
+		}
+	})
+
+	t.Run("LabelSlug", func(t *testing.T) {
+		s := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et purus pulvinar, iaculis enim at, placerat mauris. Phasellus lacus turpis, egestas id cursus condimentum, facilisis malesuada dui cras."
+		sTxt := slug.Make(s)
+
+		l := entity.NewLabel("TestingLabelLength", 5)
+		l.LabelSlug = txt.Clip(sTxt, 80)
+		l.CustomSlug = txt.Clip(sTxt, 80)
+		assert.Len(t, l.LabelSlug, 80)
+		assert.NotEqual(t, sTxt, l.LabelSlug)
+		require.NoError(t, l.Create())
+		require.NoError(t, entity.UnscopedDb().Delete(&l).Error)
+
+		l = entity.NewLabel("TestingLabelLength", 5)
+		l.LabelSlug = txt.Clip(sTxt, 160)
+		l.CustomSlug = txt.Clip(sTxt, 160)
+		assert.Len(t, l.LabelSlug, 160)
+		assert.NotEqual(t, sTxt, l.LabelSlug)
+		require.NoError(t, l.Create())
+		require.NoError(t, entity.UnscopedDb().Delete(&l).Error)
+
+		l = entity.NewLabel("TestingLabelLength", 5)
+		l.LabelSlug = txt.Clip(sTxt, 161)
+		l.CustomSlug = txt.Clip(sTxt, 161)
+		assert.Len(t, l.LabelSlug, 161)
+		assert.NotEqual(t, sTxt, l.LabelSlug)
+		switch entity.DbDialect() {
+		case entity.MySQL:
+			require.ErrorContains(t, l.Create(), "Data too long")
+		default:
+			require.NoError(t, l.Create())
+			require.NoError(t, entity.UnscopedDb().Delete(&l).Error)
+		}
+	})
+
+	t.Run("LensSlug", func(t *testing.T) {
+		s := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et purus pulvinar, iaculis enim at, placerat mauris. Phasellus lacus turpis, egestas id cursus condimentum, facilisis malesuada dui cras."
+		sTxt := slug.Make(s)
+
+		l := entity.NewLens("TestingLensLength", "Test Lens")
+		l.LensSlug = txt.Clip(sTxt, 80)
+		assert.Len(t, l.LensSlug, 80)
+		assert.NotEqual(t, sTxt, l.LensSlug)
+		require.NoError(t, l.Create())
+		require.NoError(t, entity.UnscopedDb().Delete(&l).Error)
+
+		l = entity.NewLens("TestingLensLength", "Test Lens")
+		l.LensSlug = txt.Clip(sTxt, 160)
+		assert.Len(t, l.LensSlug, 160)
+		assert.NotEqual(t, sTxt, l.LensSlug)
+		require.NoError(t, l.Create())
+		require.NoError(t, entity.UnscopedDb().Delete(&l).Error)
+
+		l = entity.NewLens("TestingLensLength", "Test Lens")
+		l.LensSlug = txt.Clip(sTxt, 161)
+		assert.Len(t, l.LensSlug, 161)
+		assert.NotEqual(t, sTxt, l.LensSlug)
+		switch entity.DbDialect() {
+		case entity.MySQL:
+			require.ErrorContains(t, l.Create(), "Data too long")
+		default:
+			require.NoError(t, l.Create())
+			require.NoError(t, entity.UnscopedDb().Delete(&l).Error)
+		}
+	})
+
+	t.Run("ShareSlug", func(t *testing.T) {
+		s := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et purus pulvinar, iaculis enim at, placerat mauris. Phasellus lacus turpis, egestas id cursus condimentum, facilisis malesuada dui cras."
+		sTxt := slug.Make(s)
+
+		l := entity.NewLink(rnd.GenerateUID(entity.AlbumUID), false, false)
+		l.ShareSlug = txt.Clip(sTxt, 80)
+		assert.Len(t, l.ShareSlug, 80)
+		assert.NotEqual(t, sTxt, l.ShareSlug)
+		require.NoError(t, l.Save())
+		require.NoError(t, entity.UnscopedDb().Delete(&l).Error)
+
+		l = entity.NewLink(rnd.GenerateUID(entity.AlbumUID), false, false)
+		l.ShareSlug = txt.Clip(sTxt, 160)
+		assert.Len(t, l.ShareSlug, 160)
+		assert.NotEqual(t, sTxt, l.ShareSlug)
+		require.NoError(t, l.Save())
+		require.NoError(t, entity.UnscopedDb().Delete(&l).Error)
+
+		l = entity.NewLink(rnd.GenerateUID(entity.AlbumUID), false, false)
+		l.ShareSlug = txt.Clip(sTxt, 161)
+		assert.Len(t, l.ShareSlug, 161)
+		assert.NotEqual(t, sTxt, l.ShareSlug)
+		switch entity.DbDialect() {
+		case entity.MySQL:
+			require.ErrorContains(t, l.Save(), "Data too long")
+		default:
+			require.NoError(t, l.Save())
+			require.NoError(t, entity.UnscopedDb().Delete(&l).Error)
+		}
+	})
+
+	t.Run("SubjSlug", func(t *testing.T) {
+		s := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et purus pulvinar, iaculis enim at, placerat mauris. Phasellus lacus turpis, egestas id cursus condimentum, facilisis malesuada dui cras."
+		sTxt := slug.Make(s)
+
+		l := entity.NewSubject("TestingLensLength", entity.SubjPerson, entity.SrcManual)
+		l.SubjSlug = txt.Clip(sTxt, 80)
+		assert.Len(t, l.SubjSlug, 80)
+		assert.NotEqual(t, sTxt, l.SubjSlug)
+		require.NoError(t, l.Create())
+		require.NoError(t, entity.UnscopedDb().Delete(&l).Error)
+
+		l = entity.NewSubject("TestingLensLength", entity.SubjPerson, entity.SrcManual)
+		l.SubjSlug = txt.Clip(sTxt, 160)
+		assert.Len(t, l.SubjSlug, 160)
+		assert.NotEqual(t, sTxt, l.SubjSlug)
+		require.NoError(t, l.Create())
+		require.NoError(t, entity.UnscopedDb().Delete(&l).Error)
+
+		l = entity.NewSubject("TestingLensLength", entity.SubjPerson, entity.SrcManual)
+		l.SubjSlug = txt.Clip(sTxt, 161)
+		assert.Len(t, l.SubjSlug, 161)
+		assert.NotEqual(t, sTxt, l.SubjSlug)
+		switch entity.DbDialect() {
+		case entity.MySQL:
+			require.ErrorContains(t, l.Create(), "Data too long")
+		default:
+			require.NoError(t, l.Create())
+			require.NoError(t, entity.UnscopedDb().Delete(&l).Error)
+		}
 	})
 
 	log.Info("End expecting data to long Error or SQLSTATE from dbtest_fieldlength_test")
