ed10ffb6860af45020bca0a07d122f13ef4e2192
Amey Pawar <ameyap007aaa@gmail.com>
2026-07-05 12:05:16 +0530

    Metadata: Preset time zone in rating normalization tests #713

--- internal/entity/photo_rating_test.go
+++ internal/entity/photo_rating_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/photoprism/photoprism/pkg/time/tz"
 )
 
 func TestPhoto_SetRating(t *testing.T) {
@@ -74,19 +76,19 @@ func TestPhoto_SetRating(t *testing.T) {
 
 func TestPhoto_NormalizeRating(t *testing.T) {
 	t.Run("ClampAboveMax", func(t *testing.T) {
-		m := Photo{PhotoRating: 9}
+		m := Photo{TimeZone: tz.Local, PhotoRating: 9}
 
 		assert.True(t, m.NormalizeValues())
 		assert.Equal(t, 5, m.PhotoRating)
 	})
 	t.Run("ClampBelowMin", func(t *testing.T) {
-		m := Photo{PhotoRating: -3}
+		m := Photo{TimeZone: tz.Local, PhotoRating: -3}
 
 		assert.True(t, m.NormalizeValues())
 		assert.Equal(t, 0, m.PhotoRating)
 	})
 	t.Run("InRangeUnchanged", func(t *testing.T) {
-		m := Photo{PhotoRating: 5}
+		m := Photo{TimeZone: tz.Local, PhotoRating: 5}
 
 		assert.False(t, m.NormalizeValues())
 		assert.Equal(t, 5, m.PhotoRating)
